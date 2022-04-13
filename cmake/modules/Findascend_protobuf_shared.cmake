d6c205820393cd3c9df5da0eb24a04c8ede139fc
yu-liang-bin <y1416490440@163.com>
2022-04-13 16:35:56 +0800

    [CHG]use spuerbuild
    Match-id-796e82fd6bec42114a1f483c0d03305a6451bbfc

---
+++ cmake/modules/Findascend_protobuf_shared.cmake
@@ -0,0 +1,33 @@
+set(ASCEND_PROTOBUF_SHARED_DIR ${CMAKE_INSTALL_PREFIX}/ascend_protobuf)
+
+find_path(ASCEND_PROTOBUF_SHARED_INCLUDE
+             PATHS ${ASCEND_PROTOBUF_SHARED_DIR}/include
+             NO_DEFAULT_PATH
+             CMAKE_FIND_ROOT_PATH_BOTH
+             NAMES google/protobuf/api.pb.h)
+mark_as_advanced(ASCEND_PROTOBUF_SHARED_INCLUDE)
+
+find_library(ASCEND_PROTOBUF_SHARED_LIBRARY
+            PATHS ${ASCEND_PROTOBUF_SHARED_DIR}/lib
+            NO_DEFAULT_PATH
+            CMAKE_FIND_ROOT_PATH_BOTH
+            NAMES libascend_protobuf.so libprotobufd.lib libprotobuf.lib)
+mark_as_advanced(ASCEND_PROTOBUF_SHARED_LIBRARY)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ascend_protobuf_shared
+    REQUIRED_VARS ASCEND_PROTOBUF_SHARED_INCLUDE ASCEND_PROTOBUF_SHARED_LIBRARY
+    )
+
+if(ascend_protobuf_shared_FOUND)
+    set(ASCEND_PROTOBUF_SHARED_INCLUDE_DIR ${ASCEND_PROTOBUF_SHARED_INCLUDE})
+    set(ASCEND_PROTOBUF_SHARED_LIBRARY_DIR ${ASCEND_PROTOBUF_SHARED_DIR}/lib)
+
+    if(NOT TARGET ascend_protobuf)
+        add_library(ascend_protobuf SHARED IMPORTED)
+        set_target_properties(ascend_protobuf PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${ASCEND_PROTOBUF_SHARED_INCLUDE}"
+            IMPORTED_LOCATION             "${ASCEND_PROTOBUF_SHARED_LIBRARY}"
+            )
+    endif()
+endif()
