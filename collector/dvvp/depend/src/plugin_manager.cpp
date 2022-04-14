d88c818e959f73b82b4d3c322055802e04648b04
yu-liang-bin <y1416490440@163.com>
2022-04-14 10:30:13 +0800

    [CHG]get so path by envValue
    Match-id-2f39a6e4e64b7bdb1c022a51d997727151213997

--- collector/dvvp/depend/src/plugin_manager.cpp
+++ collector/dvvp/depend/src/plugin_manager.cpp
@@ -25,13 +25,18 @@ std::string PluginManager::RealPath(const std::string &path) const
     return res;
 }
 
-Status PluginManager::OpenPlugin(const std::string& path)
+Status PluginManager::OpenPlugin(const std::string envValue)
 {
-    if (path.empty() || path.size() >= MAX_PATH_LENGTH) {
-        MSPROF_LOGE("[OpenPlugin]so path wrong");
+    if (envValue.empty() || envValue.size() >= MAX_PATH_LENGTH) {
+        MSPROF_LOGE("[OpenPlugin]envValue wrong");
         return PLUGIN_LOAD_FAILED;
     }
-    std::string absoluteDir = RealPath(path);
+    std::string soPath = GetSoPath(envValue);
+    if (soPath.empty()) {
+        MSPROF_LOGE("[OpenPlugin]Get so path failed");
+        return PLUGIN_LOAD_FAILED;
+    }
+    std::string absoluteDir = RealPath(soPath);
     if (absoluteDir.empty()) {
         MSPROF_LOGE("[OpenPlugin]Get realpath failed.");
         return PLUGIN_LOAD_FAILED;
@@ -68,6 +73,36 @@ bool PluginManager::HasLoad()
     return load_;
 }
 
+void PluginManager::SplitPath(const std::string &mutil_path, std::vector<std::string> &path_vec) const
+{
+    const std::string tmp_string = mutil_path + ":";
+    std::string::size_type start_pos = 0U;
+    std::string::size_type cur_pos = tmp_string.find(':', 0U);
+    while (cur_pos != std::string::npos) {
+        const std::string path = tmp_string.substr(start_pos, cur_pos - start_pos);
+        if (!path.empty()) {
+        path_vec.push_back(path);
+        }
+        start_pos = cur_pos + 1U;
+        cur_pos = tmp_string.find(':', start_pos);
+    }
+}
+
+std::string PluginManager::GetSoPath(const std::string &envValue) const
+{
+    char path_env[MAX_PATH_LENGTH]{};
+    const char *env = getenv(envValue.c_str());
+    strncpy(path_env, env, MAX_PATH_LENGTH);
+    std::vector<std::string> path_vec;
+    SplitPath(std::string(path_env), path_vec);
+    for (auto path : path_vec) {
+        std::string ret = path + "/" + so_name_;
+        if (access(ret.c_str(), F_OK) != -1) {
+            return ret;
+        }
+    }
+    return "";
+}
 
 } // namespace Plugin
 } // namespace Dvvp
