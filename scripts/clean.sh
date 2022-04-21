46a21a85e57a2801fc20400b94db0f012fdfba86
yu-liang-bin <y1416490440@163.com>
2022-04-21 20:20:02 +0800

    [BUGFIX]check
    Match-id-4ea74ae03ae9eb3b40305737d1d35c705b49a4c1

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -11,6 +11,6 @@ rm -rf ${TOP_DIR}/opensource/json
 rm -rf ${TOP_DIR}/opensource/securec
 rm -rf ${TOP_DIR}/llt/opensource/googletest
 rm -rf ${TOP_DIR}/llt/opensource/mockcpp/install
-rm -rf ${TOP_DIR}/llt/build
+rm -rf ${TOP_DIR}/llt/build_llt
 rm -rf ${TOP_DIR}/llt/output
 rm -rf ${TOP_DIR}/output
