c9306b8914d36f3233adc01dcaf139f70a15584a
msprofDeveloper <msprofDeveloper>
2022-04-25 18:51:18 +0800

    [SCH]Henerate Converage For CPP Code.
    Match-id-351c5478e8c39b2f3a859de9e2e131c7aa6464b7

--- scripts/GenerateCoverageCpp.sh
+++ scripts/GenerateCoverageCpp.sh
@@ -1,25 +1,52 @@
-#!/bin/bash
+#/bin/bash
 
+# 82.0%
 set -e
 CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=${CUR_DIR}/..
-COV_DIR=${TOP_DIR}/llt/output
-cd ${COV_DIR}
-mkdir coverage
+COV_DIR=${TOP_DIR}/llt/output/coverage
+BUILD_DIR=${TOP_DIR}/llt/build_llt
 
-#
-#
-#
-#
+if [ ! -d ${COV_DIR} ] ; then
+    mkdir ${COV_DIR}
+fi
 
-echo "***************Generate Coverage*****************"
-lcov -c -d ./build/CMakeFiles/testcode.dir -o ./coverage/lcov_arm1.info -b ./coverage --rc lcov_branch_coverage=1
-lcov -r ./coverage/lcov_arm1.info '*c++*' -o ./coverage/lcov_arm1.info --rc lcov_branch_coverage=1
-lcov -r ./coverage/lcov_arm1.info '*opensource*' -o ./coverage/lcov_arm1.info --rc lcov_branch_coverage=1
-lcov -r ./coverage/lcov_arm1.info '*pb_out*' -o ./coverage/lcov_arm1.info --rc lcov_branch_coverage=1
-lcov -r ./coverage/lcov_arm1.info '*testcode*' -o ./coverage/lcov_arm1.info --rc lcov_branch_coverage=1
-lcov -r ./coverage/lcov_arm1.info '*platform*' -o ./coverage/lcov_arm1.info --rc lcov_branch_coverage=1
-genhtml ./coverage/lcov_arm1.info -o ./coverage/html1 --branch-coverage
+#----------------------------------------------------------
+# coverage function
+generate_coverage(){
+    echo "********************** Generate $2 Coverage Start.************************"
+    lcov -c -d $1 -o ${COV_DIR}/lcov_$2.info --rc lcov_branch_coverage=1
+    lcov -r ${COV_DIR}/lcov_$2.info '*c++*' -o ${COV_DIR}/lcov_$2.info --rc lcov_branch_coverage=1
+    lcov -r ${COV_DIR}/lcov_$2.info '*msprof_llt*' -o ${COV_DIR}/lcov_$2.info --rc lcov_branch_coverage=1
+    lcov -r ${COV_DIR}/lcov_$2.info '*msprof_cpp*' -o ${COV_DIR}/lcov_$2.info --rc lcov_branch_coverage=1
+    lcov -r ${COV_DIR}/lcov_$2.info '*ascend_protobuf*' -o ${COV_DIR}/lcov_$2.info --rc lcov_branch_coverage=1
+    lcov -r ${COV_DIR}/lcov_$2.info '*gtest*' -o ${COV_DIR}/lcov_$2.info --rc lcov_branch_coverage=1
+    lcov -r ${COV_DIR}/lcov_$2.info '*opensource*' -o ${COV_DIR}/lcov_$2.info --rc lcov_branch_coverage=1
+    echo "********************** Generate $2 Coverage Stop.*************************"
+}
+#----------------------------------------------------------
+test_obj=(
+    device_utest
+    job_wrapper_utest
+    common_utest
+    msprof_bin_utest
+    streamio_common_utest
+    client_utest
+    application_utest
+    message_utest
+    msprofiler_utest
+    msprof_utest
+    transport_utest
+)
 
-tar -zcvf ./coverage/html1.tar.gz ./coverage/html1
-cp ./build/test_detail.xml ./coverage/test_detail.xml
+str_test=""
+for test in ${test_obj[@]} ; do
+    str_test=${str_test}"-a ${COV_DIR}/lcov_${test}.info "
+    test_dir=`find ${BUILD_DIR} -name "${test}.dir"`
+    target_dir=`ls -F ${test_dir} | grep "/$" | grep -v "test" | grep -v "__"`
+    generate_coverage ${test_dir}/${target_dir} ${test}
+done
+
+echo "${str_test}"
+lcov ${str_test} -o ${COV_DIR}/ut_report.info
+genhtml ${COV_DIR}/ut_report.info -o ${COV_DIR}/result --branch-coverage
