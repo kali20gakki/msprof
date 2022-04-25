599af6151071066d53ed1c960bb81de4ab3980ef
yu-liang-bin <y1416490440@163.com>
2022-04-25 16:36:55 +0800

    [SCH]Coverage Test.
    Match-id-f1a8d4f0b4e9cc76e8141cab518e114c34abe6c4

--- scripts/ExecuteTestCase.sh
+++ scripts/ExecuteTestCase.sh
@@ -6,6 +6,6 @@ CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=${CUR_DIR}/..
 mkdir -p ${TOP_DIR}/llt/build_llt
 cd ${TOP_DIR}/llt/build_llt
-cmake ../ -DPACKAGE=st
+cmake ../ -DPACKAGE=ut
 make -j64
 
