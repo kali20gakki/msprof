0c0a22766569258a2948cbad0e70bc8d61e8ae1c
yu-liang-bin <y1416490440@163.com>
2022-04-28 18:34:03 +0800

    [DEL]Delete LLT Code.
    Match-id-975b56ce2be4448ccd6b68f280f2da41d3d5c3c8

--- llt/msprof_cpp/ut/stub/ide-daemon-sock-stub.cpp
+++
@@ -1,5 +0,0 @@
-#include "ide-daemon-sock-stub.h"
-int Putpkt(struct IdeSockHandle sock, const char *send_buf, unsigned int cnt) {
-    return 0;
-}
-
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,10 @@
 set -e
 CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=${CUR_DIR}/..
-mkdir -p ${TOP_DIR}/build_test
-cd ${TOP_DIR}/build_test
+if [! -d ${TOP_DIR}/build ] ; then
+    mkdir -p  ${TOP_DIR}/build
+fi
+cd ${TOP_DIR}/build
 cmake ../cmake/superbuild/
 make -j64
 
