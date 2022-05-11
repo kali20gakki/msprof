c0d0c26b94c6263a37c5ac659564584ac6a0c957
msprofDeveloper <msprofDeveloper>
2022-05-11 19:12:53 +0800

    add ut 6
    Match-id-37d384f7bfb140b284cc0b73e10dcb6e4b30666c

--- scripts/execute_test_case.sh
+++ scripts/execute_test_case.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 set -e
-
 CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=${CUR_DIR}/..
-echo "XXX0" ${TOP_DIR}
 mkdir -p ${TOP_DIR}/test/llt/build_llt
 cd ${TOP_DIR}/test/llt/build_llt
 cmake ../ -DPACKAGE=ut
