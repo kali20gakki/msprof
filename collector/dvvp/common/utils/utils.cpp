60c2cc04a4b9f3d5c2bc6a3896bb40a36c03a3c7
msprofDeveloper <msprofDeveloper>
2022-05-19 16:28:54 +0800

    fix sc
    Match-id-a9ea5e8e412d5242d2376fbb6550ed09ff779a7f

--- collector/dvvp/common/utils/utils.cpp
+++ collector/dvvp/common/utils/utils.cpp
@@ -1234,8 +1234,7 @@ uint32_t Utils::GenerateSignature(CONST_UINT8_PTR data, uint64_t len)
     };
 
     static const int offset = 8;
-    while (len != 0 ) {
-        len -= 1;
+    for (uint32_t i = 0; i < len; ++i) {
         signature = signatureTable[(signature ^ *data++) & 0xff] ^ (signature >> offset);
     }
     return signature;
