9dbe65075d7d8c9c60a7d8521445293c152e2d79
msprofDeveloper <msprofDeveloper>
2022-05-19 16:06:45 +0800

    DTS:DTS2022051907507 Description:Add Debug and Release type.
    Team:HISI_TOOLS
    Feature or Bugfix:Bugfix
    
    Match-id-731cd8f1450a812fb2a6af6781bb698291665c20

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,17 @@
 #/bin/bash
+# This script is used to build msprofbin&&libmsprofiler.so&&stub/libmsprofiler.so
+# Copyright Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
 
-# msprofbin libmsprofiler.so stub/libmsprofiler.so
 set -e
 CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=${CUR_DIR}/..
-if [ ! -d ${TOP_DIR}/build ]; then
-    mkdir -p  ${TOP_DIR}/build
+BUILD_TYPE="Release"
+if [ $# = 1 ] && [ "$1" = "Debug" ];
+then
+    BUILD_TYPE="Debug"
 fi
 cd ${TOP_DIR}/build
-cmake ../cmake/superbuild/
+cmake ../cmake/superbuild/ -DMSPROF_BUILD_TYPE=${BUILD_TYPE}
 make -j64
 
 bash ${TOP_DIR}/scripts/create_run_package.sh ${1}
