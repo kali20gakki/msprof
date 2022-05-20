ec1d24f48b88c3b9f35d30389127fb95694790bc
msprofDeveloper <msprofDeveloper>
2022-05-20 18:49:37 +0800

    编译选项优化
    Match-id-d68dafe71679fb1b8a2b937c2ee49f0d7a9c446f

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,29 @@
 
 set -e
 CUR_DIR=$(dirname $(readlink -f $0))
+TOP_DIR=${CUR_DIR}/..
+VERSION=""
+BUILD_TYPE=""
 
+# input param check
+if [ $# != 0 ] && [ $# != 1 ];
+    then
+        echo "[ERROR]Please check input params. For example:"
+        echo "./build.sh           # offline (Release)"
+        echo "./build.sh Debug     # offline (Debug)"
+        echo "./build.sh [version] # online (Release)"    
+        exit
+fi
+
+if [ $# = 1 ] && [ "$1" = "Debug" ];
+    then
+        BUILD_TYPE="Debug"
+elif [ $# = 1 ] && [ "$1" != "Debug" ];
+    then
+        VERSION=$1
+fi
+
+# binary check
 function bep_env_init() {
     source /etc/profile
     local bep_env_config=${CUR_DIR}/bep/bep_env.conf
@@ -24,17 +46,6 @@ function bep_env_init() {
 }
 
 bep_env_init
-
-TOP_DIR=${CUR_DIR}/..
-VERSION=""
-BUILD_TYPE=""
-if [ $# = 1 ] && [ "$1" = "Debug" ];
-    then
-        BUILD_TYPE="Debug"
-elif [ $# = 1 ] && [ "$1" != "Debug" ];
-    then
-        VERSION=$1
-fi
 
 cd ${TOP_DIR}/build
 cmake ../cmake/superbuild/ -DMSPROF_BUILD_TYPE=${BUILD_TYPE}
