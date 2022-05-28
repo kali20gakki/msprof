f99e7b5eba3ed3c1554f785568a2b72d918e592b
msprofDeveloper <msprofDeveloper>
2022-05-28 18:33:23 +0800

    patch package
    Match-id-b9b276d5a356f7379175b05d5cdf7081554c4f45

--- scripts/create_run_package.sh
+++ scripts/create_run_package.sh
@@ -6,7 +6,9 @@ TOP_DIR=${CUR_DIR}/..
 # store product
 TEMP_OUTPUT=${TOP_DIR}/build/output
 MSPROF_TEMP_DIR=${TOP_DIR}/build/msprof_tmp
+COMMON_DIR="common_script"
 mkdir ${MSPROF_TEMP_DIR}
+mkdir ${MSPROF_TEMP_DIR}/${COMMON_DIR}
 
 # makeself is tool for compiling run package
 MAKESELF_DIR=${TOP_DIR}/opensource/makeself
@@ -23,9 +25,21 @@ mkdir ${OUTPUT_DIR}
 
 RUN_SCRIPT_DIR=${TOP_DIR}/scripts/run_script
 FILTER_PARAM_SCRIPT=${RUN_SCRIPT_DIR}/help.sh
-MAIN_SCRIPT=/main.sh
-MSPROF_RUN_NAME="msprof"
+MAIN_SCRIPT=main.sh
+
+# script for spc
+MAIN_SPC=main_spc.sh
+FILTER_PARAM_SCRIPT_SPC=${RUN_SCRIPT_DIR}/help_spc.sh
+BACKUP=backup.sh
+ROLLBACK_PRECHECK=rollback_precheck.sh
+ROLLBACK=rollback.sh
+UNINSTALL=uninstall.sh
+COMMON_ROLLBACK=rollback.sh
+COMMON_UNINSTALL=uninstall.sh
+
+MSPROF_RUN_NAME="mindstudio-msprof"
 version="unknwon"
+package_type="unknwon"
 
 function parse_script_args() {
     while true; do
@@ -35,6 +49,11 @@ function parse_script_args() {
         case "$1" in
         version_dir=*)
 			version=${1#version_dir=}
+			shift
+			continue
+            ;;
+        Patch)
+			package_type="Patch"
 			shift
 			continue
             ;;
@@ -50,13 +69,35 @@ function parse_script_args() {
 function create_temp_dir() {
 	local temp_dir=${1}
 	
-	cp ${TEMP_OUTPUT}/lib/libmsprofiler.so ${temp_dir}
-	cp -r ${TEMP_OUTPUT}/stub ${temp_dir}
-	cp ${TEMP_OUTPUT}/bin/msprof ${temp_dir}
-	cp -r ${TOP_DIR}/analysis ${temp_dir}
-	
-	cp ${RUN_SCRIPT_DIR}${MAIN_SCRIPT} ${temp_dir}
-	chmod 500 ${temp_dir}${MAIN_SCRIPT}
+	if [ "${package_type}" = "Patch" ];
+		then
+			cp ${TEMP_OUTPUT}/lib/libmsprofiler.so ${temp_dir}
+
+			copy_script ${MAIN_SPC} ${temp_dir}
+			copy_script ${BACKUP} ${temp_dir}
+			copy_script ${ROLLBACK_PRECHECK} ${temp_dir}
+			copy_script ${ROLLBACK} ${temp_dir}
+			copy_script ${UNINSTALL} ${temp_dir}
+			copy_script ${COMMON_DIR}/${COMMON_ROLLBACK} ${temp_dir}
+			copy_script ${COMMON_DIR}/${COMMON_UNINSTALL} ${temp_dir}
+
+		else
+			cp ${TEMP_OUTPUT}/lib/libmsprofiler.so ${temp_dir}
+			cp -r ${TEMP_OUTPUT}/stub ${temp_dir}
+			cp ${TEMP_OUTPUT}/bin/msprof ${temp_dir}
+			cp -r ${TOP_DIR}/analysis ${temp_dir}
+	fi
+
+	copy_script ${MAIN_SCRIPT} ${temp_dir}
+}
+
+# copy script
+function copy_script() {
+	local script_name=${1}
+	local temp_dir=${2}
+
+	cp ${RUN_SCRIPT_DIR}/${script_name} ${temp_dir}/${script_name}
+	chmod 500 ${temp_dir}/${script_name}
 }
 
 function version() {
@@ -67,21 +108,23 @@ function version() {
 
 function get_package_name() {
     local _product="Ascend"
-    local _name="mindstudio-msprof"
+    local _name=${MSPROF_RUN_NAME}
 
     local _version=$(echo $(version) | cut -d '.' -f 1,2,3)
     local _os_arch=$(arch)
     echo "${_product}-${_name}_${_version}_linux-${_os_arch}.run"
 }
 
-function create_run_package(){
+function create_run_package() {
 	local _run_name=${1}
 	local _temp_dir=${2}
+	local _main_script=${3}
+	local _filer_param=${4}
 	local _package_name=$(get_package_name)
 	
 	${CREATE_RUN_SCRIPT} \
 	--header ${CONTROL_PARAM_SCRIPT} \
-	--help-header ${FILTER_PARAM_SCRIPT} --pigz \
+	--help-header ${_filer_param} --pigz \
 	--complevel 4 \
 	--nomd5 \
 	--sha256 \
@@ -89,13 +132,33 @@ function create_run_package(){
 	${_temp_dir} \
 	${OUTPUT_DIR}/${_package_name} \
 	${_run_name} \
-	.${MAIN_SCRIPT}
+	./${_main_script}
+}
+
+function sed_param() {
+	local _main_script=${1}
+	sed -i "2i VERSION=$version" "${RUN_SCRIPT_DIR}/${_main_script}"
+	sed -i "2i package_arch=$(arch)" "${RUN_SCRIPT_DIR}/${_main_script}"
+}
+
+function delete_sed_param() {
+	local _main_script=${1}
+	sed -i "2d" "${RUN_SCRIPT_DIR}/${_main_script}"
+	sed -i "2d" "${RUN_SCRIPT_DIR}/${_main_script}"
 }
 
 parse_script_args $*
-sed -i "2i VERSION=$version" ${RUN_SCRIPT_DIR}${MAIN_SCRIPT}
-sed -i "2i package_arch=$(arch)" "${RUN_SCRIPT_DIR}/${MAIN_SCRIPT}"
+
+sed_param ${MAIN_SCRIPT}
+sed_param ${MAIN_SPC}
+
 create_temp_dir ${MSPROF_TEMP_DIR}
-create_run_package ${MSPROF_RUN_NAME} ${MSPROF_TEMP_DIR}
-sed -i '2d' ${RUN_SCRIPT_DIR}${MAIN_SCRIPT}
-sed -i '2d' "${RUN_SCRIPT_DIR}/${MAIN_SCRIPT}"
+if [ "${package_type}" = "Patch" ];
+	then
+		create_run_package ${MSPROF_RUN_NAME} ${MSPROF_TEMP_DIR} ${MAIN_SPC} ${FILTER_PARAM_SCRIPT_SPC}
+	else
+		create_run_package ${MSPROF_RUN_NAME} ${MSPROF_TEMP_DIR} ${MAIN_SCRIPT} ${FILTER_PARAM_SCRIPT}
+fi
+
+delete_sed_param ${MAIN_SCRIPT}
+delete_sed_param ${MAIN_SPC}
---
+++ scripts/run_script/uninstall.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+function del_file() {
+    file_path=$1
+
+    if [ -f "${file_path}" ]; then
+        rm -f "${file_path}"
+        if [ $? = 0 ]; then
+            print "INFO" "delete file ${file_path} successfully"
+        else
+            print "ERROR" "delete file ${file_path} fail"
+            exit 1
+        fi
+    else
+        print "WARNING" "the file ${file_path} is not exist"
+    fi
+}
+
+function del_dir() {
+    local dir_path=$1
+
+    if [ -n "${dir_path}" ] && [[ ! "${dir_path}" =~ ^/+$ ]]; then
+        if [ -d "${dir_path}" ]; then
+            chmod 750 -R ${dir_path}
+            rm -rf "${dir_path}"
+            if [ $? = 0 ]; then
+                print "INFO" "delete directory ${dir_path} successfully"
+            else
+                print "ERROR" "delete directory ${dir_path} fail"
+                exit 1
+            fi
+        else
+            print "WARNING" "the directory ${dir_path} is not exist"
+        fi
+    else
+        print "WARNING" "the directory ${dir_path} path is NULL"
+    fi
+}
+
+function remove_empty_dir() {
+    [ ! -d "$1" ] && return 1
+    if [ -z "$(ls -A $1 2>&1)" ]; then
+        rm -rf "$1"
+        if [ $? = 0 ]; then
+            print "INFO" "delete directory $1 successfully"
+        else
+            print "ERROR" "delete directory $1 fail"
+            exit 1
+        fi
+    fi
+}
+
+is_dir_empty() {
+    [ ! -d "$1" ] && return 1
+    [ "$(ls -A "$1" 2>&1)" != "" ] && return 2
+    return 0
+}
+
+remove_dir_if_empty() {
+    local dirpath="$1"
+    is_dir_empty "${dirpath}"
+    if [ $? -eq 0 ]; then
+        rm -rf "${dirpath}"
+    fi
+    return 0
+}
+
+function deal_uninstall() {
+    # delete product
+    del_file ${install_path}/${SPC_DIR}/${BACKUP_DIR}/${MSPROF_RUN_NAME}/${LIBMSPROFILER_PATH}/${LIBMSPROFILER}
+
+    # delete backup
+    del_dir ${install_path}/${SPC_DIR}/${BACKUP_DIR}/${MSPROF_RUN_NAME}
+    remove_empty_dir ${install_path}/${SPC_DIR}/${BACKUP_DIR}/${MSPROF_RUN_NAME}
+    remove_dir_if_empty ${install_path}/${SPC_DIR}/${BACKUP_DIR}
+    print "INFO" "${MSPROF_RUN_NAME} uninstalled successfully, the directory ${SPC_DIR}/${BACKUP_DIR}/${MSPROF_RUN_NAME} has been deleted"
+
+    # delete script
+    del_dir ${install_path}/${SPC_DIR}/${SCRIPT_DIR}/${MSPROF_RUN_NAME}
+    tree ${install_path}/${SPC_DIR}
+    remove_empty_dir ${install_path}/${SPC_DIR}/${SCRIPT_DIR}/${MSPROF_RUN_NAME}
+    print "INFO" "${MSPROF_RUN_NAME} uninstalled successfully, the directory ${SPC_DIR}/${SCRIPT_DIR}/${MSPROF_RUN_NAME} has been deleted"
+}
+
+function print() {
+    if [ ! -f "$log_file" ]; then
+        echo "[${MSPROF_RUN_NAME}] [$(date +"%Y-%m-%d %H:%M:%S")] [$1]: $2"
+    else
+        echo "[${MSPROF_RUN_NAME}] [$(date +"%Y-%m-%d %H:%M:%S")] [$1]: $2" | tee -a $log_file
+    fi
+}
+
+function get_log_file() {
+	local log_dir
+	if [ "$UID" = "0" ]; then
+		log_dir="/var/log/ascend_seclog"
+	else
+		log_dir="${HOME}/var/log/ascend_seclog"
+	fi
+	echo "${log_dir}/ascend_install.log"
+}
+
+function log_init() {
+    if [ ! -f "$log_file" ]; then
+        touch $log_file
+        if [ $? -ne 0 ]; then
+            print "ERROR" "touch $log_file permission denied"
+            exit 1
+        fi
+    fi
+    chmod 640 $log_file
+}
+
+# init log file
+log_file=$(get_log_file)
+log_init
+
+# spc dir
+SPC_DIR="spc"
+BACKUP_DIR="backup"
+SCRIPT_DIR="script"
+MSPROF_RUN_NAME="mindstudio-msprof"
+
+# product
+LIBMSPROFILER_PATH="/runtime/lib64/"
+LIBMSPROFILER="libmsprofiler.so"
+
+# get install path
+install_path="$(
+    cd "$(dirname "$0")/../../../"
+    pwd
+)"
+
+# script for spc
+UNINSTALL_SCRIPT="uninstall.sh"
+
+deal_uninstall
