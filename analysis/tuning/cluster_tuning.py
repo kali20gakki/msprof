fd5fa308897acc66f55232db2ac3f124318d7b50
yu-liang-bin <y1416490440@163.com>
2022-06-01 15:12:07 +0800

    PROFs analyse
    Match-id-4ccaead21787c18cd9ae5b5a2c56edecfa249257

--- analysis/tuning/cluster_tuning.py
+++ analysis/tuning/cluster_tuning.py
@@ -3,7 +3,7 @@
 """
 Copyright Huawei Technologies Co., Ltd. 2020. All rights reserved.
 """
-
+from common_func.common_prof_rule import CommonProfRule
 from mscalculate.trailing_calculator import TrailingCalculator
 
 
@@ -13,16 +13,45 @@ class ClusterTuning:
     """
 
     def __init__(self: any) -> None:
-        self.calculate_list = [TrailingCalculator]
-        self.tuning_result = {}
+        self.calculate_list = {TrailingCalculator: 'Slow node'}
+        self.tuning_result = []
 
     def run(self: any, cluster_params: list) -> None:
         """
         run and recommend
         """
-        for calculator in self.calculate_list:
-            self.tuning_result[calculator] = calculator(cluster_params).run()
+        for calculator, value in self.calculate_list.items():
+            calculator_result = {'rule_type': value, 'result': calculator(cluster_params).ms_run()}
+            self.tuning_result.append(calculator_result)
         self.show_tuning_result()
 
     def show_tuning_result(self: any) -> None:
-        pass
+        print("\nThe current data is identified as cluster data.")
+        print("Cluster Tuning Report:")
+        for index, every_data in enumerate(self.tuning_result):
+            self.print_first_level(index + 1, every_data)
+            self.print_second_level(every_data.get("result"))
+        print("\n")
+
+    @staticmethod
+    def print_first_level(index: any, data: dict) -> None:
+        """
+        print title of first level
+        :param index: index
+        :param data: data
+        :return: None
+        """
+        if data and data.get(CommonProfRule.RESULT_RULE_TYPE):
+            print("{0}. {1}:".format(index, data.get(CommonProfRule.RESULT_RULE_TYPE)))
+
+    @staticmethod
+    def print_second_level(data: any) -> None:
+        """
+        :param data: data
+        :return: None
+        """
+        if not data:
+            print("\tN/A")
+            return
+        for result_index, result in enumerate(data):
+            print("\t{0}){1}:".format(result_index + 1, result))
