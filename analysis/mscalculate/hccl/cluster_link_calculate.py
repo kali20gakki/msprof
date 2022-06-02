a84b225dc3442f3f96f4af843f6ae906a6d35fac
yu-liang-bin <y1416490440@163.com>
2022-06-02 16:03:27 +0800

    slow link
    Match-id-ebce6a7ddbd893002a1fa6ebfbef4f4204ed12ab

--- analysis/mscalculate/hccl/cluster_link_calculate.py
+++ analysis/mscalculate/hccl/cluster_link_calculate.py
@@ -4,42 +4,54 @@
 function: this script used to calculate cluster link from hccl.
 Copyright Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
 """
-import os
-from multiprocessing import Manager
 
+from multiprocessing import Manager
 from common_func.constant import Constant
 from common_func.db_name_constant import DBNameConstant
 from common_func.ms_constant.number_constant import NumberConstant
-from common_func.ms_constant.str_constant import StrConstant
 from common_func.ms_multi_process import MsMultiProcess
 from model.hccl.hccl_model import HCCLModel
-from mscalculate.interface.icalculator import ICalculator
-from profiling_bean.prof_enum.data_tag import DataTag
 
 
-class ClusterLinkCalculator(MsMultiProcess):
+class ClusterLinkCalculator:
     """
     class used to calculate ge hash info
     """
     THRESHOLD_VALUE = 0.2
     LINK_TYPE_LIST = [Constant.TYPE_SDMA, Constant.TYPE_RDMA]
 
-    def __init__(self: any, file_list: list, sample_config: dict) -> None:
-        super().__init__(sample_config)
-        self._sample_config = sample_config
+    def __init__(self: any, file_list: list) -> None:
         self._file_list = file_list
         self.slow_link_list = []
+        self.current_dict = {}
         self.result_dict = {}
+
+    def format_link_data(self: any) -> dict:
+        """
+        save hccl data into database
+        :return: None
+        """
+        for link_type in self.current_dict.keys():
+            type_tuning = "{0}: link type and percent(link bandwidth/bandwidth average)".format(link_type)
+            self.result_dict.setdefault(type_tuning, self.current_dict.get(link_type))
+
+        return self.result_dict
 
     def get_slow_dict(self: any) -> None:
         """
         save hccl data into database
         :return: None
         """
-
+        link_dict = {}
+        max_value = 0
         for link_data in self.slow_link_list:
             for type_link_data in link_data.items():
-                self.result_dict.setdefault(type_link_data[0], []).append(type_link_data[1])
+                if type_link_data[1]:
+                    for value in type_link_data[1]:
+                        key = "{0}-{1}".format(value[1], value[2])
+                        max_value = max(max_value, value[3])
+                        link_dict.update({key: max_value})
+                    self.current_dict.setdefault(type_link_data[0], []).extend(list(link_dict.items()))
 
     def get_single_slow_link_list(self: any) -> None:
         """
@@ -49,7 +61,7 @@ class ClusterLinkCalculator(MsMultiProcess):
         with Manager() as manager:
             data_list = manager.list()
             for _file_path in self._file_list:
-                parsing_obj.append(ClusterSingleLinkCalculator(_file_path, self._sample_config, data_list))
+                parsing_obj.append(ClusterSingleLinkCalculator(_file_path, {'result_dir': _file_path}, data_list))
             # start parsing processor
             for item in parsing_obj:
                 item.start()
@@ -57,22 +69,23 @@ class ClusterLinkCalculator(MsMultiProcess):
             # join parsing processor
             for item in parsing_obj:
                 item.join()
-            self.slow_link_list = data_list.copy()
+            self.slow_link_list = data_list._getvalue()
 
-    def ms_run(self: any) -> None:
+    def run(self: any) -> dict:
         """
         entrance for calculating ge
         :return: None
         """
         self.get_single_slow_link_list()
+        self.get_slow_dict()
+        return self.format_link_data()
 
 
-# class ClusterLinkCalculator(ICalculator, MsMultiProcess):
 class ClusterSingleLinkCalculator(MsMultiProcess):
     """
         class used to calculate ge hash info
         """
-    THRESHOLD_VALUE = 0.2
+    THRESHOLD_VALUE = 20
 
     # def __init__(self: any, project_path: dict, sample_config: dict) -> None:
     def __init__(self: any, project_path: str, sample_config: dict, data_list: list) -> None:
@@ -94,16 +107,20 @@ class ClusterSingleLinkCalculator(MsMultiProcess):
         :return:
         """
         cluster_link_list = []
-        for link_type_data in self.link_dict.get(link_type, []):
-            link_bw = round(
-                abs(link_type_data[0] - self.average_data.get(link_type)) / self.average_data.get(link_type),
-                NumberConstant.ROUND_TWO_DECIMAL)
-            if link_bw >= self.THRESHOLD_VALUE:
-                link_type_data.extend([link_bw])
-                cluster_link_list.append(link_type_data)
-        return cluster_link_list
+        try:
+            for link_type_data in self.link_dict.get(link_type, []):
+                link_bw = round(
+                    abs(link_type_data[0] - self.average_data.get(link_type)) / self.average_data.get(link_type)
+                    * NumberConstant.PERCENTAGE,
+                    NumberConstant.ROUND_TWO_DECIMAL)
+                if link_bw >= self.THRESHOLD_VALUE:
+                    link_type_data.extend([link_bw])
+                    cluster_link_list.append(link_type_data)
+            return cluster_link_list
+        except ZeroDivisionError:
+            return cluster_link_list
 
-    def get_cluster_link_data(self: any) -> None:
+    def get_slow_link_data(self: any) -> None:
         """
         get cluster link data
         :return: None
@@ -118,18 +135,20 @@ class ClusterSingleLinkCalculator(MsMultiProcess):
             self.average_data.setdefault(Constant.TYPE_SDMA, sdma_average)
         rdma_list = self.calculate_cluster_link_list(Constant.TYPE_RDMA)
         sdma_list = self.calculate_cluster_link_list(Constant.TYPE_SDMA)
-        self.result_dict.setdefault(Constant.TYPE_RDMA, rdma_list)
-        self.result_dict.setdefault(Constant.TYPE_SDMA, sdma_list)
+        if rdma_list:
+            self.result_dict.setdefault(Constant.TYPE_RDMA, rdma_list)
+        if sdma_list:
+            self.result_dict.setdefault(Constant.TYPE_SDMA, sdma_list)
 
-    def get_cluster_link_dict(self: any) -> None:
+    def get_all_link_dict(self: any) -> None:
         """
-        update ge data with hash data
+        get link dict
         :return: None
         """
         for hccl_data in self.hccl_data:
             if hccl_data.bandwidth == Constant.NULL:
                 continue
-            if hccl_data.src_rank == hccl_data.dst_rank:
+            if hccl_data.src_rank == hccl_data.dst_rank or hccl_data.src_rank == Constant.GE_OP_MODEL_ID:
                 continue
             if hccl_data.transport_type == Constant.TYPE_RDMA or hccl_data.transport_type == Constant.TYPE_SDMA:
                 self.link_dict.setdefault(hccl_data.transport_type, []). \
@@ -146,19 +165,12 @@ class ClusterSingleLinkCalculator(MsMultiProcess):
             self.hccl_data = self.model.get_hccl_data()
             if not self.hccl_data:
                 return
-            self.get_cluster_link_dict()
-            self.get_cluster_link_data()
-
-    def save(self: any) -> None:
-        """
-        save hccl data into database
-        :return: None
-        """
-        pass
+            self.get_all_link_dict()
+            self.get_slow_link_data()
 
     def ms_run(self: any) -> None:
         """
-        entrance for calculating ge
+        entrance for calculating cluster link data
         :return: None
         """
         self.calculate()
