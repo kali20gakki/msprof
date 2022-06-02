fe9ed2a205fb34b3d4c79e72358dda1968b6445c
msprofDeveloper <msprofDeveloper>
2022-06-02 20:20:39 +0800

    uT
    Match-id-ca054766b7967595a062297aa899e9bf51112959

--- test/msprof_python/ut/testcase/viewer/test_tuning_view.py
+++ test/msprof_python/ut/testcase/viewer/test_tuning_view.py
@@ -9,22 +9,14 @@ NAMESPACE = 'viewer.tuning_view'
 class TestTuningView(unittest.TestCase):
     def test_show_by_dev_id(self):
         with mock.patch(NAMESPACE + '.TuningView.tuning_report'):
-            TuningView("", {}).show_by_dev_id(0)
+            TuningView("", {}, 0).show_by_dev_id()
 
     def test_load_result_file_1(self):
-        read = json.dumps({"data": "", "status": 1})
-        with mock.patch('os.path.exists', side_effect=FileNotFoundError):
-            res = TuningView("", {})._load_result_file(0)
-        self.assertEqual(res, {})
-
-        with mock.patch('os.path.exists', return_value=False):
-            res = TuningView("", {})._load_result_file(0)
-        self.assertEqual(res, {})
-
-        with mock.patch('os.path.exists', return_value=True), \
-                mock.patch('builtins.open', mock.mock_open(read_data=read)):
-            res = TuningView("", {})._load_result_file(0)
-        self.assertEqual(res, {'data': '', 'status': 1})
+        with mock.patch('builtins.open', side_effect=FileNotFoundError), \
+                mock.patch(NAMESPACE + '.PathManager.get_summary_dir', return_value='test'), \
+                mock.patch('os.path.exists', return_value=True):
+            res = TuningView("", {}, 0).get_tuning_data()
+        self.assertEqual(res, None)
 
     def test_tuning_report(self):
         data = {"data": [{"result": [{'rule_subtype': 'test'}]}], "rule_type": 1}
@@ -32,24 +24,24 @@ class TestTuningView(unittest.TestCase):
                 mock.patch('os.path.exists', return_value=True),\
                 mock.patch('builtins.open', mock.mock_open(read_data=json.dumps(data))):
 
-            TuningView("", {}).tuning_report()
+            TuningView("", {}, 0).tuning_report()
 
         with mock.patch(NAMESPACE + '.TuningView._load_result_file', return_value=data), \
                 mock.patch(NAMESPACE + '.TuningView.print_first_level'), \
                 mock.patch(NAMESPACE + '.TuningView.print_second_level'):
-            TuningView("", {}).tuning_report()
+            TuningView("", {}, 0).tuning_report()
 
     def test_print_first_level(self):
         data = {"rule_type": 1}
-        TuningView("", {}).print_first_level(0, data)
+        TuningView("", {}, 0).print_first_level(0, data)
 
     def test_print_second_level(self):
         data = [{"rule_type": "1", "op_list": "3", "rule_suggestion": "4"}]
         data_sub = [{"rule_type": "1", "rule_subtype": "2", "op_list": "3", "rule_suggestion": "4"},
                     {"rule_type": "1", "rule_subtype": "2", "op_list": "3", "rule_suggestion": "4"}]
-        TuningView("", {}).print_second_level(None)
-        TuningView("", {}).print_second_level(data)
-        TuningView("", {}).print_second_level(data_sub)
+        TuningView("", {}, 0).print_second_level(None)
+        TuningView("", {}, 0).print_second_level(data)
+        TuningView("", {}, 0).print_second_level(data_sub)
 
 
 if __name__ == '__main__':
