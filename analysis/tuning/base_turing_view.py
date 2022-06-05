b8e5fa6bc5818bf0c60ea820f7b7d0ae00a8ac36
yu-liang-bin <y1416490440@163.com>
2022-06-05 18:16:13 +0800

    clean code
    Match-id-d7b0a714db88fb26ef93597efd14606f35da4bb7

--- analysis/tuning/base_turing_view.py
+++ analysis/tuning/base_turing_view.py
@@ -3,12 +3,8 @@
 Copyright Huawei Technologies Co., Ltd. 2020. All rights reserved.
 """
 
-import json
-import logging
-import os
-
+from common_func.common import print_msg
 from common_func.common_prof_rule import CommonProfRule
-from common_func.path_manager import PathManager
 
 
 class BaseTuningView:
@@ -29,7 +25,7 @@ class BaseTuningView:
         :return: None
         """
         if data and data.get(CommonProfRule.RESULT_RULE_TYPE):
-            print("{0}. {1}:".format(index, data.get(CommonProfRule.RESULT_RULE_TYPE)))
+            print_msg("{0}. {1}:".format(index, data.get(CommonProfRule.RESULT_RULE_TYPE)))
 
     @staticmethod
     def print_second_level(data: any) -> None:
@@ -38,7 +34,7 @@ class BaseTuningView:
         :return: None
         """
         if not data:
-            print("\tN/A")
+            print_msg("\tN/A")
             return
         sub_rule_dict = {}
         for result_index, result in enumerate(data):
@@ -46,14 +42,14 @@ class BaseTuningView:
             if rule_sub_type:
                 sub_rule_dict.get(rule_sub_type, []).append(result)
             else:
-                print("\t{0}){2}: [{1}]".format(result_index + 1,
+                print_msg("\t{0}){2}: [{1}]".format(result_index + 1,
                                                 ",".join(list(map(str, result.get(CommonProfRule.RESULT_OP_LIST, [])))),
                                                 result.get(CommonProfRule.RESULT_RULE_SUGGESTION, "")))
         if sub_rule_dict:
             for sub_key_index, sub_key in enumerate(sub_rule_dict.keys()):
-                print("\t{0}){1}:".format(sub_key_index + 1, sub_key))
+                print_msg("\t{0}){1}:".format(sub_key_index + 1, sub_key))
                 for value_index, value in enumerate(sub_rule_dict.get(sub_key)):
-                    print(
+                    print_msg(
                         "\t\t{0}){2}: [{1}]".format(value_index + 1,
                                                     ",".join(value.get(CommonProfRule.RESULT_OP_LIST)),
                                                     value.get(CommonProfRule.RESULT_RULE_SUGGESTION)))
@@ -73,8 +69,8 @@ class BaseTuningView:
         self.get_tuning_data()
         if not self.data:
             return
-        print("\n{0}:".format(self.turing_start))
+        print_msg("\n{0}:".format(self.turing_start))
         for index, every_data in enumerate(self.data):
             self.print_first_level(index + 1, every_data)
             self.print_second_level(every_data.get("result"))
-        print("\n")
+        print_msg("\n")
