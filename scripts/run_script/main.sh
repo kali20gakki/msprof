07a620b962bf15aeb5e2528d2d4928b102866bd7
msprofDeveloper <msprofDeveloper>
2022-06-15 17:19:52 +0800

    upgrade uninstall
    Match-id-ca8e26eb45a3e33eded616c7dba2c6dbe6ec170c

--- scripts/run_script/main.sh
+++ scripts/run_script/main.sh
@@ -1,12 +1,13 @@
+@@ -1,23 +1,23 @@
 #!/bin/bash
 # the params for checking
 install_args_num=0
 install_path_num=0
-
+ 
 uninstall_flag=0
 upgrade_flag=0
 install_for_all_flag=0
-
+ 
 function parse_script_args() {
     while true; do
 		if [ "$3" = "" ]; then
@@ -61,35 +62,48 @@ function parse_script_args() {
         esac
     done
 }
-
+ 
 function check_args() {
 	if [ ${install_args_num} -ne 0 ] && [ ${uninstall_flag} = 1 ]; then
 		print "ERROR" "Input option is invalid. Please try --help."
 		exit 1
 	fi
-
+ 
 	if [ ${install_path_num} -gt 1 ]; then
 		print "ERROR" "Do not input --install-path many times. Please try --help."
 		exit 1
 	fi
 }
-
+ 
 function execute_run() {
 	if [ ${uninstall_flag} = 1 ]; then
-		bash "${install_path}/${MSPROF_RUN_NAME}/script/uninstall.sh"
-		print "INFO" "Mindstudio msprof package uninstall success."
-		exit 0
+		if [ -f "${install_path}/${MSPROF_RUN_NAME}/script/uninstall.sh" ];
+		then
+			bash "${install_path}/${MSPROF_RUN_NAME}/script/uninstall.sh"
+			print "INFO" "${MSPROF_RUN_NAME} package uninstall success."
+			exit 0
+		else
+			print "ERROR" "${MSPROF_RUN_NAME} package uninstall failed."
+			exit 1
+		fi
+		
 	fi
-
+ 
 	if [ ${upgrade_flag} = 1 ] && [ -L "${install_path}/../latest/${MSPROF_RUN_NAME}" ]; then
 		local uninstall_absolute_path=$(readlink -f "${install_path}/../latest/${MSPROF_RUN_NAME}/script/uninstall.sh")
-		bash ${uninstall_absolute_path}
-		print "INFO" "Mindstudio msprof package uninstall success."
+		if [ -f ${uninstall_absolute_path} ];
+		then
+			bash ${uninstall_absolute_path}
+			print "INFO" "${MSPROF_RUN_NAME} package uninstall success."
+		else
+			print "ERROR" "${MSPROF_RUN_NAME} package uninstall failed."
+			exit 1
+		fi
 	fi
-
+ 
 	bash install.sh ${install_path} ${package_arch} ${install_for_all_flag}
 }
-
+ 
 function get_default_install_path() {
 	if [ "$UID" = "0" ]; then
 		echo "/usr/local/Ascend/ascend-toolkit/latest"
@@ -97,39 +111,39 @@ function get_default_install_path() {
 		echo "${HOME}/Ascend/ascend-toolkit/latest"
 	fi
 }
-
+ 
 function store_uninstall_script() {
 	local install_right=500
-
-	if [ -f "${install_path}/${MSPROF_RUN_NAME}/script/uninstall.sh" ]; then
+ 
+	if [ -f "${install_path}/${MSPROF_RUN_NAME}/script/uninstall.sh" ] || [ -f "${install_path}/${MSPROF_RUN_NAME}/script/utils.sh" ]; then
 		return
 	fi
-
+ 
 	mkdir -p "${install_path}/${MSPROF_RUN_NAME}/script/"
 	cp "uninstall.sh" "${install_path}/${MSPROF_RUN_NAME}/script/"
 	cp "utils.sh" "${install_path}/${MSPROF_RUN_NAME}/script/"
-
+ 
 	chmod -R ${install_right} ${install_path}/${MSPROF_RUN_NAME}
 }
-
+ 
 function set_latest() {
 	local latest_path=${install_path}/../latest/
 	remove_latest_link ${latest_path}
 	add_latest_link ${latest_path}
 }
-
+ 
 function remove_latest_link() {
 	local latest_path=$1
     if [ -L "${latest_path}/${MSPROF_RUN_NAME}" ]; then
         rm_file_safe ${latest_path}/${MSPROF_RUN_NAME}
     fi
 }
-
+ 
 function add_latest_link() {
 	local latest_path=$1
     ln -sf ../${VERSION}/${MSPROF_RUN_NAME} ${latest_path}/${MSPROF_RUN_NAME}
 }
-
+ 
 function regist_uninstall() {
     if [ -f "${install_path}/cann_uninstall.sh" ]; then
         write_cann_uninstall
@@ -138,23 +152,24 @@ function regist_uninstall() {
         write_cann_uninstall
     fi
 }
-
+ 
 function write_cann_uninstall() {
     chmod 500 ${install_path}/cann_uninstall.sh
     chmod u+w ${install_path}/cann_uninstall.sh
     sed -i "/^exit /i uninstall_package \"${MSPROF_RUN_NAME}/script\"" "${install_path}/cann_uninstall.sh"
     chmod u-w ${install_path}/cann_uninstall.sh
 }
-
+ 
 # use utils function and constant
 source utils.sh
 install_path=$(get_default_install_path)
-
+ 
 #0, this footnote path;1, path for executing run;2, parents' dir for run package;3, run params
 parse_script_args $*
 check_args
-store_uninstall_script
 execute_run
+store_uninstall_script
+ 
 set_latest
 regist_uninstall
 print "INFO" "${MSPROF_RUN_NAME} package install success."
