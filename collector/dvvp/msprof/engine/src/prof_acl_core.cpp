f4c9aca9dfc749a55c8ae8013582cfd43c7d195e
yu-liang-bin <y1416490440@163.com>
2022-07-20 15:54:23 +0800

    runtime track
    Match-id-ceeb1b462d05599c5f6dff8941a22fc66e374ef0

--- collector/dvvp/msprof/engine/src/prof_acl_core.cpp
+++ collector/dvvp/msprof/engine/src/prof_acl_core.cpp
@@ -340,6 +340,7 @@ aclError aclprofStart(ACL_PROF_CONFIG_CONST_PTR profilerConfig)
     MSPROF_LOGI("Allocate start profiling config to Acl");
     uint64_t dataTypeConfig = profilerConfig->config.dataTypeConfig;
     ProfAclMgr::instance()->AddModelLoadConf(dataTypeConfig);
+    dataTypeConfig |= PROF_RUNTIME_TRACE;
     ret = Analysis::Dvvp::ProfilerCommon::CommandHandleProfStart(
         profilerConfig->config.devIdList, profilerConfig->config.devNums, dataTypeConfig | PROF_OP_DETAIL);
     RETURN_IF_NOT_SUCCESS(ret);
