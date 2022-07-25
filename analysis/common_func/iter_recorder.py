735f8dd183ede0131fa64d6951833b298d347f57
msprofDeveloper <msprofDeveloper>
2022-07-25 22:52:21 +0800

    hwts
    Match-id-b5d6c5056fbf653f07fc70f69aa7e02b4911aef9

--- analysis/common_func/iter_recorder.py
+++ analysis/common_func/iter_recorder.py
@@ -82,7 +82,6 @@ class IterRecorder:
         if not self._op_iter_queue:
             logging.error("Data cannot be found in any op_iteration.")
             raise ProfException(ProfException.PROF_INVALID_DATA_ERROR)
-            return
         if self._op_iter_queue and self._graph_iter_queue and \
                 sys_cnt >= self._graph_iter_dict.get(self._graph_iter_queue[-1])[0]:
             self._current_op_iter = self._op_iter_queue[-1]
