02d14c7500bb7c082c2918db515a6e0a66d4b5ac
msprofDeveloper <msprofDeveloper>
2022-07-30 16:46:43 +0800

    save rankId
    Match-id-be7091a10cb877681548db954a0d36469f9da262

---
+++ collector/dvvp/depend/src/hccl_plugin.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+ * Description: driver interface
+ * Author: Huawei Technologies Co., Ltd.
+ * Create: 2022-04-15
+ */
+#include "hccl_plugin.h"
+
+namespace Collector {
+namespace Dvvp {
+namespace Plugin {
+void HcclPlugin::LoadDriverSo()
+{
+    int32_t ret = PROFILING_SUCCESS;
+    if (!pluginHandle_.HasLoad()) {
+        ret = pluginHandle_.OpenPlugin("LD_LIBRARY_PATH");
+        if (ret != PROFILING_SUCCESS) {
+            return;
+        }
+    }
+}
+
+bool HcclPlugin::IsFuncExist(const std::string &funcName) const
+{
+    return pluginHandle_.IsFuncExist(funcName);
+}
+
+// HcomGetLocalRankId
+int HcclPlugin::MsprofHcomGetLocalRankId(uint32_t *localRankId)
+{
+    PthreadOnce(&loadFlag_, []()->void {HcclPlugin::instance()->LoadDriverSo();});
+    if (hcomGetLocalRankId_ == nullptr) {
+        int32_t ret = pluginHandle_.GetFunction<const char *, uint32_t>("HcomGetLocalRankId", hcomGetLocalRankId_);
+        if (ret != PROFILING_SUCCESS) {
+            return PROFILING_FAILED;
+        }
+    }
+    return hcomGetLocalRankId_(nullptr, localRankId);
+}
+} // Plugin
+} // Dvvp
+} // Collector
