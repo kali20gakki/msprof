3752310b4ef9c278708ddbc0fa8c73b9319f0b39
yu-liang-bin <y1416490440@163.com>
2022-07-30 15:27:14 +0800

    ut-testcase fix
    Match-id-3e138c2c872cdc09302797683bb5b049154bbd11

--- test/msprof_python/ut/testcase/msprof/interface/test_msprof_import.py
+++ test/msprof_python/ut/testcase/msprof/interface/test_msprof_import.py
@@ -1,4 +1,5 @@
 import unittest
+from argparse import Namespace
 from unittest import mock
 
 from msinterface.msprof_import import ImportCommand
@@ -9,23 +10,27 @@ NAMESPACE = 'msinterface.msprof_import'
 class TestImportCommand(unittest.TestCase):
     def test_do_import(self):
         result_dir = '123'
+        args_dic = {"collection_path": "test", "cluster_flag": False}
+        args = Namespace(**args_dic)
         with mock.patch(NAMESPACE + '.ConfigMgr.read_sample_config', return_value=True), \
                 mock.patch(NAMESPACE + '.check_collection_dir'), \
                 mock.patch(NAMESPACE + '.analyze_collect_data'):
-            key = ImportCommand('123')
+            key = ImportCommand(args)
             key.do_import(result_dir)
         with mock.patch(NAMESPACE + '.ConfigMgr.read_sample_config', return_value=True), \
                 mock.patch(NAMESPACE + '.analyze_collect_data'):
-            key = ImportCommand('123')
+            key = ImportCommand(args)
             key.do_import(result_dir)
 
     def test_process(self):
         with mock.patch(NAMESPACE + '.check_path_valid'):
+            args_dic = {"collection_path": "test", "cluster_flag": False}
+            args = Namespace(**args_dic)
             with mock.patch(NAMESPACE + '.DataCheckManager.contain_info_json_data', return_value=True), \
                     mock.patch(NAMESPACE + '.ImportCommand.do_import'), \
                     mock.patch(NAMESPACE + '.LoadInfoManager.load_info'), \
                     mock.patch('os.listdir', return_value=['123']):
-                key = ImportCommand('123')
+                key = ImportCommand(args)
                 key.process()
             with mock.patch(NAMESPACE + '.DataCheckManager.contain_info_json_data', return_value=False), \
                     mock.patch(NAMESPACE + '.LoadInfoManager.load_info'), \
@@ -34,12 +39,12 @@ class TestImportCommand(unittest.TestCase):
                     mock.patch('os.path.realpath', return_value='home\\process'), \
                     mock.patch(NAMESPACE + '.ImportCommand.do_import'), \
                     mock.patch('os.listdir', return_value=['123']):
-                key = ImportCommand('123')
+                key = ImportCommand(args)
                 key.process()
                 with mock.patch(NAMESPACE + '.DataCheckManager.contain_info_json_data', return_value=False), \
                         mock.patch(NAMESPACE + '.warn'), \
                         mock.patch('os.listdir', return_value=['123']):
-                    key = ImportCommand('123')
+                    key = ImportCommand(args)
                     key.process()
 
 
