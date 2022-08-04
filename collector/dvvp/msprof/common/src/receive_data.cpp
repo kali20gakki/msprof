41eea7276ac2aa31b7ae76f35217fb34f08ff1ff
msprofDeveloper <msprofDeveloper>
2022-08-04 16:45:38 +0800

    =修复clean code问题
    Match-id-236602a2044dc3309e0329f8ef347b45141edbd1

--- collector/dvvp/msprof/common/src/receive_data.cpp
+++ collector/dvvp/msprof/common/src/receive_data.cpp
@@ -175,7 +175,7 @@ int ReceiveData::DoReport(CONST_REPORT_DATA_PTR rData)
                         rData->tag, rData->dataLen, RECEIVE_CHUNK_SIZE);
             break;
         }
-        if (moduleName_ == "runtime" && rData->deviceId >= 64) {
+        if (moduleName_ == "runtime" && rData->deviceId >= 64) {  // runtime module, deviceid 64, return
             MSPROF_LOGE("module:%s, invalid device id:%d", moduleName_.c_str(), rData->deviceId);
             return PROFILING_SUCCESS;
         }
