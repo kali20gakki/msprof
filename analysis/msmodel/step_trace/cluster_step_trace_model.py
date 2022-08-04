5da38c83f76522b4d8e629926bcb09d07219849c
yu-liang-bin <y1416490440@163.com>
2022-08-04 11:26:28 +0800

    AR:AR20220705252379 Description:集群汇集迭代轨迹，clean code Team:MSPROF_TOOLS Feature or Bugfix:Feature
    Match-id-51624373704e6c6de424c6b7ec46b5214fa07a57

--- analysis/msmodel/step_trace/cluster_step_trace_model.py
+++ analysis/msmodel/step_trace/cluster_step_trace_model.py
@@ -1,26 +1,36 @@
 # !/usr/bin/python
 # coding=utf-8
 """
+This script used to create step trace database model for cluster.
 Copyright Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
 """
+import os
 
 from common_func.db_manager import DBManager
+from common_func.path_manager import PathManager
 from msmodel.interface.base_model import BaseModel
 
 
 class ClusterStepTraceModel(BaseModel):
+    """
+    step trace model for cluster scene
+    """
+
     FORMAT_TABLE_NAME = "ClusterStepTrace"
 
-    def __init__(self: any, result_dir: str, db_name: str, table_list: list):
+    def __init__(self: any, result_dir: str, db_name: str, table_list: list) -> None:
         super().__init__(result_dir, db_name, table_list)
 
     def create_table(self: any) -> None:
-        """
-        create table
-        """
         table_map = "{0}Map".format(ClusterStepTraceModel.FORMAT_TABLE_NAME)
         for table_name in self.table_list:
             if DBManager.judge_table_exist(self.cur, table_name):
                 DBManager.drop_table(self.conn, table_name)
             sql = DBManager.sql_create_general_table(table_map, table_name, self.TABLES_PATH)
             DBManager.execute_sql(self.conn, sql)
+
+    def init(self: any) -> bool:
+        db_path = PathManager.get_db_path(self.result_dir, self.db_name)
+        if os.path.exists(db_path):
+            os.remove(db_path)
+        return super().init()
--- analysis/msparser/cluster/cluster_step_trace_parser.py
+++ analysis/msparser/cluster/cluster_step_trace_parser.py
@@ -1,18 +1,20 @@
 # !/usr/bin/python
 # coding=utf-8
 """
+This script used to parse step trace data for cluster.
 Copyright Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
 """
-
 import logging
 import os
+from itertools import chain
 
+from common_func.common import error
 from common_func.data_check_manager import DataCheckManager
 from common_func.db_manager import DBManager
 from common_func.db_name_constant import DBNameConstant
 from common_func.info_conf_reader import InfoConfReader
 from common_func.ms_constant.number_constant import NumberConstant
-from common_func.msprof_common import get_path_dir
+from common_func.msprof_common import get_path_dir, MsProfCommonConstant
 from common_func.path_manager import PathManager
 from common_func.step_trace_constant import StepTraceConstant
 from msmodel.step_trace.cluster_step_trace_model import ClusterStepTraceModel
@@ -20,6 +22,9 @@ from msparser.interface.iparser import IParser
 
 
 class ClusterStepTraceParser(IParser):
+    """
+    step trace data parser for cluster scene
+    """
 
     def __init__(self: any, collection_path: str) -> None:
         self.collection_path = collection_path
@@ -27,107 +32,112 @@ class ClusterStepTraceParser(IParser):
         self.id_with_table_map = {}
         self.cluster_model = None
 
-    def ms_run(self):
-        if DBNameConstant.DB_CLUSTER_RANK not in os.listdir(os.path.join(self.collection_path, PathManager.SQLITE)):
-            logging.error("no valid cluster data")
+    def ms_run(self: any) -> None:
+        if not self._check_collection_path_valid():
+            logging.error("The input dir path doesn't have cluster database, please check.")
+            error(MsProfCommonConstant.COMMON_FILE_NAME,
+                  "The input dir path doesn't have cluster database, please check.")
             return
         self.parse()
         self.save()
 
     def parse(self: any) -> None:
-        """
-        parse the data under the collection_path
-        :return: NA
-        """
-        self._collect_project_paths()
+        if not self._collect_project_paths():
+            error(MsProfCommonConstant.COMMON_FILE_NAME,
+                  "The cluster step trace parsing failed.")
 
     def save(self: any) -> None:
-        """
-        save the result of data parsing
-        :return: NA
-        """
-        if not self._init_database_model():
+        if not self.id_with_project_path_map:
+            return
+        if not self._init_cluster_step_trace_model():
             return
         self._collect_and_save_step_trace_data()
         self.cluster_model.finalize()
 
-    def _init_database_model(self) -> bool:
-        if not self.id_with_project_path_map:
+    def _check_collection_path_valid(self: any) -> bool:
+        sqlite_path = PathManager.get_sql_dir(self.collection_path)
+        if os.path.isdir(sqlite_path) and DBNameConstant.DB_CLUSTER_RANK in os.listdir(sqlite_path):
+            return True
+        else:
             return False
+
+    def _init_cluster_step_trace_model(self: any) -> bool:
         rank_ids = list(self.id_with_project_path_map.keys())
         for rank_id in rank_ids:
             self.id_with_table_map.setdefault(rank_id, "step_trace_{}".format(rank_id))
-            self.cluster_model = ClusterStepTraceModel(self.collection_path, DBNameConstant.DB_CLUSTER_STEP_TRACE,
-                                                       self.id_with_table_map.values())
+        self.cluster_model = ClusterStepTraceModel(self.collection_path, DBNameConstant.DB_CLUSTER_STEP_TRACE,
+                                                   self.id_with_table_map.values())
+
         if not self.cluster_model.init():
+            logging.error("The cluster step trace datebase initialization is failed.")
             return False
         self.cluster_model.create_table()
         return True
 
-    def _collect_project_paths(self: any) -> None:
+    def _collect_project_paths(self: any) -> bool:
         rank_db_path = PathManager.get_db_path(self.collection_path, DBNameConstant.DB_CLUSTER_RANK)
         conn, curs = DBManager.check_connect_db_path(rank_db_path)
         if not conn or not curs or not DBManager.check_tables_in_db(rank_db_path, DBNameConstant.TABLE_CLUSTER_RANK):
             DBManager.destroy_db_connect(conn, curs)
-            logging.error("no cluster table")
-            return
+            logging.error("The cluster rank table doesn't exist.")
+            return False
         sql = "select rank_id, dir_name from {}".format(DBNameConstant.TABLE_CLUSTER_RANK)
         data = DBManager.fetch_all_data(curs, sql)
         DBManager.destroy_db_connect(conn, curs)
         if not data:
-            logging.error("no cluster data")
-            return
+            logging.error("The query cluster rank info is invalid.")
+            return False
+        # sort by rank_id
         data.sort(key=lambda x: x[0])
         self.id_with_project_path_map = dict(data)
+        return True
 
-    def _collect_and_save_step_trace_data(self: any):
+    def _collect_and_save_step_trace_data(self: any) -> None:
         for rank_id, path in self.id_with_project_path_map.items():
             project_path = os.path.join(self.collection_path, path)
-            dirs = get_path_dir(project_path)
-            for folder in dirs:
-                project_path = os.path.join(project_path, folder)
-                if not DataCheckManager.contain_info_json_data(project_path):
+            sub_dirs = get_path_dir(project_path)
+            for sub_dir in sub_dirs:
+                project_sub_path = os.path.join(project_path, sub_dir)
+                if not DataCheckManager.contain_info_json_data(project_sub_path):
                     continue
-                InfoConfReader().load_info(project_path)
+                InfoConfReader().load_info(project_sub_path)
                 if InfoConfReader().is_host_profiling():
                     continue
-                step_trace_data = self._search_step_trace_data(project_path)
+                step_trace_data = self._collect_step_trace_data(project_sub_path)
                 if not step_trace_data:
                     continue
                 self.cluster_model.insert_data_to_db(self.id_with_table_map.get(rank_id), step_trace_data)
 
-    def _search_step_trace_data(self: any, project_path: str) -> list:
-        data = []
+    def _collect_step_trace_data(self: any, project_path: str) -> list:
+        step_trace_data = []
         db_path_ge = PathManager.get_db_path(project_path, DBNameConstant.DB_GE_INFO)
         conn_ge, curs_ge = DBManager.check_connect_db_path(db_path_ge)
-        if not conn_ge or not curs_ge or not DBManager.check_tables_in_db(
-                db_path_ge, DBNameConstant.TABLE_GE_TASK):
+        if not conn_ge or not curs_ge or not DBManager.check_tables_in_db(db_path_ge, DBNameConstant.TABLE_GE_TASK):
             DBManager.destroy_db_connect(conn_ge, curs_ge)
-            logging.error("missing ge table")
-            return data
+            logging.error("Can't find ge task data table.")
+            return step_trace_data
         db_path = PathManager.get_db_path(project_path, DBNameConstant.DB_TRACE)
         conn, curs = DBManager.check_connect_db_path(db_path)
-        if not conn or not curs or not DBManager.check_tables_in_db(
-                db_path, DBNameConstant.TABLE_TRAINING_TRACE):
+        if not conn or not curs or not DBManager.check_tables_in_db(db_path, DBNameConstant.TABLE_TRAINING_TRACE):
             DBManager.destroy_db_connect(conn_ge, curs_ge)
             DBManager.destroy_db_connect(conn, curs)
-            logging.error("missing step trace table")
-            return data
-        model_id_set = self._search_ge_model_id(curs_ge)
+            logging.error("Can't find step trace data table")
+            return step_trace_data
+        model_ids = self._search_ge_model_id(curs_ge)
         step_trace_data = self._search_step_trace(curs)
         DBManager.destroy_db_connect(conn_ge, curs_ge)
         DBManager.destroy_db_connect(conn, curs)
-        if not model_id_set or not step_trace_data:
-            logging.error("missing data")
-            return data
-        step_trace_data = self._append_ge_model_tag(step_trace_data, model_id_set)
+        if not step_trace_data:
+            logging.error("Can't get step trace data.")
+            return step_trace_data
+        step_trace_data = self._append_ge_model_tag(step_trace_data, model_ids)
         return step_trace_data
 
-    def _search_ge_model_id(self: any, curs: any) -> set:
+    def _search_ge_model_id(self: any, curs: any) -> list:
         sql = "select distinct model_id from {}".format(DBNameConstant.TABLE_GE_TASK)
-        data = DBManager.fetch_all_data(curs, sql)
-        model_id_set = {model_id[0] for model_id in data}
-        return model_id_set
+        model_ids = DBManager.fetch_all_data(curs, sql)
+        model_ids = list(chain.from_iterable(model_ids))
+        return model_ids
 
     def _search_step_trace(self: any, curs: any) -> list:
         sql = "select device_id, " \
@@ -148,11 +158,11 @@ class ClusterStepTraceParser(IParser):
         data = DBManager.fetch_all_data(curs, sql)
         return data
 
-    def _append_ge_model_tag(self: any, data: list, model_id_set: set) -> list:
+    def _append_ge_model_tag(self: any, step_trace_data: list, model_ids: list) -> list:
         result = []
-        for item in data:
+        for item in step_trace_data:
             item = list(item)
-            if item[1] in model_id_set:
+            if item[1] in model_ids:
                 item.append(1)
             else:
                 item.append(0)
