60a13e5787bd3b7969eadf9b49662c78efcb1115
yu-liang-bin <y1416490440@163.com>
2022-08-10 10:52:57 +0800

    AR:AR20220705252387 Description:msprof提供query接口，查询多卡和单卡的迭代轨迹数据 Team:MSPROF_TOOLS Feature or Bugfix:Feature
    Match-id-ac31d28bb7a858f11921adcbc58b5f669d10c0f0

--- analysis/profiling_bean/cluster_collection/step_trace_summary.py
+++ analysis/profiling_bean/cluster_collection/step_trace_summary.py
@@ -16,9 +16,8 @@ from common_func.db_manager import DBManager
 from common_func.db_name_constant import DBNameConstant
 from common_func.info_conf_reader import InfoConfReader
 from common_func.ms_constant.number_constant import NumberConstant
-from common_func.msprof_common import MsProfCommonConstant, get_path_dir
+from common_func.msprof_common import get_path_dir
 from common_func.msprof_exception import ProfException
-from common_func.msprof_query_summary_manager import MsprofQuerySummaryManager
 from common_func.msvp_common import create_json
 from common_func.path_manager import PathManager
 from common_func.step_trace_constant import StepTraceConstant
@@ -33,6 +32,7 @@ class StepTraceSummay:
                "Iteration Refresh", "Iteration Start", "FP Start", "BP End", "Iteration End"]
     ID_NUM_FOR_ALL_DEVICES = -1
     ID_NUM_FOR_ALL_ITERATIONS = -1
+    NUMBER_0F_DECIMAL_PLACE = 2
 
     def __init__(self: any, params: dict) -> None:
         self.collection_path = params["collection_path"]
@@ -45,16 +45,19 @@ class StepTraceSummay:
         self.all_devices = False
 
     def process(self: any) -> None:
+        self._check_query_all_devices()
+        if not self._check_iteration_id_valid():
+            return
         if self.is_cluster_scene:
             self._process_in_cluster_scene()
         else:
             self._process_in_non_cluster_scene()
         if self.data_collection:
-            self._export_summary_data()
+            self._storage_summary_data()
 
-    def _export_summary_data(self: any) -> None:
+    def _storage_summary_data(self: any) -> None:
         output_file_name = "step_trace_{}_{}_{}.json".format(self.npu_id, self.model_id, self.iteration_id)
-        output_file_path = os.path.join(self.collection_path, MsprofQuerySummaryManager.FOLDER_NAME_TO_STORAGE,
+        output_file_path = os.path.join(self.collection_path, PathManager.QUERY_CLUSTER,
                                         output_file_name)
         result = create_json(output_file_path, StepTraceSummay.HEADERS, self.data_collection, save_old_file=False)
         result_json = json.loads(result)
@@ -66,9 +69,16 @@ class StepTraceSummay:
     def _process_in_cluster_scene(self: any) -> None:
         if not self._check_cluster_db_valid():
             return
-        self._check_query_all_devices_scene()
-        rank_id_collection = list(self._get_all_rank_ids())
+        rank_id_collection = self._get_all_rank_ids()
+        if not rank_id_collection:
+            error(StepTraceSummay.FILE_NAME,
+                  "Get cluster rank id info failed.")
+            return
         query_data = self._query_in_cluster_scene(rank_id_collection)
+        if not query_data:
+            error(StepTraceSummay.FILE_NAME,
+                  "Get cluster step trace data failed.")
+            return
         self._format_cluster_data(query_data, rank_id_collection)
 
     def _check_cluster_db_valid(self: any) -> bool:
@@ -76,25 +86,37 @@ class StepTraceSummay:
         if not os.path.exists(db_file):
             error(StepTraceSummay.FILE_NAME,
                   "There is not step trace cluster database file. Please check the input dir.")
-            logging.error("There is not step trace cluster database file. Please check the input dir.")
             return False
         return True
 
-    def _check_query_all_devices_scene(self: any) -> None:
+    def _check_query_all_devices(self: any) -> None:
         self.all_devices = True if self.npu_id == StepTraceSummay.ID_NUM_FOR_ALL_DEVICES else False
+
+    def _check_iteration_id_valid(self: any) -> bool:
+        if self.all_devices and self.iteration_id == StepTraceSummay.ID_NUM_FOR_ALL_ITERATIONS:
+            error(StepTraceSummay.FILE_NAME,
+                  "For querying all devices data, you should input a valid iteration id.")
+            return False
+        if not self.all_devices and self.iteration_id != StepTraceSummay.ID_NUM_FOR_ALL_ITERATIONS:
+            error(StepTraceSummay.FILE_NAME,
+                  "For querying single device data, you should not input a iteration id.")
+            return False
+        return True
 
     def _query_in_cluster_scene(self: any, rank_id_collection: list) -> list:
         data = []
         db_file = PathManager.get_db_path(self.collection_path, DBNameConstant.DB_CLUSTER_STEP_TRACE)
         conn, curs = DBManager.check_connect_db_path(db_file)
-        if not (conn and curs):
+        if not conn or not curs:
+            DBManager.destroy_db_connect(conn, curs)
+            logging.error("The connect to cluster step trace database is failed.")
             return data
         if self.all_devices:
-            data = self._get_data_from_all_device(curs, rank_id_collection)
+            data = self._query_data_from_all_device(curs, rank_id_collection)
         else:
             if self.npu_id not in rank_id_collection:
                 return data
-            data = self._get_data_from_single_device(curs)
+            data = self._query_data_from_single_device(curs)
         DBManager.destroy_db_connect(conn, curs)
         return data
 
@@ -114,7 +136,7 @@ class StepTraceSummay:
         DBManager.destroy_db_connect(conn, curs)
         return rank_ids
 
-    def _get_data_from_all_device(self: any, curs: any, rank_id_collection: list) -> list:
+    def _query_data_from_all_device(self: any, curs: any, rank_id_collection: list) -> list:
         data_colleciton = []
         for rank_id in rank_id_collection:
             table = "step_trace_{}".format(rank_id)
@@ -123,7 +145,7 @@ class StepTraceSummay:
             data_colleciton.append(list(chain.from_iterable(data)))
         return data_colleciton
 
-    def _get_data_from_single_device(self, curs: any) -> list:
+    def _query_data_from_single_device(self, curs: any) -> list:
         table = "step_trace_{}".format(self.npu_id)
         sql = self._sql_for_query_all_iteration(table)
         data = DBManager.fetch_all_data(curs, sql)
@@ -132,18 +154,19 @@ class StepTraceSummay:
     def _sql_for_query_all_iteration(self: any, table_name: str) -> str:
         sql = "select (case when model_id={0} then 'N/A' else model_id end), " \
               "iteration_id, " \
-              "(case when iteration_time={1} then 'N/A' else iteration_time end), " \
-              "(case when fp_bp_time={1} then 'N/A' else fp_bp_time end), " \
-              "(case when data_aug_bound={1} then 'N/A' else data_aug_bound end), " \
-              "(case when bp_end={1} then 'N/A' else iteration_end - bp_end end), " \
+              "(case when iteration_time={1} then 'N/A' else round(iteration_time, {2}) end), " \
+              "(case when fp_bp_time={1} then 'N/A' else round(fp_bp_time, {2}) end), " \
+              "(case when data_aug_bound={1} then 'N/A' else round(data_aug_bound, {2}) end), " \
+              "(case when bp_end={1} then 'N/A' else round(iteration_end - bp_end, {2}) end), " \
               "(case when iteration_time={1} or iteration_end={1} then 'N/A' else " \
-              "iteration_end - iteration_time end), " \
-              "(case when fp_start={1} then 'N/A' else FP_start end), " \
-              "(case when bp_end={1} then 'N/A' else BP_end end), " \
-              "(case when iteration_end={1} then 'N/A' else iteration_end end) " \
-              "from {2} where model_id={3}".format(
+              "round(iteration_end - iteration_time, {2}) end), " \
+              "(case when fp_start={1} then 'N/A' else round(FP_start, {2}) end), " \
+              "(case when bp_end={1} then 'N/A' else round(BP_end, {2}) end), " \
+              "(case when iteration_end={1} then 'N/A' else round(iteration_end, {2}) end) " \
+              "from {3} where model_id={4}".format(
             NumberConstant.DEFAULT_MODEL_ID,
             NumberConstant.NULL_NUMBER,
+            StepTraceSummay.NUMBER_0F_DECIMAL_PLACE,
             table_name,
             self.model_id)
         return sql
@@ -156,8 +179,7 @@ class StepTraceSummay:
     def _process_in_non_cluster_scene(self: any) -> None:
         project_dir = self._find_info_file_dir()
         if project_dir:
-            data = self._query_in_non_cluster_scene(project_dir)
-            self.data_collection = data
+            self.data_collection = self._query_in_non_cluster_scene(project_dir)
         else:
             error(StepTraceSummay.FILE_NAME,
                   "The query dir is wrong. Please check.")
@@ -177,7 +199,7 @@ class StepTraceSummay:
                 if not DataCheckManager.contain_info_json_data(project_path):
                     continue
                 InfoConfReader().load_info(project_path)
-                if InfoConfReader().get_rank_id() != -1:
+                if InfoConfReader().get_rank_id() is not None:
                     logging.error(StepTraceSummay.FILE_NAME,
                                   "To query cluster step trace data, please import cluster info data first.")
                     return ""
@@ -215,19 +237,20 @@ class StepTraceSummay:
         sql = "select device_id, " \
               "(case when model_id={0} then 'N/A' else model_id end), " \
               "iteration_id, " \
-              "(case when iteration_time={1} then 'N/A' else iteration_time*{2} end), " \
-              "(case when fp_bp_time={1} then 'N/A' else fp_bp_time*{2} end), " \
-              "(case when data_aug_bound={1} then 'N/A' else data_aug_bound*{2} end), " \
-              "(case when bp_end={1} then 'N/A' else iteration_end - bp_end end), " \
+              "(case when iteration_time={1} then 'N/A' else round(iteration_time*{2}, {3}) end), " \
+              "(case when fp_bp_time={1} then 'N/A' else round(fp_bp_time*{2}, {3} end), " \
+              "(case when data_aug_bound={1} then 'N/A' else round(data_aug_bound*{2}, {3}) end), " \
+              "(case when bp_end={1} then 'N/A' else round((iteration_end - bp_end)*{2}, {3}) end), " \
               "(case when iteration_time={1} or iteration_end={1} then 'N/A' else " \
-              "(iteration_end - iteration_time)*{2} end), " \
-              "(case when FP_start={1} then 'N/A' else FP_start*{2} end), " \
-              "(case when BP_end={1} then 'N/A' else BP_end*{2} end), " \
-              "(case when iteration_end={1} then 'N/A' else iteration_end*{2} end) " \
-              "from {3} where model_id={4}".format(
+              "round((iteration_end - iteration_time)*{2}, {3}) end), " \
+              "(case when FP_start={1} then 'N/A' else round(FP_start*{2}, {3}) end), " \
+              "(case when BP_end={1} then 'N/A' else round(BP_end*{2}, {3}) end), " \
+              "(case when iteration_end={1} then 'N/A' else round(iteration_end*{2}, {3}) end) " \
+              "from {4} where model_id={5}".format(
             NumberConstant.DEFAULT_MODEL_ID,
             NumberConstant.NULL_NUMBER,
             StepTraceConstant.syscnt_to_micro(),
+            StepTraceSummay.NUMBER_0F_DECIMAL_PLACE,
             table_name,
             self.model_id)
         return sql
