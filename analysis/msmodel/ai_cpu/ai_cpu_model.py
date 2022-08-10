e1ddea1e1d7c97690eee49bcbc1fe0baffb7ea3d
yu-liang-bin <y1416490440@163.com>
2022-08-10 08:52:58 +0800

    aicpu
    Match-id-9c16253d53fcfe0f9c3915f859c523413f0fe9c9

--- analysis/msmodel/ai_cpu/ai_cpu_model.py
+++ analysis/msmodel/ai_cpu/ai_cpu_model.py
@@ -5,7 +5,6 @@ function: this script used to operate AI_CPU
 Copyright Huawei Technologies Co., Ltd. 2021. All rights reserved.
 """
 
-from common_func.db_manager import DBManager
 from common_func.db_name_constant import DBNameConstant
 from msmodel.interface.parser_model import ParserModel
 
@@ -18,20 +17,11 @@ class AiCpuModel(ParserModel):
     def __init__(self: any, result_dir: str, table_list: list) -> None:
         super().__init__(result_dir, DBNameConstant.DB_AI_CPU, table_list)
 
-    def create_table(self: any) -> None:
-        """
-        create table
-        """
-        for table_name in self.table_list:
-            if DBManager.judge_table_exist(self.cur, table_name):
-                continue
-            sql = DBManager.sql_create_general_table('AiCpuDataMap', table_name, self.TABLES_PATH)
-            DBManager.execute_sql(self.conn, sql)
-
-    def flush(self: any, data_list: list) -> None:
+    def flush(self: any, data_list: list, ai_cpu_table_name: str = DBNameConstant.TABLE_AI_CPU) -> None:
         """
         insert data to table
         :param data_list: ai_cpu data
+        :param ai_cpu_table_name: ai cpu table name
         :return:
         """
-        self.insert_data_to_db(DBNameConstant.TABLE_AI_CPU, data_list)
+        self.insert_data_to_db(ai_cpu_table_name, data_list)
