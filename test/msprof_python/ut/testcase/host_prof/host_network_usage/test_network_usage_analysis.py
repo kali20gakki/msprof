57e563e1a1bda43f8045430e247a9b5bbe0b4ed2
msprofDeveloper <msprofDeveloper>
2022-08-13 14:07:18 +0800

    ut code restores
    Match-id-abd92ad13c070b1678265d58454a2703234c8fce

--- test/msprof_python/ut/testcase/host_prof/host_network_usage/test_network_usage_analysis.py
+++ test/msprof_python/ut/testcase/host_prof/host_network_usage/test_network_usage_analysis.py
@@ -18,8 +18,7 @@ class TestNetworkUsageAnalysis(unittest.TestCase):
                            return_value=True), \
                 mock.patch('os.listdir', return_value=['host_network.data.slice_0']), \
                 mock.patch(NAMESPACE + '.is_valid_original_data', return_value=True), \
-                mock.patch('host_prof.host_cpu_usage.presenter.'
-                           'host_cpu_usage_presenter.HostCpuUsagePresenter.run'), \
+                mock.patch(NAMESPACE + '.HostNetworkUsagePresenter.run'), \
                 mock.patch('os.path.isfile', return_value=False), \
                 mock.patch('host_prof.host_prof_base.host_prof_presenter_base.'
                            'PathManager.get_data_file_path', return_value='test'):
