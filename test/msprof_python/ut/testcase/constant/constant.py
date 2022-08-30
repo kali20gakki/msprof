ce2c9d8421354213ba748f9a850e7a04364b679e
yu-liang-bin <y1416490440@163.com>
2022-08-30 22:07:38 +0800

    add DT code
    Match-id-81bd2a11af2c6afc558192cd48e04f7bb7c7f069

--- test/msprof_python/ut/testcase/constant/constant.py
+++ test/msprof_python/ut/testcase/constant/constant.py
@@ -1,3 +1,6 @@
+import os
+import stat
+
 INFO_JSON = {'DeviceInfo': [{'id': 0, 'env_type': 3, 'ctrl_cpu_id': 'ARMv8_Cortex_A55',
                              'ctrl_cpu_core_num': 1, 'ctrl_cpu_endian_little': 1,
                              'ts_cpu_core_num': 1,
@@ -10,3 +13,16 @@ CONFIG = {
     'result_dir': 'test', 'device_id': '0', 'iter_id': 1,
     'job_id': 'job_default', 'ip_address': '127.0.0.1', 'model_id': -1
 }
+UT_CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__), "..", "config")
+WRITE_MODES = stat.S_IWUSR | stat.S_IRUSR | stat.S_IRGRP
+WRITE_FLAGS = os.O_WRONLY | os.O_CREAT | os.O_TRUNC
+def clear_dt_project(path):
+    if os.path.exists(path):
+        for root, dirs, files in os.walk(path, topdown=False):
+            for name in files:
+                print(name)
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                print(name)
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(path)
