0e6684ba32c6cb0d2ddd88c42178acae3792906c
yu-liang-bin <y1416490440@163.com>
2022-08-31 16:30:33 +0800

    add code
    Match-id-92b263d0fdb8a58c617a4685a7044ca9bc43249b

--- collector/dvvp/params_adapter/params_adapter_impl.cpp
+++ collector/dvvp/params_adapter/params_adapter_impl.cpp
@@ -309,8 +309,10 @@ int MsprofParamAdapter::GetParamFromInputCfg(std::unordered_map<int, std::pair<M
         return PROFILING_FAILED;
     }
     ret = SetModeDefaultParams(msprofMode_);
+    if (ret != PROFILING_SUCCESS) {
         CmdLog::instance()->CmdErrorLog("msprof running mode invalid.");
         return PROFILING_FAILED;
+    }  
     ret = TransToParam(paramContainer_, params_);
     if (ret != PROFILING_SUCCESS) {
         return PROFILING_FAILED;
@@ -351,6 +353,8 @@ void MsprofParamAdapter::CreateCfgMap()
 {
     std::unordered_map<int, InputCfg>({ 
         {ARGS_OUTPUT, INPUT_CFG_COM_OUTPUT},
+        {ARGS_STORAGE_LIMIT, INPUT_CFG_COM_STORAGE_LIMIT},
+        {ARGS_APPLICATION, INPUT_CFG_MSPROF_APPLICATION},
         {ARGS_ENVIRONMENT, INPUT_CFG_MSPROF_ENVIRONMENT},
         {ARGS_AIC_MODE, INPUT_CFG_COM_AIC_MODE},
         {ARGS_AIC_METRICE, INPUT_CFG_COM_AIC_METRICS},
