8660f67baf3126c2c04dd5caff7707d2cac07383
yu-liang-bin <y1416490440@163.com>
2022-08-31 14:04:57 +0800

    queue data analyse
    Match-id-b6cd5415b963f1a23925a22864cc515878b0f78c

--- analysis/msparser/aicpu/data_queue_parser.py
+++ analysis/msparser/aicpu/data_queue_parser.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # coding=utf-8
 """
-function: this script used to calculate host queue data.
-Copyright Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
+function: this script used to calculate data queue data.
+Copyright Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
 """
 
 import logging
@@ -21,23 +21,32 @@ from profiling_bean.prof_enum.data_tag import DataTag
 
 class DataQueueParser(IParser, MsMultiProcess):
     """
-    class used to calculate fops data in cluster
+    class used to calculate data queue in cluster
     """
 
     AIC_PMU_SIZE = 128
 
     def __init__(self: any, file_list: dict, sample_config: dict) -> None:
-        super(DataQueueParser, self).__init__(sample_config)
+        super().__init__(sample_config)
         self._file_list = file_list.get(DataTag.DATA_QUEUE, [])
         self._sample_config = sample_config
         self._project_path = self._sample_config.get(StrConstant.SAMPLE_CONFIG_PROJECT_PATH)
         self._model = DataQueueModel(self._project_path, [DBNameConstant.TABLE_DATA_QUEUE])
         self._data_list = []
-        self._file_list.sort(key=lambda x: int(x.split("_")[-1]))
+
+    @staticmethod
+    def _read_data(file_path: str) -> list:
+        check_file_readable(file_path)
+        lines = []
+        with open(file_path, 'rb') as file_reader:
+            line = str(file_reader.read().replace(b'\n\x00', b' ___ ').replace(b'\x00', b' ___ '),
+                       encoding='utf-8')
+            lines += list(filter(None, line.split(" ___ ")))
+        return lines
 
     def parse(self: any) -> None:
         """
-        to read host queue data
+        to read data queue
         :return: None
         """
         try:
@@ -60,13 +69,11 @@ class DataQueueParser(IParser, MsMultiProcess):
             with self._model as _model:
                 _model.flush(self._data_list)
         except sqlite3.Error as err:
-            logging.error("Save host queue data failed! %s", err)
-        finally:
-            pass
+            logging.error("Save data queue failed! %s", err)
 
     def ms_run(self: any) -> None:
         """
-        parse host queue data and save it to db.
+        parse data queue and save it to db.
         :return:None
         """
         if self._file_list:
@@ -89,13 +96,3 @@ class DataQueueParser(IParser, MsMultiProcess):
                 self._data_list.append(res_data)
         except (ValueError, IndexError) as err:
             logging.error(err, exc_info=Constant.TRACE_BACK_SWITCH)
-
-    @staticmethod
-    def _read_data(file_path: str) -> list:
-        check_file_readable(file_path)
-        lines = []
-        with open(file_path, 'rb') as file_reader:
-            line = str(file_reader.read().replace(b'\n\x00', b' ___ ').replace(b'\x00', b' ___ '),
-                       encoding='utf-8')
-            lines += list(filter(None, line.split(" ___ ")))
-        return lines
