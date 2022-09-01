812430585058e49245f1282cbeb48655c2cc9992
msprofDeveloper <msprofDeveloper>
2022-09-01 21:31:37 +0800

    if else simplify
    Match-id-6914264ff40fcc8c993dcd4bd2d36659526f5bd3

--- analysis/msparser/cluster/cluster_parallel_parser.py
+++ analysis/msparser/cluster/cluster_parallel_parser.py
@@ -21,19 +21,20 @@ class ClusterParallelParser:
     """
         cluster parallel data analysis
     """
-    THRESHOLD_VALUE = 0.1
-    TYPE_0_MESSAGE = "The communication time ratio of all NPU cards is good and " \
-                     "below the threshold of %s." % format(THRESHOLD_VALUE, '.0%')
-    TYPE_1_MESSAGE = "The communication time ratio of all NPU cards exceeds " \
-                     "the threshold of %s." % format(THRESHOLD_VALUE, '.0%')
-    TYPE_2_MESSAGE = "The communication time ratio of some NPU cards exceeds the threshold of %s. " \
-                     "You need to check whether there are slow nodes or slow links." % format(THRESHOLD_VALUE, '.0%')
 
     def __init__(self, params: dict) -> None:
         self.collection_path = params["collection_path"]
         self._cluster_info_model = ClusterInfoViewModel(params["collection_path"])
         self._cluster_communication_model = ClusterCommunicationModel(params)
         self.parallel_analysis_result = {}
+
+    @classmethod
+    def _get_result_type_message(cls: any, parallel_data: list, device_and_rank_ids: list) -> any:
+        if not parallel_data:
+            return ClusterParallelConstant.ALL_BELOW_THRESHOLD_ID, ClusterParallelConstant.ALL_BELOW_THRESHOLD_MESSAGE
+        if len(parallel_data) == len(device_and_rank_ids):
+            return ClusterParallelConstant.ALL_OVER_THRESHOLD_ID, ClusterParallelConstant.ALL_OVER_THRESHOLD_MESSAGE
+        return ClusterParallelConstant.PART_OVER_THRESHOLD_ID, ClusterParallelConstant.PART_OVER_THRESHOLD_MESSAGE
 
     def process(self: any) -> None:
         self._parallel_analysis()
@@ -53,30 +54,23 @@ class ClusterParallelParser:
         parallel_data = []
         with self._cluster_communication_model as _model:
             for device_and_rank in device_and_rank_ids:
-                if device_and_rank[1] == Constant.NA:
-                    id_name = 'device_id'
-                    id_value = device_and_rank[0]
-                else:
-                    id_name = 'rank_id'
-                    id_value = device_and_rank[1]
+                id_name = 'device_id' if device_and_rank[1] == Constant.NA else 'rank_id'
+                id_value = device_and_rank[0] if device_and_rank[1] == Constant.NA else device_and_rank[1]
                 communication_time_ratio = _model.get_communication_time_ratio(id_value)
                 if communication_time_ratio == Constant.DEFAULT_INVALID_VALUE or communication_time_ratio is None:
                     error(MsProfCommonConstant.COMMON_FILE_NAME, "Invalid communication_time_ratio!")
                     return Constant.DEFAULT_INVALID_VALUE
-                if communication_time_ratio < self.THRESHOLD_VALUE:
+                if communication_time_ratio < ClusterParallelConstant.THRESHOLD_VALUE:
                     continue
-                parallel_analysis_value = {}
-                parallel_analysis_value.setdefault(id_name, id_value)
-                parallel_analysis_value.setdefault("communication_time_ratio", communication_time_ratio)
+                parallel_analysis_value = {id_name: id_value, "communication_time_ratio": communication_time_ratio}
                 parallel_data.append(parallel_analysis_value)
         if parallel_data:
             parallel_data.sort(key=lambda x: x["communication_time_ratio"], reverse=True)
         return parallel_data
 
     def _parallel_analysis(self: any) -> None:
-        self.parallel_analysis_result["type"] = Constant.DEFAULT_INVALID_VALUE
-        self.parallel_analysis_result["value"] = []
-        self.parallel_analysis_result["message"] = "Invalid parallel analysis data!"
+        self.parallel_analysis_result.update(
+            {"type": Constant.DEFAULT_INVALID_VALUE, "value": [], "message": "Invalid parallel analysis data!"})
         device_and_rank_ids = self._get_device_and_rank_ids()
         if device_and_rank_ids == Constant.DEFAULT_INVALID_VALUE:
             return
@@ -87,17 +81,9 @@ class ClusterParallelParser:
         parallel_data = self._get_parallel_data(device_and_rank_ids)
         if parallel_data == Constant.DEFAULT_INVALID_VALUE:
             return
-        elif not parallel_data:
-            self.parallel_analysis_result["type"] = 0
-            self.parallel_analysis_result["message"] = self.TYPE_0_MESSAGE
-            return
         self.parallel_analysis_result["value"] = parallel_data
-        if len(parallel_data) == len(device_and_rank_ids):
-            self.parallel_analysis_result["type"] = 1
-            self.parallel_analysis_result["message"] = self.TYPE_1_MESSAGE
-        else:
-            self.parallel_analysis_result["type"] = 2
-            self.parallel_analysis_result["message"] = self.TYPE_2_MESSAGE
+        type_value, message_value = self._get_result_type_message(parallel_data, device_and_rank_ids)
+        self.parallel_analysis_result.update({"type": type_value, "message": message_value})
 
     def _storage_parallel_analysis_result(self: any) -> None:
         output_file_name = "cluster_parallel_analysis.json"
@@ -117,3 +103,17 @@ class ClusterParallelParser:
                 json.dump(self.parallel_analysis_result, file, indent=4)
         except (OSError, SystemError, ValueError, TypeError, RuntimeError) as err:
             error(MsProfCommonConstant.COMMON_FILE_NAME, err)
+
+
+class ClusterParallelConstant:
+    THRESHOLD_VALUE = 0.1
+    ALL_BELOW_THRESHOLD_ID = 0
+    ALL_OVER_THRESHOLD_ID = 1
+    PART_OVER_THRESHOLD_ID = 2
+    ALL_BELOW_THRESHOLD_MESSAGE = "The communication time ratio of all NPU cards is good and " \
+                                  "below the threshold of %s." % format(THRESHOLD_VALUE, '.0%')
+    ALL_OVER_THRESHOLD_MESSAGE = "The communication time ratio of all NPU cards exceeds " \
+                                 "the threshold of %s." % format(THRESHOLD_VALUE, '.0%')
+    PART_OVER_THRESHOLD_MESSAGE = "The communication time ratio of some NPU cards exceeds the threshold of %s. " \
+                                  "You need to check whether there are slow nodes or slow links." % format(
+        THRESHOLD_VALUE, '.0%')
