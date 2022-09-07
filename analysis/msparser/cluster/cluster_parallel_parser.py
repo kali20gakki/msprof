ebab47d147f58304216f7afa9c0cc6c21321ec37
msprofDeveloper <msprofDeveloper>
2022-09-07 14:03:17 +0800

    diedai3 test dts fix
    Match-id-99e6126fb7fdf852d80fbd3f6fe3a1d259bf0e53

--- analysis/msparser/cluster/cluster_parallel_parser.py
+++ analysis/msparser/cluster/cluster_parallel_parser.py
@@ -3,14 +3,17 @@
 # Copyright (c) Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
 
 import json
+import logging
 import os
 
-from common_func.common import error
+from common_func.common import error, warn
 from common_func.constant import Constant
 from common_func.db_name_constant import DBNameConstant
 from common_func.ms_constant.number_constant import NumberConstant
-from common_func.msprof_common import MsProfCommonConstant
+from common_func.msprof_common import MsProfCommonConstant, prepare_log
 from common_func.path_manager import PathManager
+from common_func.common import print_info
+from common_func.msprof_exception import ProfException
 from msmodel.cluster_info.cluster_communication_model import ClusterCommunicationModel
 from msmodel.cluster_info.cluster_info_model import ClusterInfoViewModel
 
@@ -22,6 +25,9 @@ class ClusterParallelParser:
 
     def __init__(self, params: dict) -> None:
         self.collection_path = params["collection_path"]
+        self._npu_id = params["npu_id"]
+        self._model_id = params["model_id"]
+        self._iteration_id = params["iteration_id"]
         self._cluster_info_model = ClusterInfoViewModel(params["collection_path"])
         self._cluster_communication_model = ClusterCommunicationModel(params)
         self.parallel_analysis_result = {}
@@ -35,18 +41,34 @@ class ClusterParallelParser:
         return ClusterParallelConstant.PART_OVER_THRESHOLD_ID, ClusterParallelConstant.PART_OVER_THRESHOLD_MESSAGE
 
     def process(self: any) -> None:
+        self._prepare_parallel_analysis()
         self._parallel_analysis()
         self._storage_parallel_analysis_result()
+        print_info(MsProfCommonConstant.COMMON_FILE_NAME,
+                   'Parallel analysis finished!')
 
     def _get_device_and_rank_ids(self: any) -> any:
         if not os.path.exists(PathManager.get_db_path(self.collection_path, DBNameConstant.DB_CLUSTER_RANK)):
             error(MsProfCommonConstant.COMMON_FILE_NAME, "Cannot find the cluster_rank.db or Permission denied!")
-            return Constant.DEFAULT_INVALID_VALUE
+            raise ProfException(ProfException.PROF_CLUSTER_INVALID_DB)
         if not os.path.exists(PathManager.get_db_path(self.collection_path, DBNameConstant.DB_CLUSTER_STEP_TRACE)):
             error(MsProfCommonConstant.COMMON_FILE_NAME, "Cannot find the cluster_step_trace.db or Permission denied!")
-            return Constant.DEFAULT_INVALID_VALUE
+            raise ProfException(ProfException.PROF_CLUSTER_INVALID_DB)
         with self._cluster_info_model as _model:
             return _model.get_device_and_rank_ids()
+
+    def _prepare_parallel_analysis(self: any) -> None:
+        if self._npu_id != Constant.DEFAULT_INVALID_VALUE:
+            error(MsProfCommonConstant.COMMON_FILE_NAME, "Query arguments error! The argument '--id' must be -1.")
+            raise ProfException(ProfException.PROF_INVALID_PARAM_ERROR)
+        if self._model_id is not None or self._iteration_id is not None:
+            warn(MsProfCommonConstant.COMMON_FILE_NAME, "The parameters: '--model-id' and '--iteration-id' is invalid."
+                                                        " you don't hava to input these parameters.")
+            raise ProfException(ProfException.PROF_INVALID_PARAM_ERROR)
+        output_file_path = PathManager.get_query_result_path(self.collection_path, 'cluster_parallel_analysis.json')
+        if os.path.exists(output_file_path):
+            os.remove(output_file_path)
+        prepare_log(self.collection_path)
 
     def _get_parallel_data(self: any, device_and_rank_ids: list) -> any:
         parallel_data = []
@@ -57,7 +79,7 @@ class ClusterParallelParser:
                 communication_time_ratio = _model.get_communication_time_ratio(id_value)
                 if communication_time_ratio == Constant.DEFAULT_INVALID_VALUE or communication_time_ratio is None:
                     error(MsProfCommonConstant.COMMON_FILE_NAME, "Invalid communication_time_ratio!")
-                    return Constant.DEFAULT_INVALID_VALUE
+                    raise ProfException(ProfException.PROF_INVALID_DATA_ERROR)
                 if communication_time_ratio < ClusterParallelConstant.THRESHOLD_VALUE:
                     continue
                 parallel_analysis_value = {id_name: id_value, "communication_time_ratio": communication_time_ratio}
@@ -67,23 +89,19 @@ class ClusterParallelParser:
         return parallel_data
 
     def _parallel_analysis(self: any) -> None:
-        self.parallel_analysis_result.update(
-            {"type": Constant.DEFAULT_INVALID_VALUE, "value": [], "message": "Invalid parallel analysis data!"})
         device_and_rank_ids = self._get_device_and_rank_ids()
-        if device_and_rank_ids == Constant.DEFAULT_INVALID_VALUE:
-            return
         if not device_and_rank_ids:
             error(MsProfCommonConstant.COMMON_FILE_NAME, "Cannot get the device or rank ids, "
                                                          "please check the table ClusterRank!")
-            return
+            raise ProfException(ProfException.PROF_INVALID_DATA_ERROR)
         parallel_data = self._get_parallel_data(device_and_rank_ids)
-        if parallel_data == Constant.DEFAULT_INVALID_VALUE:
-            return
         self.parallel_analysis_result["value"] = parallel_data
         type_value, message_value = self._get_result_type_message(parallel_data, device_and_rank_ids)
         self.parallel_analysis_result.update({"type": type_value, "message": message_value})
 
     def _storage_parallel_analysis_result(self: any) -> None:
+        if not self.parallel_analysis_result:
+            return
         output_file_name = "cluster_parallel_analysis.json"
         query_path = os.path.join(self.collection_path, PathManager.QUERY_CLUSTER)
         if not os.path.exists(query_path):
