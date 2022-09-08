d9cec2942aafbb5436cbce1bbc6e16bc0a43ff82
yu-liang-bin <y1416490440@163.com>
2022-09-08 17:52:15 +0800

    llc值初始化和AI Core默认值设置
    Match-id-bd3136f338e1ee5550ed6efc537446c952eef014

--- collector/dvvp/params_adapter/params_adapter_impl.cpp
+++ collector/dvvp/params_adapter/params_adapter_impl.cpp
@@ -157,18 +157,18 @@ void MsprofParamAdapter::SetDefaultParamsApp()
     }
     if (paramContainer_[INPUT_CFG_COM_AI_CORE].empty()) {
         paramContainer_[INPUT_CFG_COM_AI_CORE] = MSVP_PROF_ON;
-        paramContainer_[INPUT_CFG_COM_AIC_MODE] = PROFILING_MODE_TASK_BASED;
-        paramContainer_[INPUT_CFG_COM_AIC_FREQ] = std::to_string(THOUSAND / DEFAULT_PROFILING_INTERVAL_10MS);
     }
+    paramContainer_[INPUT_CFG_COM_AIC_FREQ] = paramContainer_[INPUT_CFG_COM_AIC_FREQ].empty() ?
+        std::to_string(THOUSAND / DEFAULT_PROFILING_INTERVAL_10MS) : paramContainer_[INPUT_CFG_COM_AIC_FREQ];
     paramContainer_[INPUT_CFG_COM_AIC_MODE] = (paramContainer_[INPUT_CFG_COM_AIC_MODE].empty()) ?
         PROFILING_MODE_TASK_BASED : paramContainer_[INPUT_CFG_COM_AIC_MODE];
     paramContainer_[INPUT_CFG_COM_AIC_METRICS] = (paramContainer_[INPUT_CFG_COM_AIC_METRICS].empty()) ?
         PIPE_UTILIZATION : paramContainer_[INPUT_CFG_COM_AIC_METRICS];
     if (paramContainer_[INPUT_CFG_COM_AI_VECTOR].empty()) {
         paramContainer_[INPUT_CFG_COM_AI_VECTOR] = MSVP_PROF_ON;
-        paramContainer_[INPUT_CFG_COM_AIV_MODE] = PROFILING_MODE_TASK_BASED;
-        paramContainer_[INPUT_CFG_COM_AIV_FREQ] = std::to_string(THOUSAND / DEFAULT_PROFILING_INTERVAL_10MS);
     }
+    paramContainer_[INPUT_CFG_COM_AIV_FREQ] = paramContainer_[INPUT_CFG_COM_AIV_FREQ].empty() ?
+        std::to_string(THOUSAND / DEFAULT_PROFILING_INTERVAL_10MS) : paramContainer_[INPUT_CFG_COM_AIV_FREQ];
     paramContainer_[INPUT_CFG_COM_AIV_MODE] = (paramContainer_[INPUT_CFG_COM_AIV_MODE].empty()) ?
         PROFILING_MODE_TASK_BASED : paramContainer_[INPUT_CFG_COM_AIV_MODE];
     paramContainer_[INPUT_CFG_COM_AIV_METRICS] = (paramContainer_[INPUT_CFG_COM_AIV_METRICS].empty()) ?
