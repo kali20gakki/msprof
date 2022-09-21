e5fa1a1d4fed3c807a94d2783289b562b4b9c4b8
yu-liang-bin <y1416490440@163.com>
2022-09-21 09:44:25 +0800

    set rank id
    Match-id-405bed574facb7ff0e8d7b71d1a5cb0c8476b4b3

--- collector/dvvp/task_handle/include/info_json.h
+++ collector/dvvp/task_handle/include/info_json.h
@@ -64,7 +64,7 @@ private:
     void AddSysTime(SHARED_PTR_ALIA<analysis::dvvp::proto::InfoMain> infoMain);
     void AddMemTotal(SHARED_PTR_ALIA<analysis::dvvp::proto::InfoMain> infoMain);
     void AddNetCardInfo(SHARED_PTR_ALIA<analysis::dvvp::proto::InfoMain> infoMain);
-    void AddRankId(SHARED_PTR_ALIA<analysis::dvvp::proto::InfoMain> infoMain);
+    void SetRankId(SHARED_PTR_ALIA<analysis::dvvp::proto::InfoMain> infoMain);
 
 private:
     std::string jobInfo_;
