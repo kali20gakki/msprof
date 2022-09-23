7f2748b1a9ab6c7bcdf9ff68049cce2f150561b1
msprofDeveloper <msprofDeveloper>
2022-09-23 13:51:53 +0800

    noge data error
    Match-id-42fe64c30f9d2004f72e47fe68cf7b1c0bb789a5

---
+++ analysis/profiling_bean/db_dto/hwts_rec_dto.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+
+
+class HwtsRecDto:
+    """
+    step trace dro
+    """
+    DEFAULT_ITER_ID = -1
+
+    def __init__(self: any) -> None:
+        self._ai_core_num = None
+        self._task_count = None
+        self._sys_cnt = None
+        self._iter_id = None
+
+    @property
+    def ai_core_num(self: any) -> any:
+        return self._ai_core_num
+
+    @ai_core_num.setter
+    def ai_core_num(self: any, value: any) -> None:
+        self._ai_core_num = value
+
+    @property
+    def task_count(self: any) -> any:
+        return self._task_count
+
+    @task_count.setter
+    def task_count(self: any, value: any) -> None:
+        self._task_count = value
+
+    @property
+    def sys_cnt(self: any) -> any:
+        return self._sys_cnt
+
+    @sys_cnt.setter
+    def sys_cnt(self: any, value: any) -> None:
+        self._sys_cnt = value
+
+    @property
+    def iter_id(self: any) -> any:
+        return self._iter_id
+
+    @iter_id.setter
+    def iter_id(self: any, value: any) -> None:
+        self._iter_id = value
