10bb78c75a3d8adb9a617934fc5a0d72e2423e35
yu-liang-bin <y1416490440@163.com>
2022-09-26 22:14:10 +0800

    aicpu and tasktime csv youhua
    Match-id-168c1b5c795ba5edcd1738aa1cd4a2ee12033fc2

--- test/msprof_python/ut/testcase/viewer/memory_copy/memory_copy_viewer.py
+++ test/msprof_python/ut/testcase/viewer/memory_copy/memory_copy_viewer.py
@@ -13,15 +13,13 @@ class TestMemoryCopyViewer(unittest.TestCase):
         self.memcopy_viewer = MemoryCopyViewer(self.current_path)
 
     def test_get_memory_copy_chip0_summary_1(self):
-        expect_res = [(0, 20000, 1, 20000.0, 20000, 20000, 100, 20000, MemoryCopyConstant.DEFAULT_VIEWER_VALUE,
-                           MemoryCopyConstant.TYPE, StrConstant.API_TYPE_MAPPING.get(
-                StrConstant.AYNC_MEMCPY_NUM), 1,
+        expect_res = [(0, 20000, 1, 20000, 20000, 20000, 100, 20000, MemoryCopyConstant.DEFAULT_VIEWER_VALUE,
+                           MemoryCopyConstant.TYPE, StrConstant.AYNC_MEMCPY, 1,
                            MemoryCopyConstant.DEFAULT_VIEWER_VALUE, 1)]
 
-        export_data = [(20000, MemoryCopyConstant.TYPE,
-                       1, 1, 100, 20000)]
+        export_data = [(20000, MemoryCopyConstant.TYPE, 1, 1, 100, 20000, 20000, 20000, 1)]
         with mock.patch(NAMESPACE + '.check_db', return_value=True), \
-                mock.patch(NAMESPACE + '.export_chip0_summary', return_value=export_data):
+                mock.patch(NAMESPACE + '.return_chip0_summary', return_value=export_data):
             res = self.memcopy_viewer.get_memory_copy_chip0_summary()
 
         self.assertEqual(expect_res, res)
@@ -31,18 +29,18 @@ class TestMemoryCopyViewer(unittest.TestCase):
 
         export_data = []
         with mock.patch(NAMESPACE + '.check_db', return_value=False), \
-                mock.patch(NAMESPACE + '.export_chip0_summary', return_value=export_data):
+                mock.patch(NAMESPACE + '.return_chip0_summary', return_value=export_data):
             res = self.memcopy_viewer.get_memory_copy_chip0_summary()
 
         self.assertEqual(expect_res, res)
 
     def test_get_memory_copy_non_chip0_summary_1(self):
-        expect_res = [("MemcopyAsync", "other", 11, 12, '"100"', '"2200"', '"2300"')]
+        expect_res = [("MemcopyAsync", "other", 11, 12, '100', '"2200"', '"2300"')]
 
         export_data = [(MemoryCopyConstant.ASYNC_MEMCPY_NAME, MemoryCopyConstant.TYPE, 11,
                        12, 100, 2.2, 2.3)]
         with mock.patch(NAMESPACE + '.check_db', return_value=True), \
-                mock.patch(NAMESPACE + '.export_non_chip0_summary', return_value=export_data):
+                mock.patch(NAMESPACE + '.return_not_chip0_summary', return_value=export_data):
             res = self.memcopy_viewer.get_memory_copy_non_chip0_summary()
 
         self.assertEqual(expect_res, res)
@@ -51,7 +49,7 @@ class TestMemoryCopyViewer(unittest.TestCase):
         expect_res = []
         export_data = []
         with mock.patch(NAMESPACE + '.check_db', return_value=False), \
-                mock.patch(NAMESPACE + '.export_non_chip0_summary', return_value=export_data):
+                mock.patch(NAMESPACE + '.return_not_chip0_summary', return_value=export_data):
             res = self.memcopy_viewer.get_memory_copy_non_chip0_summary()
 
         self.assertEqual(expect_res, res)
