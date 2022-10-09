8cd361c42ebc168d442fefb1b7af7e3b25c85235
msprofDeveloper <msprofDeveloper>
2022-10-09 18:45:37 +0800

    multi_model
    Match-id-6db0916b9045dfaa7bed3b7cc5deb1a0894d2f9f

---
+++ analysis/profiling_sql_framework/prof_sql_visitor.py
@@ -0,0 +1,12 @@
+class ProfVisitor:
+    def visit(self: any) -> None:
+        """
+
+        :return:
+        """
+
+    def get_result(self: any) -> any:
+        """
+
+        :return:
+        """
