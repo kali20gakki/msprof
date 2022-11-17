8f2104ab02af7a5b3bffaa59ea920d01c5c526ea
msprofDeveloper <msprofDeveloper>
2022-11-17 11:29:41 +0800

    config ut
    Match-id-bbd930bfa5b9dafc494b34a6dd25895fc4315e14

--- test/msprof_python/ut/testcase/config/test_config_manager.py
+++ test/msprof_python/ut/testcase/config/test_config_manager.py
@@ -14,7 +14,7 @@ class TestConfigManager(unittest.TestCase):
         config = ConfigManager.get(ConfigManager.PROF_RULE)
         self.assertEqual(len(config.get_data()), 16)
         config = ConfigManager.get(ConfigManager.DATA_PARSERS)
-        self.assertEqual(len(config.sections()), 39)
+        self.assertEqual(len(config.sections()), 42)
         config = ConfigManager.get(ConfigManager.STARS)
         self.assertEqual(config.has_section('AA'), False)
         config = ConfigManager.get(ConfigManager.TABLES)
