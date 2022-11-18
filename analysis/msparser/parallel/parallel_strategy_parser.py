d000c17a29ae00e0e1d7616403d3b85d4219bec6
msprofDeveloper <msprofDeveloper>
2022-11-18 17:09:06 +0800

    parallel strategy
    Match-id-e65359a561d0a09ea44eb54d0e4e0f8930fcaac5

--- analysis/msparser/parallel/parallel_strategy_parser.py
+++ analysis/msparser/parallel/parallel_strategy_parser.py
@@ -4,7 +4,11 @@
 
 import json
 import logging
+import os
+from json import JSONDecodeError
 
+from common_func.common import error
+from common_func.constant import Constant
 from common_func.db_name_constant import DBNameConstant
 from common_func.file_manager import FileOpen
 from common_func.ms_constant.str_constant import StrConstant
@@ -17,6 +21,9 @@ from profiling_bean.prof_enum.data_tag import DataTag
 
 
 class ParallelStrategyParser(IParser, MsMultiProcess):
+    FILE_MAX_SIZE = 9999999
+    FILE_NAME = os.path.basename(__file__)
+
     def __init__(self: any, file_list: dict, sample_config: dict):
         super().__init__(sample_config)
         self._file_list = file_list
@@ -24,23 +31,31 @@ class ParallelStrategyParser(IParser, MsMultiProcess):
         self._parallel_strategy_data = []
 
     def ms_run(self) -> None:
-        self.parse()
-        self.save()
-
-    def parse(self: any) -> None:
         parallel_files = self._file_list.get(DataTag.PARALLEL_STRATEGY, [])
         if not parallel_files:
-            raise ProfException(ProfException.PROF_SYSTEM_EXIT)
+            return
         logging.info("Start to parse parallel strategy data!")
+        self.parse(parallel_files)
+        self.save()
+
+    def parse(self: any, parallel_files: list) -> None:
         parallel_data = ""
         for _parallel_file in parallel_files:
             parallel_file = PathManager.get_data_file_path(self._project_path, _parallel_file)
+            _file_size = os.path.getsize(parallel_file)
+            if _file_size < 0 or _file_size > self.FILE_MAX_SIZE:
+                error(self.FILE_NAME, "Parallel strategy file size exceeds the upper limit.")
+                return
             with FileOpen(parallel_file, 'rt') as _file:
-                parallel_data = parallel_data + _file.file_reader.readline()
-        parallel_data = json.loads(parallel_data).get("config", {})
+                parallel_data = parallel_data + _file.file_reader.read()
+        try:
+            parallel_data = json.loads(parallel_data).get("config", {})
+        except JSONDecodeError:
+            error(self.FILE_NAME, "Invalid parallel strategy data.")
+            return
         if not parallel_data:
-            logging.error("No valid parallel strategy data.")
-            raise ProfException(ProfException.PROF_SYSTEM_EXIT)
+            error(self.FILE_NAME, "Invalid parallel strategy data.")
+            return
         parallel_mode = self._get_parallel_mode(parallel_data.get("parallelType"), parallel_data.get("stage_num"))
         self._parallel_strategy_data.append([parallel_data.get("ai_framework_type"), parallel_data.get("stage_num"),
                                              parallel_data.get("rankId"), parallel_data.get("stageId"),
@@ -49,18 +64,13 @@ class ParallelStrategyParser(IParser, MsMultiProcess):
 
     def save(self: any) -> None:
         if not self._parallel_strategy_data:
-            logging.error('No valid parallel strategy data.')
             return
         with ParallelModel(self._project_path) as _model:
             _model.flush(DBNameConstant.TABLE_PARALLEL_STRATEGY, self._parallel_strategy_data)
 
     def _get_parallel_mode(self: any, parallel_type: str, stage_num: int) -> str:
-        parallel_type = "data-parallel" if not parallel_type else parallel_type
+        parallel_type = StrConstant.DATA_PARALLEL if not parallel_type else parallel_type
         stage_num = 1 if not stage_num else stage_num
         if stage_num > 1:
-            parallel_mode = "pipeline-parallel"
-        elif parallel_type != "data_parallel":
-            parallel_mode = "model-parallel"
-        else:
-            parallel_mode = "data-parallel"
-        return parallel_mode
+            return StrConstant.PIPELINE_PARALLEL
+        return StrConstant.MODEL_PARALLEL if parallel_type != StrConstant.DATA_PARALLEL else parallel_type
