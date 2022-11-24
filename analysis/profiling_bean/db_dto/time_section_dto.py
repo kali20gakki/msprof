431a56eb283f6258b14e9f72e0459e18b0ddc104
msprofDeveloper <msprofDeveloper>
2022-11-24 16:41:27 +0800

    pytnon3.7.5 adapt
    Match-id-202a31fcb871ba8cfc593e7b8601767465d6f5cd

--- analysis/profiling_bean/db_dto/time_section_dto.py
+++ analysis/profiling_bean/db_dto/time_section_dto.py
@@ -9,6 +9,8 @@ class TimeSectionDto:
     def __init__(self: any):
         self._model_id = Constant.DEFAULT_INVALID_VALUE
         self._index_id = Constant.DEFAULT_INVALID_VALUE
+        self._stream_id = Constant.DEFAULT_INVALID_VALUE
+        self._task_id = Constant.DEFAULT_INVALID_VALUE
         self._start_time = Constant.DEFAULT_INVALID_VALUE
         self._end_time = Constant.DEFAULT_INVALID_VALUE
         self._overlap_time = Constant.DEFAULT_INVALID_VALUE
@@ -28,6 +30,22 @@ class TimeSectionDto:
     @index_id.setter
     def index_id(self: any, value: any) -> None:
         self._index_id = value
+
+    @property
+    def stream_id(self: any) -> any:
+        return float(self._stream_id)
+
+    @stream_id.setter
+    def stream_id(self: any, value: any) -> None:
+        self._stream_id = value
+
+    @property
+    def task_id(self: any) -> any:
+        return float(self._task_id)
+
+    @task_id.setter
+    def task_id(self: any, value: any) -> None:
+        self._task_id = value
 
     @property
     def start_time(self: any) -> any:
