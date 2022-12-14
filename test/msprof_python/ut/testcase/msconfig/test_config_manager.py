8a33c9118f78e08bc76ccf362875623c3c843a36
yu-liang-bin <y1416490440@163.com>
2022-12-14 16:00:28 +0800

    config module rename
    Match-id-bed0e84dee96d8c352d31cd0bc50ecb801560567

---
+++ test/msprof_python/ut/testcase/msconfig/test_config_manager.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#  Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+
+import unittest
+
+from msconfig.config_manager import ConfigManager
+
+
+class TestConfigManager(unittest.TestCase):
+    def test_get(self):
+        config = ConfigManager.get(ConfigManager.PROF_CONDITION)
+        self.assertEqual(len(config.get_data()), 27)
+        config = ConfigManager.get(ConfigManager.PROF_RULE)
+        self.assertEqual(len(config.get_data()), 17)
+        config = ConfigManager.get(ConfigManager.DATA_PARSERS)
+        self.assertEqual(len(config.sections()), 43)
+        config = ConfigManager.get(ConfigManager.STARS)
+        self.assertEqual(config.has_section('AA'), False)
+        config = ConfigManager.get(ConfigManager.TABLES)
+        self.assertEqual(len(config.options('BiuCyclesMap')), 9)
+        config = ConfigManager.get(ConfigManager.TABLES_TRAINING)
+        self.assertEqual(config.get('BiuCyclesMap', 'AA'), '')
+        config = ConfigManager.get(ConfigManager.TABLES_OPERATOR)
+        self.assertEqual(len(config.items('SummaryGeMap')), 11)
+        config = ConfigManager.get(ConfigManager.MSPROF_EXPORT_DATA)
+        self.assertEqual(len(config.sections()), 54)
+        config = ConfigManager.get(ConfigManager.DATA_CALCULATOR)
+        self.assertEqual(len(config.sections()), 16)
+        config = ConfigManager.get(ConfigManager.TUNING_RULE)
+        self.assertEqual(len(config.get_data()), 16)
