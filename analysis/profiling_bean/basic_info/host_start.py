855d6166fcddde5b6692e0a64febead61c5bd39f
yu-liang-bin <y1416490440@163.com>
2023-02-20 18:55:42 +0800

    host and device time synchronization
    Match-id-1938972a54cda0df30af98187787ef2b332a7aa2

--- analysis/profiling_bean/basic_info/host_start.py
+++ analysis/profiling_bean/basic_info/host_start.py
@@ -2,28 +2,23 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2020-2021. All rights reserved.
 
+
 class TimerBean:
 
-    def __init__(self, time_dict: dict):
+    def __init__(self, time_dict: dict, host_freq: float):
         self.clock_realtime = time_dict.get("clock_realtime", 0)
-        self.clock_realtime_start = time_dict.get("clock_realtime_start", 0)
-        self.clock_realtime_end = time_dict.get("clock_realtime_end", 0)
         self.clock_monotonic_raw = time_dict.get("clock_monotonic_raw", 0)
-        self.clock_monotonic_raw_start = time_dict.get("clock_monotonic_raw_start", 0)
-        self.clock_monotonic_raw_end = time_dict.get("clock_monotonic_raw_end", 0)
+        self.cntvct = time_dict.get("cntvct", 0)
+        self.cntvct_diff = time_dict.get("cntvct_diff", 0)
+        self._host_freq = host_freq
 
     @property
     def host_wall(self):
-        if self.clock_realtime_start:
-            return int(self.clock_realtime_start) + round(
-                (int(self.clock_realtime_end) - int(self.clock_realtime_start)) / 2)
-        else:
-            return int(self.clock_realtime)
+        return int(self.clock_realtime)
 
     @property
     def host_mon(self):
-        if self.clock_monotonic_raw_start:
-            return int(self.clock_monotonic_raw_start) + round(
-                (int(self.clock_monotonic_raw_end) - int(self.clock_monotonic_raw_start)) / 2)
+        if self.cntvct_diff and self._host_freq:
+            return int(self.clock_monotonic_raw) + round(int(self.cntvct_diff) / self._host_freq)
         else:
             return int(self.clock_monotonic_raw)
