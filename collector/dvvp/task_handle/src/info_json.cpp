1903eb2429bbe561b12a3b197d411cc6f7e1a538
msprofDeveloper <msprofDeveloper>
2023-03-30 21:49:00 +0800

    debug
    Match-id-72d0a4a2a9fc9194d1162c9117cd5fdfd2c6e89f

--- collector/dvvp/task_handle/src/info_json.cpp
+++ collector/dvvp/task_handle/src/info_json.cpp
@@ -485,7 +485,7 @@ void InfoJson::SetPidInfo(SHARED_PTR_ALIA<InfoMain> infoMain, int pid)
             MSPROF_LOGE("Set pid_name failed(failed to open the file for pid_name info), pid=%d", pid);
         }
         int pidNamePos = pidName.find_first_not_of("Name:\t");
-        if (pidNamePos != std::npos) {
+        if (pidNamePos != std::string::npos) {
             pidName = pidName.substr(pidNamePos, pidName.size()-pidNamePos);
         } else {
             MSPROF_LOGE("Set pid_name failed, pid=%d", pid);
