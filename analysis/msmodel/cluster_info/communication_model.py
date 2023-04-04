85178b7d4d828443e6ba4ef8ac5ab86219426b35
yu-liang-bin <y1416490440@163.com>
2023-04-04 17:02:10 +0800

    集群通信分析性能提升（O(n^2) -> O（n))
    Match-id-9bb3d8a347f366ff50224c4c6aa8d9b2ee601649

--- analysis/msmodel/cluster_info/communication_model.py
+++ analysis/msmodel/cluster_info/communication_model.py
@@ -15,24 +15,13 @@ class CommunicationModel(ViewModel):
     def __init__(self, collection_path):
         super().__init__(collection_path, DBNameConstant.DB_HCCL, [])
 
-    def get_hccl_data_by_conditions(self: any, conditions: dict) -> list:
-        """
-        get hccl data by conditions[op_name, iteration]
-        :params conditions: dict contains op_name, iter_start, iter_end
-        :return: communication operators event list
-        """
-        sql = "select * from {0} where first_timestamp < ? and first_timestamp >= ? and op_name = ?". \
-            format(DBNameConstant.TABLE_HCCL_ALL_REDUCE)
-        data = DBManager.fetch_all_data(self.cur, sql,
-                                        (conditions.get('iter_end', 0), conditions.get('iter_start', float('inf')),
-                                         conditions.get('op_name', '')), dto_class=HcclDto)
-        return data
-
-    def get_distinct_op_name(self: any) -> list:
+    def get_all_events_from_db(self: any, conditions: dict) -> list:
         """
         get hccl op names
         :return:
         """
-        sql = "select distinct op_name from {}".format(DBNameConstant.TABLE_HCCL_ALL_REDUCE)
-        data = DBManager.fetch_all_data(self.cur, sql, dto_class=HcclDto)
+        sql = "select * from {0} where first_timestamp < ? and first_timestamp >= ?".\
+            format(DBNameConstant.TABLE_HCCL_ALL_REDUCE)
+        data = DBManager.fetch_all_data(self.cur, sql, (conditions.get('iter_end', 0),
+                                                        conditions.get('iter_start', float('inf'))), dto_class=HcclDto)
         return data
