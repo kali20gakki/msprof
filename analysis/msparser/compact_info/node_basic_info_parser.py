dfc68597e12341b475afd37b9796e36b3f9ad64c
msprofDeveloper <msprofDeveloper>
2023-04-26 18:42:43 +0800

    【问题单号 Defect】AR20230116774175 【修改说明 Modification】【CMD】Profiling支持离线解析GE新数据结构 【修改人 Modifier】 huangtianxing hwx1187758 【评审人 Reviewer】 chenhao c00379919
    Match-id-c7e06da0230a56797b0dc9c5790c8519f20277b5

--- analysis/msparser/compact_info/node_basic_info_parser.py
+++ analysis/msparser/compact_info/node_basic_info_parser.py
@@ -4,6 +4,7 @@
 
 import logging
 import sqlite3
+from enum import IntEnum
 
 from common_func.constant import Constant
 from common_func.hash_dict_constant import HashDictData
@@ -25,21 +26,14 @@ class NodeBasicInfoParser(DataParser, MsMultiProcess):
         super().__init__(sample_config)
         super(DataParser, self).__init__(sample_config)
         self._file_list = file_list
-        self._node_basic_info_data = []
+        self._node_basic_info_data = {}
         self._project_path = sample_config.get(StrConstant.SAMPLE_CONFIG_PROJECT_PATH)
-
-    @staticmethod
-    def read_binary_data(bean_class: any, bean_data: any) -> any:
-        """
-        read binary data
-        """
-        return bean_class().fusion_decode(bean_data)
 
     @staticmethod
     def _get_node_basic_data(bean_data: any) -> list:
         if not bean_data:
             return []
-        return [bean_data.level, bean_data.add_info_type, bean_data.thread_id, bean_data.timestamp,
+        return [bean_data.level, bean_data.data_type, bean_data.thread_id, bean_data.timestamp,
                 bean_data.node_id, bean_data.task_type, bean_data.op_type, bean_data.block_dim,
                 bean_data.mix_block_dim, bean_data.op_flag]
 
@@ -49,9 +43,10 @@ class NodeBasicInfoParser(DataParser, MsMultiProcess):
         """
         basic_info_files = self._file_list.get(DataTag.NODE_BASIC_INFO, [])
         basic_info_files = self.group_aging_file(basic_info_files)
-        for file_list in basic_info_files.values():
-            self._node_basic_info_data.extend(self.parse_bean_data(file_list, StructFmt.NODE_BASIC_INFO_SIZE,
-                                                                   NodeBasicInfoBean, self._get_node_basic_data))
+        for mode, file_list in basic_info_files.items():
+            self._node_basic_info_data[mode] = self.parse_bean_data(file_list, StructFmt.NODE_BASIC_INFO_SIZE,
+                                                                            NodeBasicInfoBean,
+                                                                            self._get_node_basic_data)
 
     def save(self: any) -> None:
         """
@@ -62,7 +57,7 @@ class NodeBasicInfoParser(DataParser, MsMultiProcess):
             return
         model = NodeBasicInfoModel(self._project_path)
         with model as _model:
-            _model.flush(self._transform_fusion_info_data(self._node_basic_info_data))
+            _model.flush(self._transform_node_basic_info_data(self._node_basic_info_data))
 
     def ms_run(self: any) -> None:
         """
@@ -81,13 +76,23 @@ class NodeBasicInfoParser(DataParser, MsMultiProcess):
         except sqlite3.Error as err:
             logging.error(str(err), exc_info=Constant.TRACE_BACK_SWITCH)
 
-    def _transform_fusion_info_data(self: any, data_list: list) -> list:
+    def _transform_node_basic_info_data(self: any, data_dict: dict) -> list:
         hash_dict_data = HashDictData(self._project_path)
         type_hash_dict = hash_dict_data.get_type_hash_dict()
         ge_hash_dict = hash_dict_data.get_ge_hash_dict()
-        for data in data_list:
-            # 1 type hash, 4 node hash, 6 op type
-            data[1] = type_hash_dict.get('node', {}).get(data[1], 'unmatched')
-            data[4] = ge_hash_dict.get(data[4], 'unmatched')
-            data[6] = ge_hash_dict.get(data[6], 'unmatched')
-        return data_list
+        node_basic_info_data = []
+        for mode, data_list in data_dict.items():
+            for data in data_list:
+                # 1 type hash, 4 node hash, 6 op type
+                data[1] = type_hash_dict.get('node', {}).get(data[1], data[1])
+                data[4] = ge_hash_dict.get(data[4], data[4])
+                data[6] = ge_hash_dict.get(data[6], data[6])
+                # aging_file -> dynamic op, unaging_file -> static op
+                data.append(OPType.DYNAMIC_OP.value if mode == 'aging_file' else OPType.STATIC_OP.value)
+            node_basic_info_data.extend(data_list)
+        return node_basic_info_data
+
+
+class OPType(IntEnum):
+    DYNAMIC_OP = 1
+    STATIC_OP = 0
