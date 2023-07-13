376aad9beb2944584ee2d63e6eca52b2d4228bb8
msprofDeveloper <msprofDeveloper>
2023-07-13 18:40:08 +0800

    修复设置msproftx时，未向liteprofiling注册回调的问题
    Match-id-b33357272c0cedb850dbe29f7dfab27c21176bcf

--- collector/dvvp/msprof/engine/src/msprof_callback_impl.cpp
+++ collector/dvvp/msprof/engine/src/msprof_callback_impl.cpp
@@ -214,11 +214,10 @@ int32_t MsprofReporterCallbackImpl(uint32_t moduleId, uint32_t type, VOID_PTR da
 
 int32_t RegisterReporterCallback()
 {
-    if (!Msprof::Engine::MsprofCallbackHandler::reporters_.empty()) {
-        return ACL_SUCCESS;
+    if (Msprof::Engine::MsprofCallbackHandler::reporters_.empty()) {
+        MSPROF_LOGI("MsprofCallbackHandler InitReporters");
+        Msprof::Engine::MsprofCallbackHandler::InitReporters();
     }
-    MSPROF_LOGI("MsprofCallbackHandler InitReporters");
-    Msprof::Engine::MsprofCallbackHandler::InitReporters();
     
     MSPROF_LOGI("Call profRegReporterCallback");
     aclError ret = ProfApiPlugin::instance()->MsprofProfRegReporterCallback(MsprofReporterCallbackImpl);
