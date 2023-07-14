093f925416a6fbbb18d5841a3f6509e59bec9eb2
yu-liang-bin <y1416490440@163.com>
2023-07-14 17:41:30 +0800

    ai_cpu.csv时间单位统一
    Match-id-e04d1bc3cfb25b8cc3b881ae72ebd8dc67f356e4

--- test/msprof_python/ut/testcase/viewer/test_aicpu_viewer.py
+++ test/msprof_python/ut/testcase/viewer/test_aicpu_viewer.py
@@ -10,6 +10,7 @@ from sqlite.db_manager import DBOpen
 from viewer.aicpu_viewer import ParseAiCpuData
 from analyzer.scene_base.profiling_scene import ProfilingScene
 from common_func.constant import Constant
+from common_func.ms_constant.number_constant import NumberConstant
 
 
 sample_config = {"model_id": 1, 'iter_id': 'dasfsd', 'result_dir': 'jasdfjfjs'}
@@ -18,8 +19,8 @@ NAMESPACE = 'viewer.aicpu_viewer'
 
 def test_analysis_aicpu():
     project_path = 'home\\project'
-    headers = ["Timestamp", "Node", "Compute_time(ms)", "Memcpy_time(ms)", "Task_time(ms)",
-               "Dispatch_time(ms)", "Total_time(ms)", "Stream ID", "Task ID"]
+    headers = ["Timestamp", "Node", "Compute_time(us)", "Memcpy_time(us)", "Task_time(us)",
+               "Dispatch_time(us)", "Total_time(us)", "Stream ID", "Task ID"]
     with mock.patch(NAMESPACE + '.PathManager.get_db_path', return_value='ai_cpu.db'), \
             mock.patch(NAMESPACE + '.DBManager.destroy_db_connect'):
         with mock.patch(NAMESPACE + '.DBManager.check_connect_db_path', return_value=(False, False)):
@@ -56,6 +57,32 @@ def test_get_ai_cpu_sql():
             ProfilingScene().init(" ")
             ProfilingScene()._scene = Constant.STEP_INFO
             result = ParseAiCpuData.get_ai_cpu_data("", ITER_RANGE, db_open.db_conn)
+            unittest.TestCase().assertEqual(result, [])
+
+
+def test_get_aicpu_data_missing_ge_data():
+    with DBOpen('test.db') as db_open:
+        with mock.patch(NAMESPACE + '.DBManager.check_tables_in_db', return_value=False), \
+             mock.patch(NAMESPACE + '.DBManager.attach_to_db', return_value=True), \
+             mock.patch(NAMESPACE + '.DBManager.fetch_all_data', return_value=[]), \
+             mock.patch(NAMESPACE + '.MsprofIteration.get_iter_interval', return_value=(1, 2)), \
+             mock.patch(NAMESPACE + '.MsprofIteration.get_iteration_time', return_value=[[0, 1]]):
+            ProfilingScene().init(" ")
+            ProfilingScene()._scene = Constant.STEP_INFO
+            result = ParseAiCpuData._get_aicpu_data_missing_ge_data(db_open.db_conn, ITER_RANGE, "")
+            unittest.TestCase().assertEqual(result, [])
+
+
+def test_get_aicpu_data_with_ge_data():
+    with DBOpen('test.db') as db_open:
+        with mock.patch(NAMESPACE + '.DBManager.attach_to_db', return_value=True), \
+             mock.patch(NAMESPACE + '.DBManager.fetch_all_data', return_value=[]), \
+             mock.patch(NAMESPACE + '.MsprofIteration.get_iter_interval', return_value=(1, 2)), \
+             mock.patch(NAMESPACE + '.MsprofIteration.get_index_id_list_with_index_and_model', return_value=[]), \
+             mock.patch(NAMESPACE + '.MsprofIteration.get_iteration_time', return_value=[[0, 1]]):
+            ProfilingScene().init(" ")
+            ProfilingScene()._scene = Constant.STEP_INFO
+            result = ParseAiCpuData._get_aicpu_data_with_ge_data(db_open.db_conn, ITER_RANGE, "")
             unittest.TestCase().assertEqual(result, [])
 
 
