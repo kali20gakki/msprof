7b62883aabb3a372917d42024551375efab8e9bd
yu-liang-bin <y1416490440@163.com>
2023-08-07 23:27:52 +0800

    AR20230801569040 各表中增加device字段
    Match-id-4ac0bf9b66ad74cbcec42507a0542295a7a6561a

--- analysis/msmodel/runtime/runtime_host_task_model.py
+++ analysis/msmodel/runtime/runtime_host_task_model.py
@@ -27,12 +27,12 @@ class RuntimeHostTaskModel(BaseModel):
         """
         self.insert_data_to_db(table_name, data_list)
 
-    def get_host_tasks(self: any, is_all: bool, model_id: int, iter_id: int) -> list:
+    def get_host_tasks(self: any, is_all: bool, model_id: int, iter_id: int, device_id : int) -> list:
         sql = "select {0}.model_id, {0}.request_id as index_id, {0}.stream_id, {0}.task_id, " \
-              "{0}.context_ids, {0}.batch_id, {0}.task_type, {0}.timestamp from {0} " \
+              "{0}.context_ids, {0}.batch_id, {0}.task_type, {0}.device_id,{0}.timestamp from {0} " \
               "{1} order by {0}.timestamp" \
             .format(DBNameConstant.TABLE_HOST_TASK,
-                    SqlWhereCondition.get_host_select_condition(is_all, model_id, iter_id))
+                    SqlWhereCondition.get_host_select_condition(is_all, model_id, iter_id, device_id))
         host_tasks = DBManager.fetch_all_data(self.cur, sql)
         if not host_tasks:
             logging.error("get host task from %s.%s error",
