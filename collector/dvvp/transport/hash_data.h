91c6faa9bd80d60b814ecbca0723a564992d8aae
yu-liang-bin <y1416490440@163.com>
2023-08-14 22:51:07 +0800

    Support to analysis normally in interrupt scene.
    Match-id-bb92c49be4e888f9cb8e6bdac54f92659e7bfe41

--- collector/dvvp/transport/hash_data.h
+++ collector/dvvp/transport/hash_data.h
@@ -38,17 +38,19 @@ public:
     // for new struct data
     uint64_t GenHashId(const std::string &hashInfo);
     std::string &GetHashInfo(uint64_t hashId);
+    void SaveHashDataRealtime(const uint64_t hashId, const std::string &hashData);
 
 private:
     uint64_t DoubleHash(const std::string &data) const;
     void FillPbData(const std::string &module, int32_t upDevId, const std::string &saveHashData,
-                    SHARED_PTR_ALIA<FileChunkReq> fileChunk);
+                    SHARED_PTR_ALIA<FileChunkReq> fileChunk, bool isLastChunk);
 private:
     bool inited_;
     std::mutex initMutex_;
     std::mutex hashMutex_;
     std::unordered_map<std::string, uint64_t> hashInfoMap_;   // <hashInfo, hashId>
     std::unordered_map<uint64_t, std::string> hashIdMap_;     // <hashId, hashInfo>
+    std::set<uint64_t> savedHashId_;
     std::map<std::string, std::shared_ptr<std::mutex>> hashMapMutex_;       // <module, hashMutex>
     std::map<std::string, std::map<std::string, uint64_t>> hashDataKeyMap_; // <module, <data, hashId>>
     std::map<std::string, std::map<uint64_t, std::string>> hashIdKeyMap_;   // <module, <hashId, data>>
