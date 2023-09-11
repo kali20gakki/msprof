d18ef233bacbdd92c9c7ed94a5e75c5cbc538dfd
msprofDeveloper <msprofDeveloper>
2023-09-11 21:01:16 +0800

    timeline中 host-sys和CANN的时间戳不对齐
    Match-id-f78ea9e464b5da16bb63a4d2518ebff58ce58235

--- analysis/host_prof/host_disk_usage/model/host_disk_usage.py
+++ analysis/host_prof/host_disk_usage/model/host_disk_usage.py
@@ -46,10 +46,8 @@ class HostDiskUsage(HostProfDataBase):
         result = []
         for disk_item in disk_info_list:
             time_mem = {
-                "start": InfoConfReader().trans_into_local_time(
-                            InfoConfReader().time_from_host_syscnt(disk_item[0], NumberConstant.MICRO_SECOND)),
-                "end": InfoConfReader().trans_into_local_time(
-                            InfoConfReader().time_from_host_syscnt(disk_item[1], NumberConstant.MICRO_SECOND)),
+                "start": InfoConfReader().trans_into_local_time(disk_item[0], NumberConstant.NANO_SECOND),
+                "end": InfoConfReader().trans_into_local_time(disk_item[1], NumberConstant.NANO_SECOND),
                 "usage": disk_item[5]
             }
             result.append(time_mem)
