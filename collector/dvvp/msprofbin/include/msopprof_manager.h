756cb9f24b1c2243e262194c3c931842f4104366
msprofDeveloper <msprofDeveloper>
2023-09-20 17:36:31 +0800

    【MindStudio 6.0.RC3】【安全测试】【msprof】【参数校验】msprof_bin.cpp文件中 空指针未校验
    # message auto-generated for no-merge-commit merge:
    merge z30043230 into master
    
    【MindStudio 6.0.RC3】【安全测试】【msprof】【参数校验】msprof_bin.cpp文件中 空指针未校验
    
    Created-by: msprofDeveloper
    
    Match-id-4d255ea0f9041bdb37e95f3bc4f85d771fcf0f67

---
+++ collector/dvvp/msprofbin/include/msopprof_manager.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+ * Description: msopprof manager
+ * Author: zyb
+ * Create: 2023-09-18
+ */
+
+#ifndef MSOPPROF_MANAGER_H
+#define MSOPPROF_MANAGER_H
+#include <vector>
+#include <string>
+#include "utils/utils.h"
+#include "config/config.h"
+#include "singleton/singleton.h"
+
+namespace Analysis {
+namespace Dvvp {
+namespace Msopprof {
+using CONST_CHAR_PTR = const char *;
+using namespace analysis::dvvp::common::config;
+
+class MsopprofManager : public analysis::dvvp::common::singleton::Singleton<MsopprofManager> {
+public:
+    MsopprofManager();
+    ~MsopprofManager() {};
+    void PrintHelp();
+    int MsopprofProcess(int argc, CONST_CHAR_PTR argv[]);
+    MmProcess GetMsopprofPid() { return opProcess_; }
+
+private:
+    bool CheckInputDataValidity(int argc, CONST_CHAR_PTR argv[]) const;
+    bool CheckMsopprofIfExist(int, CONST_CHAR_PTR argv[], std::vector<std::string> &op_argv) const;
+    int ExecMsopprof(std::string path, std::vector<std::string> argsVec);
+private:
+    std::string path_;
+    MmProcess opProcess_ = MSVP_MMPROCESS;
+};
+}
+}
+}
+#endif
