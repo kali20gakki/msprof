475a01eb469755daf6976d351fa9736625f35371
msprofDeveloper <msprofDeveloper>
2023-09-27 22:01:51 +0800

    1971 pcie数据使用最后一条；tx_latency单位改为us
    # message auto-generated for no-merge-commit merge:
    merge so into master
    
    1971 pcie数据使用最后一条；tx_latency单位改为us
    
    Created-by: msprofDeveloper
    
    Match-id-e8ff43cdc098e14f6ea1a3e5d7b533e85bce50f1

--- test/msprof_python/ut/testcase/viewer/test_interconnection_view.py
+++ test/msprof_python/ut/testcase/viewer/test_interconnection_view.py
@@ -1,10 +1,13 @@
 import unittest
 from unittest import mock
 
+from common_func.common import byte_per_us2_mb_pers, ns2_us
 from common_func.db_name_constant import DBNameConstant
 from common_func.msvp_constant import MsvpConstant
 from sqlite.db_manager import DBManager
 from viewer.interconnection_view import InterConnectionView
+from common_func.platform.chip_manager import ChipManager
+from profiling_bean.prof_enum.chip_model import ChipModel
 
 NAMESPACE = 'viewer.interconnection_view'
 configs = {}
@@ -27,7 +30,7 @@ class TestInterConnectionView(unittest.TestCase):
         db_manager.destroy(test_sql)
         self.assertEqual(res[2], 2)
 
-    def test_get_pcie_summary_data_1(self):
+    def test_get_not_chip_v4_pcie_summary_data(self):
         create_sql = "CREATE TABLE IF NOT EXISTS PcieOriginalData" \
                      " (timestamp, device_id, tx_p_bandwidth_min, tx_p_bandwidth_max, " \
                      "tx_p_bandwidth_avg, tx_np_bandwidth_min, tx_np_bandwidth_max, " \
@@ -42,13 +45,38 @@ class TestInterConnectionView(unittest.TestCase):
         test_sql = db_manager.create_table("pcie.db", create_sql, insert_sql, data)
         with mock.patch(NAMESPACE + '.DBManager.check_connect_db_path', return_value=test_sql):
             key = InterConnectionView('', configs)
+            ChipManager().chip_id = ChipModel.CHIP_V2_1_0
             res = key.get_pcie_summary_data()
         test_sql = db_manager.connect_db("pcie.db")
         (test_sql[1]).execute("drop Table PcieOriginalData")
         db_manager.destroy(test_sql)
+        self.assertEqual(res[0], ["Mode", "Min", "Max", "Avg"])
         self.assertEqual(res[2], 7)
 
-    def test_get_pcie_summary_data_2(self):
+    def test_get_chip_v4_pcie_summary_data(self):
+        create_sql = "CREATE TABLE IF NOT EXISTS PcieOriginalData" \
+                     " (timestamp, device_id, tx_p_bandwidth_min, tx_p_bandwidth_max, " \
+                     "tx_p_bandwidth_avg, tx_np_bandwidth_min, tx_np_bandwidth_max, " \
+                     "tx_np_bandwidth_avg, tx_cpl_bandwidth_min, tx_cpl_bandwidth_max, " \
+                     "tx_cpl_bandwidth_avg, tx_np_lantency_min, tx_np_lantency_max, " \
+                     "tx_np_lantency_avg, rx_p_bandwidth_min, rx_p_bandwidth_max, " \
+                     "rx_p_bandwidth_avg, rx_np_bandwidth_min, rx_np_bandwidth_max, " \
+                     "rx_np_bandwidth_avg, rx_cpl_bandwidth_min, rx_cpl_bandwidth_max, rx_cpl_bandwidth_avg)"
+        data = ((176168304972690, 0, 0, 1022, 17, 0, 8, 0, 0, 0, 0, 303, 308, 50, 0, 20, 0, 0, 0, 0, 0, 32, 0),)
+        db_manager = DBManager()
+        insert_sql = db_manager.insert_sql("PcieOriginalData", data)
+        test_sql = db_manager.create_table("pcie.db", create_sql, insert_sql, data)
+        with mock.patch(NAMESPACE + '.DBManager.check_connect_db_path', return_value=test_sql):
+            key = InterConnectionView('', configs)
+            ChipManager().chip_id = ChipModel.CHIP_V4_1_0
+            res = key.get_pcie_summary_data()
+        test_sql = db_manager.connect_db("pcie.db")
+        (test_sql[1]).execute("drop Table PcieOriginalData")
+        db_manager.destroy(test_sql)
+        self.assertEqual(res[0], ["Mode", "Min", "Max", "Avg"])
+        self.assertEqual(res[2], 7)
+
+    def test_get_pcie_summary_data_return_empty_data(self):
         db = DBManager()
         test_sql = db.create_table('test.db')
         db.destroy(test_sql)
