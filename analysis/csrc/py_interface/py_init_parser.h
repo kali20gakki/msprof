fa6b49a1448ae0827afcfdf5395a5cb7c5abf4d1
msprofDeveloper <msprofDeveloper>
2023-11-06 17:58:40 +0800

    【AR20231030191660】 【基础能力】msprof数据解析模块提供python接口支持CANN侧性能数据解析 头文件上库
    Match-id-64825f3e17fc4794e920b9957e0def94e3556030

---
+++ analysis/csrc/py_interface/py_init_parser.h
@@ -0,0 +1,28 @@
+/* ******************************************************************************
+           版权所有 (c) 华为技术有限公司 2023-2023
+           Copyright, 2023, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : py_init_dump_cann_trace.h
+ * Description        : 注册python接口
+ * Author             : msprof team
+ * Creation Date      : 2023/11/2
+ * *****************************************************************************
+ */
+
+#ifndef ANALYSIS_PY_INTERFACE_PY_INIT_PARSER_H
+#define ANALYSIS_PY_INTERFACE_PY_INIT_PARSER_H
+
+#include <Python.h>
+namespace Analysis {
+namespace PyInterface {
+// 该方法供注册python接口使用，用于收集PyMethodDef供注册时挂载module
+// 挂载接口格式如下： Get{模块名}Methods
+PyMethodDef *GetParserMethods();
+
+// 解析落盘CANN数据功能的外层包装，解析python侧传入的路径后调用KernalParserWorker启动解析流程，获取返回状态码后返回python侧
+PyObject *WrapDumpCANNTrace(PyObject *self, PyObject *args);
+} // PyInterface
+} // Analyzer
+
+#endif // ANALYSIS_PY_INTERFACE_PY_INIT_PARSER_H
