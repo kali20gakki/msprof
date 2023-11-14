b8e29777747301f20e0374cfff01506894d7dc5d
msprofDeveloper <msprofDeveloper>
2023-11-14 20:26:53 +0800

    c++ data parser refactor header file
    # message auto-generated for no-merge-commit merge:
    merge cpp into master
    
    c++数据解析头文件
    
    Created-by: msprofDeveloper
    
    Match-id-427f376a000b34e2d0a8d665579a1897037eb5f9

---
+++ analysis/csrc/utils/file.h
@@ -0,0 +1,111 @@
+/* ******************************************************************************
+            版权所有 (c) 华为技术有限公司 2023-2023
+            Copyright, 2023, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : file.h
+ * Description        : 文件处理
+ * Author             : msprof team
+ * Creation Date      : 2023/11/8
+ * *****************************************************************************
+ */
+
+#ifndef ANALYSIS_UTILS_FILE_H
+#define ANALYSIS_UTILS_FILE_H
+
+#include <string>
+#include <fstream>
+#include <vector>
+
+#include "nlohmann/json.hpp"
+#include "error_code.h"
+
+namespace Analysis {
+namespace Utils {
+// 该类主要用于文件相关的处理接口
+// 主要包括以下特性：
+// 1. 空路径、软连接和路径长度的校验
+// 2. Chmod修改文件和目录的权限
+// 3. 文件和目录的rwx权限校验
+// 4. 创建目录、路径拼接、获取子文件名等操作
+class File {
+public:
+    File() = default;
+    virtual ~File() = default;
+    static bool Check(const std::string &path);
+    static bool CreateDir(const std::string& path, const uint32_t &mode = 0750);
+    static void Chmod(const std::string &path, const uint32_t &mode);
+    static std::string PathJoin(const std::vector<std::string>& paths);
+    static std::vector<std::string> GetFilesWithPrefix(const std::string &path, const std::string &prefix);
+    static std::vector<std::string> FilterFileWithSuffix(const std::vector<std::string> &files,
+                                                         const std::string &suffix);
+    static bool FileAccess(const std::string &path, const int &mode);
+    static bool FileExist(const std::string &path);
+    static bool IsSoftLink(const std::string &path);
+    static bool IsFile(const std::string &path);
+    static uint64_t FileSize(const std::string &file);
+};  // class File
+
+// 该类主要用于文件安全读取
+// 主要包括以下特性：
+// 1. 文本文件、二进制文件和json文件的安全读取
+// 2. 安全校验：空路径和软连接的校验，文件校验，文件大小校验，文件读权限校验
+class FileReader {
+public:
+    FileReader() = default;
+    explicit FileReader(const std::string &path, const std::ios_base::openmode &mode = std::ios::in) : path_(path)
+    {
+        Open(path, mode);
+    }
+    virtual ~FileReader()
+    {
+        Close();
+    }
+    void Open(const std::string &path, const std::ios_base::openmode &mode = std::ios::in);
+    void Close();
+    bool IsOpen() const;
+    int ReadBinary(std::stringstream &content);
+    int ReadText(std::vector<std::string> &content);
+    int ReadJson(nlohmann::json &content);
+    size_t GetSize() const;
+    static bool Check(const std::string &path);
+
+private:
+    std::string path_;
+    std::ifstream inStream_;
+    size_t readSize_ = 0;  // 读取的字节数
+};  // class FileReader
+
+// 该类主要用于文件安全写入
+// 主要包括以下特性：
+// 1. 字符串写入文件
+// 2. 安全校验：空路径和软连接的校验，文件校验，文件写权限校验
+// 3. 设置写入文件的rwx权限，默认0640
+class FileWriter {
+public:
+    FileWriter() = default;
+    explicit FileWriter(const std::string &path,
+                        const std::ios_base::openmode &mode = std::ios::out, const mode_t &permission = 0640)
+        : path_(path), permission_(permission)
+    {
+        Open(path, mode);
+    }
+    virtual ~FileWriter()
+    {
+        Close();
+    }
+    void Open(const std::string &path, const std::ios_base::openmode &mode = std::ios::out);
+    void Close();
+    bool IsOpen() const;
+    void WriteText(const std::string& content);
+    static bool Check(const std::string &path);
+
+private:
+    std::string path_;
+    std::ofstream outStream_;
+    mode_t permission_ = 0640;
+};  // class FileWriter
+}  // namespace Utils
+}  // namespace Analysis
+
+#endif // ANALYSIS_UTILS_FILE_H
