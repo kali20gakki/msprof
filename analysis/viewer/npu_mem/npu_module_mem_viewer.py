febc4712701d4e37dfa4cb05d82f07b2db063828
yu-liang-bin <y1416490440@163.com>
2023-11-16 18:05:56 +0800

    组件级内存修改：拆分算子级与组件级数据交付件，对应解析代码及UT修改
    # message auto-generated for no-merge-commit merge:
    merge 930_npu_profiling into master
    
    组件级内存修改：拆分算子级与组件级数据交付件，对应解析代码及UT修改
    
    Created-by: msprofDeveloper
    
    Match-id-6819e25f2c430d897f2749e495115cdf65fbef3d

--- analysis/viewer/npu_mem/npu_module_mem_viewer.py
+++ analysis/viewer/npu_mem/npu_module_mem_viewer.py
@@ -10,18 +10,19 @@ from common_func.ms_constant.number_constant import NumberConstant
 from common_func.ms_constant.str_constant import StrConstant
 from common_func.msvp_constant import MsvpConstant
 from msmodel.npu_mem.npu_ai_stack_mem_model import NpuAiStackMemModel
+from profiling_bean.prof_enum.data_tag import ModuleName
 
 
-class NpuMemRecViewer:
+class NpuModuleMemViewer:
     def __init__(self: any, configs: dict, params: dict) -> None:
         self._configs = configs
         self._params = params
         self._project_path = params.get(StrConstant.PARAM_RESULT_DIR)
         self._data = []
-        self._table = DBNameConstant.TABLE_NPU_OP_MEM_REC
+        self._table = DBNameConstant.TABLE_NPU_MODULE_MEM
         self._model = NpuAiStackMemModel(self._project_path,
-                                         DBNameConstant.DB_MEMORY_OP,
-                                         [DBNameConstant.TABLE_NPU_OP_MEM_REC])
+                                         DBNameConstant.DB_NPU_MODULE_MEM,
+                                         [DBNameConstant.TABLE_NPU_MODULE_MEM])
 
     def get_summary_data(self: any) -> tuple:
         """
@@ -34,15 +35,18 @@ class NpuMemRecViewer:
             return MsvpConstant.MSVP_EMPTY_DATA
         origin_summary_data = self._model.get_table_data(self._table)
         if not origin_summary_data:
-            logging.error("get %s summary data failed in npu memory record viewer, please check.",
+            logging.error("get %s summary data failed in npu module memory viewer, please check.",
                           self._table)
             return MsvpConstant.MSVP_EMPTY_DATA
         for datum in origin_summary_data:
-            self._data.append([datum.component,
+            if datum.module_id == ModuleName.MAX_MOUDLE_ID.value:
+                logging.error("Invalid module id, please check.")
+                return MsvpConstant.MSVP_EMPTY_DATA
+            self._data.append([ModuleName(datum.module_id).name,
                                InfoConfReader().trans_into_local_time(
-                                   InfoConfReader().time_from_host_syscnt(int(datum.timestamp),
+                                   InfoConfReader().time_from_host_syscnt(int(datum.syscnt),
                                                                           NumberConstant.MICRO_SECOND)),
-                               datum.total_allocate_memory / NumberConstant.KILOBYTE,
-                               datum.total_reserve_memory / NumberConstant.KILOBYTE,
-                               datum.device_type])
+                               datum.total_size,
+                               datum.device_type
+                               ])
         return self._configs.get(StrConstant.CONFIG_HEADERS), self._data, len(self._data)
---
+++ test/msprof_python/ut/testcase/viewer/npu_mem/test_npu_module_mem_viewer.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+
+import unittest
+from unittest import mock
+
+from common_func.msvp_constant import MsvpConstant
+from common_func.info_conf_reader import InfoConfReader
+from profiling_bean.db_dto.npu_module_mem_dto import NpuModuleMemDto
+from viewer.npu_mem.npu_module_mem_viewer import NpuModuleMemViewer
+from model.test_dir_cr_base_model import TestDirCRBaseModel
+
+NAMESPACE = 'viewer.npu_mem.npu_module_mem_viewer'
+
+
+class TestNpuModuleMemViewer(TestDirCRBaseModel):
+
+    def get_module_mem_dto(self, module_mem: list) -> list:
+        module_mem_dto = []
+        for data in module_mem:
+            data_dto = NpuModuleMemDto()
+            data_dto.module_id = data[0]
+            data_dto.syscnt = data[1]
+            data_dto.total_size = data[2]
+            data_dto.device_type = data[3]
+            module_mem_dto.append(data_dto)
+        return module_mem_dto
+
+    def test_get_summary_data_should_return_empty_when_model_init_fail(self):
+        config = {"headers": ["component", "timestamp", "total_reserve_memory", "device_type"]}
+        params = {
+            "project": "test_npu_module_mem_view",
+            "model_id": 1,
+            "iter_id": 1
+        }
+        check = NpuModuleMemViewer(config, params)
+        ret = check.get_summary_data()
+        self.assertEqual(MsvpConstant.MSVP_EMPTY_DATA, ret)
+
+    def test_get_summary_data_should_return_empty_when_invalid_module_id_is_included(self):
+        config = {"headers": ["component", "timestamp", "total_reserve_memory", "device_type"]}
+        params = {
+            "project": "test_npu_module_mem_view",
+            "model_id": 1,
+            "iter_id": 1
+        }
+        InfoConfReader()._info_json = {
+            'CPU': [{'Frequency': "1000"}]
+        }
+        InfoConfReader()._local_time_offset = 10.0
+        invalid_module_mem_data = [
+            [73, 0, 4096, 'NPU:0']
+        ]
+        invalid_module_mem_dto = self.get_module_mem_dto(invalid_module_mem_data)
+        with mock.patch(NAMESPACE + '.NpuAiStackMemModel.check_db', return_value=True), \
+                mock.patch(NAMESPACE + '.NpuAiStackMemModel.check_table', return_value=True), \
+                mock.patch(NAMESPACE + '.NpuAiStackMemModel.get_table_data', return_value=invalid_module_mem_dto):
+            check = NpuModuleMemViewer(config, params)
+            ret = check.get_summary_data()
+            self.assertEqual(ret, MsvpConstant.MSVP_EMPTY_DATA)
+
+    def test_get_summary_data_should_return_module_men_data_when_get_module_mem_data(self):
+        config = {"headers": ["component", "timestamp", "total_reserve_memory", "total_allocate_memory", "device_type"]}
+        params = {
+            "project": "test_npu_module_mem_view",
+            "model_id": 1,
+            "iter_id": 1
+        }
+        expected_headers = ['component', 'timestamp', 'total_reserve_memory', 'total_allocate_memory', 'device_type']
+        expected_data = [
+            ['RUNTIME', 10.0, 4096, 'NPU:0'],
+            ['HDC', 11.0, 2048, 'NPU:0']
+        ]
+        InfoConfReader()._info_json = {
+            'CPU': [{'Frequency': "1000"}]
+        }
+        InfoConfReader()._local_time_offset = 10.0
+        module_mem_data = [
+            [7, 0, 4096, 'NPU:0'],
+            [9, 1000, 2048, 'NPU:0']
+        ]
+        module_mem_dto = self.get_module_mem_dto(module_mem_data)
+        with mock.patch(NAMESPACE + '.NpuAiStackMemModel.check_db', return_value=True), \
+                mock.patch(NAMESPACE + '.NpuAiStackMemModel.check_table', return_value=True), \
+                mock.patch(NAMESPACE + '.NpuAiStackMemModel.get_table_data', return_value=module_mem_dto):
+            check = NpuModuleMemViewer(config, params)
+            headers, data, _ = check.get_summary_data()
+            self.assertEqual(headers, expected_headers)
+            self.assertEqual(data, expected_data)
