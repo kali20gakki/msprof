1ec715b90545eabb1ff7521bd44d8197c059e6b9
yu-liang-bin <y1416490440@163.com>
2024-01-15 10:15:16 +0800

    【AR20240110348742】 【基础能力】msprof数据解析模块提供python接口支持CANN侧性能数据解析 db dumper上库
    Match-id-74f6e40fb22abfe5b091099e63c601be0f784b23

---
+++ analysis/csrc/py_interface/py_init_parser.cpp
@@ -0,0 +1,40 @@
+/* ******************************************************************************
+           版权所有 (c) 华为技术有限公司 2023-2023
+           Copyright, 2023, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : py_init_parser.cpp
+ * Description        : 注册dump_cann_trace接口
+ * Author             : msprof team
+ * Creation Date      : 2023/11/2
+ * *****************************************************************************
+ */
+
+#include "analysis/csrc/py_interface/py_init_parser.h"
+#include "analysis/csrc/worker/kernel_parser_worker.h"
+namespace Analysis {
+namespace PyInterface {
+using KernelParserWorker = Analysis::Worker::KernelParserWorker;
+PyMethodDef g_methodTestSchedule[] = {
+    {"dump_cann_trace", WrapDumpCANNTrace, METH_VARARGS, ""},
+    {nullptr, nullptr}
+};
+
+PyMethodDef *GetParserMethods()
+{
+    return g_methodTestSchedule;
+};
+
+PyObject *WrapDumpCANNTrace(PyObject *self, PyObject *args)
+{
+    std::string parseFilePath = nullptr;
+    if (!PyArg_ParseTuple(args, "s", &parseFilePath)) {
+        PyErr_SetString(PyExc_TypeError, "parser.dump_cann_trace args parse failed!");
+        return nullptr;
+    }
+    KernelParserWorker parserWorker(parseFilePath);
+    auto res = parserWorker.Run();
+    return Py_BuildValue("i", res);
+}
+}
+}
