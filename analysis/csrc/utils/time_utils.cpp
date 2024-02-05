4a55abefabee666858fcb863a0146f43d25116d8
msprofDeveloper <msprofDeveloper>
2024-02-05 18:30:56 +0800

    clean code 报警清理
    # message auto-generated for no-merge-commit merge:
    merge cleancode_fix into master
    
    Clean code 告警问题清理
    
    Created-by: msprofDeveloper
    
    Match-id-06fb9594a35cda3b3137cce8bd658c7e70b0f90c

--- analysis/csrc/utils/time_utils.cpp
+++ analysis/csrc/utils/time_utils.cpp
@@ -28,19 +28,19 @@ const int TIME_DISPLAY_WIDTH = 2; // 日期的固定宽度是2，不足的前面
     
 std::string GetFormatLocalTime()
 {
-    time_t t = time(NULL);  // 获取1970年1月1日0点0分0秒到现在经过的秒数
+    time_t t = time(nullptr);  // 获取1970年1月1日0点0分0秒到现在经过的秒数
     struct tm localTm;
     struct tm *tm = localtime_r(&t, &localTm);
 
     std::stringstream sstr;
 
     // 将秒数转换为本地时间,年从1900算起,需要+1900,月为0-11,所以要+1
-    sstr << std::setw(YEAR_DISPLAY_WIDTH) << std::setfill('0') << (tm->tm_year + 1900)
-         << std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << (tm->tm_mon + 1)
-         << std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << tm->tm_mday
-         << std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << tm->tm_hour
-         << std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << tm->tm_min
-         << std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << tm->tm_sec;
+    sstr << std::setw(YEAR_DISPLAY_WIDTH) << std::setfill('0') << (tm->tm_year + 1900) <<
+        std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << (tm->tm_mon + 1) <<
+        std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << tm->tm_mday <<
+        std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << tm->tm_hour <<
+        std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << tm->tm_min <<
+        std::setw(TIME_DISPLAY_WIDTH) << std::setfill('0') << tm->tm_sec;
 
     // 格式：20231129134509  年月日时分秒的拼接字符串
     return sstr.str();
