90892af843aecda8c2876ccbe2fe1a6d6f7432d4
yu-liang-bin <y1416490440@163.com>
2024-02-05 18:03:22 +0800

    fix_is_analyzed_data_func
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    msprof判断是否需要预解析新增是否有sqlite目录
    
    Created-by: msprofDeveloper
    
    Match-id-4c2f1ee0e9a5f27065ef8b87df61bae00c08fabc

--- analysis/common_func/file_manager.py
+++ analysis/common_func/file_manager.py
@@ -74,8 +74,9 @@ class FileManager:
         return if data dir contains all parse complete file
         """
         data_dir = PathManager.get_data_dir(project_path)
+        sqlite_dir = PathManager.get_sql_dir(project_path)
         is_analyse = False
-        if os.path.exists(data_dir):
+        if os.path.exists(data_dir) and os.path.exists(sqlite_dir):
             all_file_name = os.path.join(data_dir, FileNameManagerConstant.ALL_FILE_TAG)
             if os.path.exists(all_file_name):
                 is_analyse = True
