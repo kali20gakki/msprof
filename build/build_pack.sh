18fc346505981934f7216fb59cb38ed265291190
yu-liang-bin <y1416490440@163.com>
2024-04-10 20:33:45 +0800

    调整build目录，清除cleanCode告警
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    【开发自提】【系统调优】调整build目录，解决cleancode告警
    
    Created-by: msprofDeveloper
    
    Match-id-105f66b4b19eb29f0e2d85202717558c23e7ce9d

--- build/build_pack.sh
+++ build/build_pack.sh
@@ -54,23 +54,24 @@ function bep_env_init() {
 bep_env_init
 
 function collector_compile() {
-    cd ${TOP_DIR}/build
-    cmake ../cmake/superbuild/ -DMSPROF_BUILD_TYPE=${BUILD_TYPE}
+    mkdir -p ${TOP_DIR}/build/collector
+    cd ${TOP_DIR}/build/collector
+    cmake ../../cmake/superbuild/ -DMSPROF_BUILD_TYPE=${BUILD_TYPE}
     make -j64
 }
 
 function analysis_compile() {
-    mkdir -p ${TOP_DIR}/analysis/csrc/build
-    cd ${TOP_DIR}/analysis/csrc/build
-    cmake .. -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${TOP_DIR}/analysis
+    mkdir -p ${TOP_DIR}/build/analysis
+    cd ${TOP_DIR}/build/analysis
+    cmake ../../analysis/csrc -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${TOP_DIR}/analysis
     make -j64 && make clean && make install
     if [ $? -ne 0 ]; then
         echo "analysis_compile failed"
         exit 1
     fi
-    cd ${TOP_DIR}/build
 }
 
 collector_compile
 analysis_compile
+cd ${TOP_DIR}/build
 bash ${TOP_DIR}/scripts/create_run_package.sh ${VERSION} ${PACKAGE_TYPE}
