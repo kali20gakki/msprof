5ba86238ff1b53d516faa417b5c42937a8ecf02a
yu-liang-bin <y1416490440@163.com>
2024-04-24 11:12:59 +0800

    【AR20240408421932】Device侧代码C化功能上库：将ProcessControl类改造为p-impl方式
    # message auto-generated for no-merge-commit merge:
    merge 0423_2 into master
    
    【AR20240408421932】Device侧代码C化功能上库：将ProcessControl类改造为p-impl方式
    
    Created-by: msprofDeveloper
    
    Match-id-d3cd20115199c1095cdb38b5f0c93727b4f74d82

--- analysis/csrc/infrastructure/process/process_control.cpp
+++ analysis/csrc/infrastructure/process/process_control.cpp
@@ -49,164 +49,177 @@ void FillStatisticianDependence(const ProcessCollection& regProcess, std::vector
 
 }
 
-/**
- * @brief 拓扑排序算法执行所有Process 使用的算法为卡恩算法
- * @param chipRelatedProcess 相应Chip的Process集合
- */
-bool ProcessControl::RunProcesses(ProcessCollection& chipRelatedProcess, DataInventory& dataInventory,
-                                  const Context& context)
-{
-    auto regProcessCopy = chipRelatedProcess; // 这里COPY一份是为了DFX中填dependProcess字段
+class ProcessControl::Impl final {
+public:
+    explicit Impl(ProcessCollection& processes)
+        : allProcess_(std::move(processes)) {}
+    ~Impl() = default;
 
-    size_t levelIndex = 0;
-    auto preparedProcess = TakeAwayPreparedProcess(chipRelatedProcess);
-    while (!preparedProcess.empty()) {
-        std::vector<ProcessStatistcs> stat(preparedProcess.size());
-        Analysis::Utils::ThreadPool pool(preparedProcess.size());
-        pool.Start();
-        RunPreparedProcess(preparedProcess, stat, pool, dataInventory, context);
-        pool.WaitAllTasks();
-        pool.Stop();
-
-        // DFX
-        FillStatisticianDependence(regProcessCopy, stat);
-        bool dfxStop = false;
-        auto ret = GetStatistician(std::move(stat), dfxStop);
-        if (!ret) {
-            ERROR("mandatory process failed!");
-            return false;
+    bool VerifyProcess(const ProcessCollection& chipRelatedProcess) const
+    {
+        auto verifyProcess = chipRelatedProcess;
+        auto preparedProcess = TakeAwayPreparedProcess(verifyProcess);
+        while (!preparedProcess.empty()) {
+            preparedProcess = TakeAwayPreparedProcess(verifyProcess);
         }
-        if (dfxStop) {
-            PRINT_INFO("!!!!!!! DFX Stops !!!!!!!!");
-            return false;
-        }
-        ReleaseNoLongerUsedData(chipRelatedProcess, dataInventory, levelIndex++);
-        preparedProcess = TakeAwayPreparedProcess(chipRelatedProcess);
+        return verifyProcess.empty();
     }
-    return true;
-}
 
-void ProcessControl::RunPreparedProcess(ProcessCollection &preparedProcess, std::vector<ProcessStatistcs> &stat,
-                                        Analysis::Utils::ThreadPool &pool, DataInventory& dataInventory,
-                                        const Context& context) const
-{
-    size_t concurrentIndex{};
-    for (const auto& processNode : preparedProcess) {
-        pool.AddTask([&processNode, &stat, &dataInventory, &context, concurrentIndex]() {
-            auto startTime = std::chrono::steady_clock::now();
-            if (!processNode.second.creator) {
-                ERROR("creator==nullptr, process name: %", processNode.second.processName);
-                return;
-            }
-            auto proc = processNode.second.creator();
-            if (proc != nullptr) {
-                stat[concurrentIndex].returnCode = proc->Run(dataInventory, context);
-                stat[concurrentIndex].mandatory = processNode.second.mandatory;
-            }
-            auto endTime = std::chrono::steady_clock::now();
+    bool ExecuteProcess(DataInventory& dataInventory, const Context& context)
+    {
+        chipId_ = context.GetChipID();
+        stat_.chipId = chipId_;
 
-            stat[concurrentIndex].startTime = std::chrono::duration_cast<std::chrono::microseconds>(
-                    startTime.time_since_epoch()).count();
-            stat[concurrentIndex].duration = std::chrono::duration_cast<std::chrono::microseconds>(
-                    endTime - startTime).count();
-            stat[concurrentIndex].processName = processNode.second.processName;
-            stat[concurrentIndex].dfxStop = false;
-            const std::string& stopProcessName = context.GetDfxStopAtName();
-            if (!stopProcessName.empty() && stopProcessName == processNode.second.processName) {
-                stat[concurrentIndex].dfxStop = true;
+        ProcessTopo processTopoBuilder(allProcess_);
+        auto chipRelatedProcess = processTopoBuilder.BuildProcessControlTopoByChip(chipId_);
+        if (!VerifyProcess(chipRelatedProcess)) {
+            ERROR("Topo Verify failed!");
+            return false;
+        }
+
+        return RunProcesses(chipRelatedProcess, dataInventory, context);
+    }
+
+    ExecuteProcessStat GetExecuteStat() const
+    {
+        return stat_;
+    }
+
+private:
+    /**
+     * @brief 拓扑排序算法执行所有Process 使用的算法为卡恩算法
+     * @param chipRelatedProcess 相应Chip的Process集合
+     */
+    bool RunProcesses(ProcessCollection& chipRelatedProcess, DataInventory& dataInventory,
+                      const Context& context)
+    {
+        auto regProcessCopy = chipRelatedProcess; // 这里COPY一份是为了DFX中填dependProcess字段
+
+        size_t levelIndex = 0;
+        auto preparedProcess = TakeAwayPreparedProcess(chipRelatedProcess);
+        while (!preparedProcess.empty()) {
+            std::vector<ProcessStatistcs> stat(preparedProcess.size());
+            Analysis::Utils::ThreadPool pool(preparedProcess.size());
+            pool.Start();
+            RunPreparedProcess(preparedProcess, stat, pool, dataInventory, context);
+            pool.WaitAllTasks();
+            pool.Stop();
+
+            // DFX
+            FillStatisticianDependence(regProcessCopy, stat);
+            bool dfxStop = false;
+            auto ret = GetStatistician(std::move(stat), dfxStop);
+            if (!ret) {
+                ERROR("mandatory process failed!");
+                return false;
             }
+            if (dfxStop) {
+                PRINT_INFO("!!!!!!! DFX Stops !!!!!!!!");
+                return false;
+            }
+            ReleaseNoLongerUsedData(chipRelatedProcess, dataInventory, levelIndex++);
+            preparedProcess = TakeAwayPreparedProcess(chipRelatedProcess);
+        }
+        return true;
+    }
+
+    void RunPreparedProcess(ProcessCollection &preparedProcess, std::vector<ProcessStatistcs> &stat,
+                            Analysis::Utils::ThreadPool &pool, DataInventory& dataInventory,
+                            const Context& context) const
+    {
+        size_t concurrentIndex{};
+        for (const auto& processNode : preparedProcess) {
+            pool.AddTask([&processNode, &stat, &dataInventory, &context, concurrentIndex]() {
+                auto startTime = std::chrono::steady_clock::now();
+                if (!processNode.second.creator) {
+                    ERROR("creator==nullptr, process name: %", processNode.second.processName);
+                    return;
+                }
+                auto proc = processNode.second.creator();
+                if (proc != nullptr) {
+                    stat[concurrentIndex].returnCode = proc->Run(dataInventory, context);
+                    stat[concurrentIndex].mandatory = processNode.second.mandatory;
+                }
+                auto endTime = std::chrono::steady_clock::now();
+
+                stat[concurrentIndex].startTime = std::chrono::duration_cast<std::chrono::microseconds>(
+                        startTime.time_since_epoch()).count();
+                stat[concurrentIndex].duration = std::chrono::duration_cast<std::chrono::microseconds>(
+                        endTime - startTime).count();
+                stat[concurrentIndex].processName = processNode.second.processName;
+                stat[concurrentIndex].dfxStop = false;
+                const std::string& stopProcessName = context.GetDfxStopAtName();
+                if (!stopProcessName.empty() && stopProcessName == processNode.second.processName) {
+                    stat[concurrentIndex].dfxStop = true;
+                }
+            });
+            ++concurrentIndex;
+        }
+    }
+
+    bool GetStatistician(std::vector<ProcessStatistcs>&& statistics, bool& dfxStop)
+    {
+        auto generalResult = std::all_of(statistics.begin(), statistics.end(), [](ProcessStatistcs& node) {
+            return (node.returnCode == 0 || !node.mandatory);
         });
-        ++concurrentIndex;
+        dfxStop = std::any_of(statistics.begin(), statistics.end(), [](ProcessStatistcs& node) {
+            return node.dfxStop;
+        });
+        stat_.allLevelStat.emplace_back();
+        auto& oneLevelStat = stat_.allLevelStat.back();
+        oneLevelStat.generalResult = generalResult;
+        oneLevelStat.processStatistcs = std::move(statistics);
+        return generalResult; // Stop on error  这里是否停止还与Process类注册时，注册宏中mandatory字段确定
     }
-}
 
-bool ProcessControl::GetStatistician(std::vector<ProcessStatistcs>&& statistics, bool& dfxStop)
-{
-    auto generalResult = std::all_of(statistics.begin(), statistics.end(), [](ProcessStatistcs& node) {
-        return (node.returnCode == 0 || !node.mandatory);
-    });
-    dfxStop = std::any_of(statistics.begin(), statistics.end(), [](ProcessStatistcs& node) {
-        return node.dfxStop;
-    });
-    stat_.allLevelStat.emplace_back();
-    auto& oneLevelStat = stat_.allLevelStat.back();
-    oneLevelStat.generalResult = generalResult;
-    oneLevelStat.processStatistcs = std::move(statistics);
-    return generalResult; // Stop on error  这里是否停止还与Process类注册时，注册宏中mandatory字段确定
-}
-
-void ProcessControl::ReleaseNoLongerUsedData(const ProcessCollection& chipRelatedProcess, DataInventory& dataInventory,
-                                             size_t levelIndex)
-{
-    std::set<std::type_index> dataTobeUsing;
-    for (const auto& processInfo : chipRelatedProcess) {
-        for (const auto& dataType : processInfo.second.paramTypes) {
-            dataTobeUsing.insert(dataType);
+    void ReleaseNoLongerUsedData(const ProcessCollection& chipRelatedProcess, DataInventory& dataInventory,
+                                 size_t levelIndex)
+    {
+        std::set<std::type_index> dataTobeUsing;
+        for (const auto& processInfo : chipRelatedProcess) {
+            for (const auto& dataType : processInfo.second.paramTypes) {
+                dataTobeUsing.insert(dataType);
+            }
         }
-    }
-    auto removedTypes = dataInventory.RemoveRestData(dataTobeUsing);
+        auto removedTypes = dataInventory.RemoveRestData(dataTobeUsing);
 
-    std::string typeStr;
-    for (const auto& typeInfo : removedTypes) {
-        typeStr += typeInfo.name();
-        typeStr += " ";
-    }
-    INFO("Level[%]Release Data Types: %", levelIndex, typeStr);
-}
-
-ProcessCollection ProcessControl::TakeAwayPreparedProcess(ProcessCollection& chipRelatedProcess) const
-{
-    ProcessCollection preparedProcess;
-    // 先拿走已经没有前向依赖的，即入度为0的节点
-    for (auto it = chipRelatedProcess.begin(); it != chipRelatedProcess.end();) {
-        if (it->second.processDependence.empty()) {
-            preparedProcess.insert({it->first, it->second});
-            it = chipRelatedProcess.erase(it);
-            continue;
+        std::string typeStr;
+        for (const auto& typeInfo : removedTypes) {
+            typeStr += typeInfo.name();
+            typeStr += " ";
         }
-        ++it;
+        INFO("Level[%]Release Data Types: %", levelIndex, typeStr);
     }
 
-    // 再将其它节点中的前向依赖删除，即修正其它节点的入度
-    for (auto& process : chipRelatedProcess) {
-        auto& processDep = process.second.processDependence;
-        for (const auto& preparedNode : preparedProcess) {
-            processDep.erase(std::remove(processDep.begin(), processDep.end(), preparedNode.first),
-                             processDep.end());
+    ProcessCollection TakeAwayPreparedProcess(ProcessCollection& chipRelatedProcess) const
+    {
+        ProcessCollection preparedProcess;
+        // 先拿走已经没有前向依赖的，即入度为0的节点
+        for (auto it = chipRelatedProcess.begin(); it != chipRelatedProcess.end();) {
+            if (it->second.processDependence.empty()) {
+                preparedProcess.insert({it->first, it->second});
+                it = chipRelatedProcess.erase(it);
+                continue;
+            }
+            ++it;
         }
-    }
-    return preparedProcess;
-}
 
-bool ProcessControl::VerifyProcess(const ProcessCollection& chipRelatedProcess) const
-{
-    auto verifyProcess = chipRelatedProcess;
-    auto preparedProcess = TakeAwayPreparedProcess(verifyProcess);
-    while (!preparedProcess.empty()) {
-        preparedProcess = TakeAwayPreparedProcess(verifyProcess);
-    }
-    return verifyProcess.empty();
-}
-
-bool ProcessControl::ExecuteProcess(DataInventory& dataInventory, const Context& context)
-{
-    chipId_ = context.GetChipID();
-    stat_.chipId = chipId_;
-
-    ProcessTopo processTopoBuilder(allProcess_);
-    auto chipRelatedProcess = processTopoBuilder.BuildProcessControlTopoByChip(chipId_);
-    if (!VerifyProcess(chipRelatedProcess)) {
-        ERROR("Topo Verify failed!");
-        return false;
+        // 再将其它节点中的前向依赖删除，即修正其它节点的入度
+        for (auto& process : chipRelatedProcess) {
+            auto& processDep = process.second.processDependence;
+            for (const auto& preparedNode : preparedProcess) {
+                processDep.erase(std::remove(processDep.begin(), processDep.end(), preparedNode.first),
+                                 processDep.end());
+            }
+        }
+        return preparedProcess;
     }
 
-    return RunProcesses(chipRelatedProcess, dataInventory, context);
-}
-
-ExecuteProcessStat ProcessControl::GetExecuteStat() const
-{
-    return stat_;
-}
+private:
+    ProcessCollection allProcess_;
+    ExecuteProcessStat stat_;  // dfx: 统计运行结果
+    uint32_t chipId_{};  // dfx: 记录运行什么芯片ID
+};
 
 void RecordProcessStat(const ExecuteProcessStat& stat, const std::string& subDir, std::string& log)
 {
@@ -237,6 +250,39 @@ void RecordProcessStat(const ExecuteProcessStat& stat, const std::string& subDir
     }
     ss << std::endl;
     log += ss.str();
+}
+
+ProcessControl::ProcessControl(ProcessCollection& processes)
+    : impl_(new Impl(processes)) {}
+
+ProcessControl::~ProcessControl() = default;
+
+bool ProcessControl::ExecuteProcess(DataInventory& dataInventory, const Context& context)
+{
+    if (!impl_) {
+        ERROR("ProcessControl Impl create failed!");
+        return false;
+    };
+    return impl_->ExecuteProcess(dataInventory, context);
+}
+
+// 获取运行结果 key为level, value为统计结构
+ExecuteProcessStat ProcessControl::GetExecuteStat() const
+{
+    if (!impl_) {
+        ERROR("ProcessControl Impl create failed!");
+        return {};
+    };
+    return impl_->GetExecuteStat();
+}
+
+bool ProcessControl::VerifyProcess(const ProcessCollection& chipRelatedProcess) const
+{
+    if (!impl_) {
+        ERROR("ProcessControl Impl create failed!");
+        return false;
+    };
+    return impl_->VerifyProcess(chipRelatedProcess);
 }
 
 }
