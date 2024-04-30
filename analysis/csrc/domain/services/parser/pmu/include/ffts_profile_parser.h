d49bd939327f5a4caae61b475cdd1049d6127358
msprofDeveloper <msprofDeveloper>
2024-04-30 11:21:58 +0800

    Device C化，PMU数据parser头文件
    # message auto-generated for no-merge-commit merge:
    merge device_c into master
    
    【AR20240408421929】PMU数据解析头文件
    
    Created-by: msprofDeveloper
    
    Match-id-14c827284d3bf3d9d7abd1be3fb0d1f2b4ff1c73

---
+++ analysis/csrc/domain/services/parser/pmu/include/ffts_profile_parser.h
@@ -0,0 +1,43 @@
+/* ******************************************************************************
+            版权所有 (c) 华为技术有限公司 2024-2024
+            Copyright, 2024, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : ffts_profile_parser.h
+ * Description        : ffts_profile类型二进制数据解析
+ * Author             : msprof team
+ * Creation Date      : 2024/4/26
+ * *****************************************************************************
+ */
+
+#ifndef ANALYSIS_DOMAIN_SERVICE_PARSER_PMU_FFTS_PROFILE_PARSER_H
+#define ANALYSIS_DOMAIN_SERVICE_PARSER_PMU_FFTS_PROFILE_PARSER_H
+
+#include <vector>
+#include "analysis/csrc/domain/services/parser/parser.h"
+#include "analysis/csrc/domain/entities/hal/include/hal_pmu.h"
+
+namespace Analysis {
+
+namespace Domain {
+
+class FftsProfileParser : public Parser {
+private:
+    uint32_t ParseDataItem(void *binaryData, uint32_t binaryDataSize, HalPmuData &data);
+
+    std::vector<std::string> GetFilePattern() override;
+
+    uint32_t GetTrunkSize() override;
+
+    uint32_t ParseData(Infra::DataInventory &dataInventory) override;
+
+    uint32_t GetNoFileCode() override;
+
+private:
+    std::vector<HalPmuData> halUniData_;
+    std::vector<std::string> filePrefix_{"ffts_profile"};
+};
+
+}
+}
+#endif // ANALYSIS_DOMAIN_SERVICE_PARSER_PMU_FFTS_PROFILE_PARSER_H
