4dd726f6a93dc4b9b26e2f23107af5009eb59af3
yu-liang-bin <y1416490440@163.com>
2024-05-07 19:06:29 +0800

    去除时间为0的aicpu node数据
    # message auto-generated for no-merge-commit merge:
    merge aicpu_parser_20240507 into master
    
    去除时间为0的aicpu node数据
    
    Created-by: msprofDeveloper
    
    Match-id-ac5724cfe14895adcc134dd168f28aff0246eaf3

--- test/msprof_python/ut/testcase/msparser/add_info/test_aicpu_add_info_parser.py
+++ test/msprof_python/ut/testcase/msparser/add_info/test_aicpu_add_info_parser.py
@@ -54,7 +54,26 @@ class TestAicpuAddInfoParser(unittest.TestCase):
         ]
         check.save()
 
-    def test_parse_should_return_aicpu_node_data_when_type_0(self):
+    def test_parse_should_return_aicpu_node_data_when_type_0_and_start_time_not_0(self):
+        InfoConfReader()._info_json = {"DeviceInfo": [{'hwts_frequency': 100}]}
+        aicpu_data = (
+            23130, 6000, 0, 1, 128, 2000,
+            1, 2, 0, 0, 1000, 10000, 10000, 15000,
+            20000, 30000, 0, 0, 40000, 40000, 40000, 40000, 2, 123, 456, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        )
+        struct_data = struct.pack(StructFmt.AI_CPU_NODE_ADD_FMT, *aicpu_data)
+        data = AicpuAddInfoBean.decode(struct_data)
+        with mock.patch(NAMESPACE + '.AicpuAddInfoParser.parse_bean_data', return_value=[data]):
+            check = AicpuAddInfoParser(self.file_list, CONFIG)
+            check.parse()
+            check.save()
+        data = check._aicpu_data.get(AicpuAddInfoBean.AICPU_NODE, [])
+        self.assertEqual(1, len(data))
+        self.assertEqual(0.123, data[0][8])
+        InfoConfReader()._info_json = {}
+
+    def test_parse_should_return_aicpu_node_data_when_type_0_and_start_time_0(self):
         InfoConfReader()._info_json = {"DeviceInfo": [{'hwts_frequency': 100}]}
         aicpu_data = (
             23130, 6000, 0, 1, 128, 2000,
@@ -69,8 +88,7 @@ class TestAicpuAddInfoParser(unittest.TestCase):
             check.parse()
             check.save()
         data = check._aicpu_data.get(AicpuAddInfoBean.AICPU_NODE, [])
-        self.assertEqual(1, len(data))
-        self.assertEqual(0.123, data[0][8])
+        self.assertEqual(0, len(data))
         InfoConfReader()._info_json = {}
 
     def test_parse_should_return_aicpu_dp_data_when_type_1(self):
