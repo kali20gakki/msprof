5b565e7734a69674b6a9b41983344445898e6f95
msprofDeveloper <msprofDeveloper>
2024-08-20 14:25:50 +0800

    AscendTask数据处理
    # message auto-generated for no-merge-commit merge:
    merge device_c into master
    
    【AR20240703476911】ascendTask数据loader与formatter上库
    
    Created-by: msprofDeveloper
    
    Match-id-2156461f743a15ee77f341042882444d77988dff

---
+++ analysis/csrc/domain/data_process/ai_task/task_processor.h
@@ -0,0 +1,39 @@
+/* ******************************************************************************
+            版权所有 (c) 华为技术有限公司 2024-2024
+            Copyright, 2024, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : task_processor.h
+ * Description        : 处理ascendTask表数据
+ * Author             : msprof team
+ * Creation Date      : 2024/8/2
+ * *****************************************************************************
+ */
+
+#ifndef ANALYSIS_DOMAIN_ASCEND_TASK_PROCESSOR_H
+#define ANALYSIS_DOMAIN_ASCEND_TASK_PROCESSOR_H
+
+#include "analysis/csrc/domain/data_process/data_processor.h"
+#include "analysis/csrc/domain/entities/viewer_data/ai_task/include/ascend_task_data.h"
+
+namespace Analysis {
+namespace Domain {
+// start_time, duration, model_id, index_id, stream_id, task_id, context_id, batch_id, connection_id host_task_type,
+// device_task_type
+using OriAscendTaskData = std::vector<std::tuple<double, double, uint32_t, int32_t, uint32_t, uint32_t, uint32_t,
+                                                 uint32_t, uint32_t, std::string, std::string>>;
+class TaskProcessor : public DataProcessor {
+public:
+    TaskProcessor() = default;
+    explicit TaskProcessor(const std::string &profPath);
+private:
+    bool Process(DataInventory& dataInventory) override;
+    OriAscendTaskData LoadData(const DBInfo &ascendTaskDB, const std::string &dbPath);
+    std::vector<AscendTaskData> FormatData(const OriAscendTaskData &oriData,
+                                           const Utils::ProfTimeRecord &timeRecord,
+                                           const uint16_t deviceId);
+};
+}
+}
+
+#endif // ANALYSIS_DOMAIN_ASCEND_TASK_PROCESSOR_H
---
+++ analysis/csrc/domain/entities/viewer_data/ai_task/include/ascend_task_data.h
@@ -0,0 +1,52 @@
+/* ******************************************************************************
+            版权所有 (c) 华为技术有限公司 2024-2024
+            Copyright, 2024, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : ascendTask_data.h
+ * Description        : ascendTask与msprofTx数据格式化后的数据结构
+ * Author             : msprof team
+ * Creation Date      : 2024/8/2
+ * *****************************************************************************
+ */
+
+#ifndef ANALYSIS_DOMAIN_ASCENDTASK_DATA_H
+#define ANALYSIS_DOMAIN_ASCENDTASK_DATA_H
+
+#include <cstdint>
+#include <string>
+
+namespace Analysis {
+namespace Domain {
+const std::string TASK_TYPE_MSTX = "MsTx";
+struct AscendTaskData {
+    uint16_t deviceId = UINT16_MAX;
+    uint32_t modelId = UINT32_MAX;
+    int32_t indexId = INT32_MAX;
+    uint32_t streamId = UINT32_MAX;
+    uint32_t taskId = UINT32_MAX;
+    uint32_t contextId = UINT32_MAX;
+    uint32_t batchId = UINT32_MAX;
+    uint32_t connectionId = UINT32_MAX;
+    uint64_t start = UINT64_MAX;
+    double duration = 0.0;
+    std::string hostType;
+    std::string deviceType;
+};
+
+struct MsprofTxDeviceData {
+    uint16_t deviceId = UINT16_MAX;
+    uint32_t modelId = UINT32_MAX;
+    int32_t indexId = INT32_MAX;
+    uint32_t streamId = UINT32_MAX;
+    uint32_t taskId = UINT32_MAX;
+    uint32_t contextId = UINT32_MAX;
+    uint32_t batchId = 0;
+    uint32_t connectionId = UINT32_MAX;
+    uint64_t start = UINT64_MAX;
+    double duration = 0.0;
+    std::string taskType = TASK_TYPE_MSTX;
+};
+}
+}
+#endif // ANALYSIS_DOMAIN_ASCENDTASK_DATA_H
