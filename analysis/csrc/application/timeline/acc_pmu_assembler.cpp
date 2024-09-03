baa482d03a1f09b0cc4fa9453a25f9df13174427
msprofDeveloper <msprofDeveloper>
2024-09-03 22:09:17 +0800

    硬件数据数据json落盘：ddr和acc pmu
    # message auto-generated for no-merge-commit merge:
    merge feature into master
    
    硬件数据数据json落盘：ddr和acc pmu
    
    Created-by: msprofDeveloper
    
    Match-id-ca0244bf77454c19fdd081646e02ee058965c091

---
+++ analysis/csrc/application/timeline/acc_pmu_assembler.cpp
@@ -0,0 +1,92 @@
+/* ******************************************************************************
+            版权所有 (c) 华为技术有限公司 2024-2024
+            Copyright, 2024, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : acc_pmu_assembler.cpp
+ * Description        : 组合Acc PMU层数据
+ * Author             : msprof team
+ * Creation Date      : 2024/8/29
+ * *****************************************************************************
+ */
+
+#include "analysis/csrc/application/timeline/acc_pmu_assembler.h"
+#include "analysis/csrc/domain/entities/viewer_data/system/include/acc_pmu_data.h"
+#include "analysis/csrc/parser/environment/context.h"
+#include "analysis/csrc/viewer/database/finals/unified_db_constant.h"
+
+namespace Analysis {
+namespace Application {
+using namespace Analysis::Parser::Environment;
+using namespace Analysis::Viewer::Database;
+using namespace Analysis::Infra;
+namespace {
+const std::vector<std::string> COUNTERS {"read_bandwidth", "read_ost", "write_bandwidth", "write_ost"};
+const std::string ACC_ID = "acc_id";
+const std::string VALUE = "value";
+}
+
+AccPmuAssembler::AccPmuAssembler() : JsonAssembler(PROCESS_ACC_PMU, {{"msprof", FileCategory::MSPROF}}) {}
+
+void GenerateAccPmuTrace(std::vector<AccPmuData> &accPmuData, const std::unordered_map<uint16_t, uint32_t> &pidMap,
+                         std::vector<std::shared_ptr<TraceEvent>> &res)
+{
+    std::shared_ptr<CounterEvent> event;
+    std::string time;
+    std::string accId;
+    uint32_t pid;
+    std::unordered_map<std::string, std::shared_ptr<CounterEvent>> eventMap;
+    for (const auto &data : accPmuData) {
+        time = std::to_string(data.timestampNs / NS_TO_US);
+        pid = pidMap.at(data.deviceId);
+        accId = std::to_string(data.accId);
+        std::vector<uint32_t> level {data.readBwLevel, data.readOstLevel, data.writeBwLevel, data.writeOstLevel};
+        for (size_t i = 0; i < level.size(); i++) {
+            std::string tmpKey = COUNTERS[i] + time + accId;
+            if (eventMap.find(tmpKey) != eventMap.end()) {
+                eventMap[tmpKey]->SetSeriesValue(ACC_ID, data.accId);
+                eventMap[tmpKey]->SetSeriesValue(VALUE, level[i]);
+            } else {
+                MAKE_SHARED_RETURN_VOID(event, CounterEvent, pid, DEFAULT_TID, time, COUNTERS[i]);
+                event->SetSeriesValue(ACC_ID, data.accId);
+                event->SetSeriesValue(VALUE, level[i]);
+                eventMap[tmpKey] = event;
+            }
+        }
+    }
+    for (auto &counterEvent: eventMap) {
+        res.push_back(counterEvent.second);
+    }
+}
+
+uint8_t AccPmuAssembler::AssembleData(DataInventory &dataInventory, JsonWriter &ostream, const std::string &profPath)
+{
+    auto accPmuData = dataInventory.GetPtr<std::vector<AccPmuData>>();
+    if (accPmuData == nullptr) {
+        WARN("Can't get accPmuData from dataInventory");
+        return DATA_NOT_EXIST;
+    }
+    std::unordered_map<uint16_t, uint32_t> pidMap;
+    auto layerInfo = GetLayerInfo(PROCESS_ACC_PMU);
+    auto deviceList = File::GetFilesWithPrefix(profPath, DEVICE_PREFIX);
+    for (const auto& devicePath: deviceList) {
+        auto deviceId = GetDeviceIdByDevicePath(devicePath);
+        auto pid = Context::GetInstance().GetPidFromInfoJson(deviceId, profPath);
+        uint32_t formatPid = JsonAssembler::GetFormatPid(pid, layerInfo.sortIndex, deviceId);
+        pidMap[deviceId] = formatPid;
+    }
+    GenerateHWMetaData(pidMap, layerInfo, res_);
+    GenerateAccPmuTrace(*accPmuData, pidMap, res_);
+    if (res_.empty()) {
+        ERROR("Can't Generate any Acc PMU process data");
+        return ASSEMBLE_FAILED;
+    }
+    for (const auto &node : res_) {
+        node->DumpJson(ostream);
+    }
+    // 为了让下一个写入的内容形成正确的JSON格式，需要补一个","
+    ostream << ",";
+    return ASSEMBLE_SUCCESS;
+}
+}
+}
