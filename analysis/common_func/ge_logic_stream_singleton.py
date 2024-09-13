daaef020bac0d71be6e81225a8fb0f90966f8efe
msprofDeveloper <msprofDeveloper>
2024-09-13 14:33:00 +0800

    修复logicStream计算过程中重复数据频繁打印日志问题
    # message auto-generated for no-merge-commit merge:
    merge master into master
    
    【DTS2024091212098】【开发自提】【msprof】修复logicStream计算过程中重复数据频繁打印日志问题
    
    Created-by: msprofDeveloper
    
    Match-id-f0566cc0d2be06398b0b15beb498fdfd7edd2d02

--- analysis/common_func/ge_logic_stream_singleton.py
+++ analysis/common_func/ge_logic_stream_singleton.py
@@ -38,14 +38,9 @@ class GeLogicStreamSingleton:
         for mspping in stream_mapping_list:
             self.add_stream_id_mapping(mspping[0], mspping[1])
 
-    def add_stream_id_mapping(self, physic_stream, logic_stream) -> bool:
+    def add_stream_id_mapping(self, physic_stream, logic_stream):
         if physic_stream <= self.max_length and self.stream_id_mapping[physic_stream] is None:
             self.stream_id_mapping[physic_stream] = logic_stream
-            return True
-        else:
-            logging.warning("logic_stream is reporting duplicate data," 
-                            "please check the accuracy of the information.")
-            return False
 
     def get_ge_logic_stream_data(self: any) -> list:
         ge_stream_result = []
@@ -56,7 +51,7 @@ class GeLogicStreamSingleton:
             self.use_flag = False
             return ge_stream_result
 
-        sql = "SELECT physic_stream, logic_stream FROM {}".format(DBNameConstant.TABLE_GE_LOGIC_STREAM_INFO)
+        sql = "SELECT distinct physic_stream, logic_stream FROM {}".format(DBNameConstant.TABLE_GE_LOGIC_STREAM_INFO)
         ge_stream_result = DBManager.fetch_all_data(ge_curs, sql)
         sql = "SELECT MAX(physic_stream) FROM {}".format(DBNameConstant.TABLE_GE_LOGIC_STREAM_INFO)
         max_physic_stream = DBManager.fetchone(ge_curs, sql)
