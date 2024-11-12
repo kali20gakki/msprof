1d1e52a34852ee78f609901f4619482e3e971db8
yu-liang-bin <y1416490440@163.com>
2024-11-12 21:38:26 +0800

    viewer C化C++统一拉起调度代码
    # message auto-generated for no-merge-commit merge:
    merge viewer_ccc into master
    
    【viewer】新增C化代码对外暴露的接口实现
    
    Created-by: msprofDeveloper
    
    Match-id-be0d8250697575021c8b9f64c8f41e839acf74cb

---
+++ analysis/csrc/application/timeline/timeline_manager.cpp
@@ -0,0 +1,138 @@
+/* ******************************************************************************
+            版权所有 (c) 华为技术有限公司 2024-2024
+            Copyright, 2024, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : timeline_manager.cpp
+ * Description        : timeline_manager，timeline导出调度
+ * Author             : msprof team
+ * Creation Date      : 2024/9/3
+ * *****************************************************************************
+ */
+
+#include "analysis/csrc/application/timeline/timeline_manager.h"
+#include <atomic>
+#include "analysis/csrc/application/timeline/timeline_factory.h"
+#include "analysis/csrc/utils/thread_pool.h"
+#include "analysis/csrc/domain/entities/viewer_data/ai_task/include/step_trace_data.h"
+#include "analysis/csrc/domain/entities/viewer_data/ai_task/include/msprof_tx_host_data.h"
+
+namespace Analysis {
+namespace Application {
+namespace {
+const std::string PREFIX_CONTEXT = "[";
+const std::string SUFFIX_CONTEXT = "]";
+const int JSON_FILE_OFFSET = -1;
+
+const std::vector<std::string> ASSEMBLER_LIST{
+    PROCESS_TASK,
+    PROCESS_ACC_PMU,
+    PROCESS_API,
+    PROCESS_DDR,
+    PROCESS_STARS_CHIP_TRANS,
+    PROCESS_HBM,
+    PROCESS_HCCL,
+    PROCESS_HCCS,
+    PROCESS_NETWORK_USAGE,
+    PROCESS_DISK_USAGE,
+    PROCESS_MEMORY_USAGE,
+    PROCESS_CPU_USAGE,
+    PROCESS_MSPROFTX,
+    PROCESS_NPU_MEM,
+    PROCESS_OVERLAP_ANALYSE,
+    PROCESS_PCIE,
+    PROCESS_SIO,
+    PROCESS_STARS_SOC,
+    PROCESS_STEP_TRACE,
+    PROCESS_AI_CORE_FREQ,
+    PROCESS_LLC,
+    PROCESS_NIC,
+    PROCESS_ROCE,
+    PROCESS_QOS
+};
+}
+
+bool TimelineManager::ProcessTimeLine(Analysis::Infra::DataInventory& dataInventory)
+{
+    const uint16_t tableProcessors = 10; // 最多有五个线程
+    Analysis::Utils::ThreadPool pool(tableProcessors);
+    pool.Start();
+    std::atomic<bool> retFlag(true);
+    for (const auto &name : ASSEMBLER_LIST) {
+        pool.AddTask([this, &name, &retFlag, &dataInventory]() {
+            auto assembler = TimelineFactory::GetAssemblerByName(name);
+            if (assembler == nullptr) {
+                ERROR("% is not defined", name);
+                retFlag = false;
+                return;
+            }
+            retFlag = assembler->Run(dataInventory, profPath_) && retFlag;
+        });
+    }
+    pool.WaitAllTasks();
+    pool.Stop();
+    if (!retFlag) {
+        ERROR("The % for json assemble failed to be executed.", profPath_);
+        PRINT_ERROR("The % for json assemble failed to be executed. "
+                    "Please check msprof_analysis_log in outputPath for more info.", profPath_);
+        return false;
+    }
+    return true;
+}
+
+void TimelineManager::WriteFile(const std::string &filePrefix, FileCategory category, const char *content)
+{
+    auto tempFile = filePrefix;
+    tempFile.append("_").append(timestampStr).append(JSON_SUFFIX);
+    auto filePath = File::PathJoin({outputPath_, tempFile});
+    DumpTool::WriteToFile(filePath, content, PREFIX_CONTEXT.size(), category);
+    fileType_.emplace(category, filePath);
+}
+
+bool TimelineManager::PreDumpJson(DataInventory &dataInventory)
+{
+    if (dataInventory.Empty()) {
+        return false;
+    }
+    WriteFile(MSPROF_JSON_FILE, FileCategory::MSPROF, PREFIX_CONTEXT.c_str());
+    if (dataInventory.GetPtr<std::vector<TrainTraceData>>()) {
+        WriteFile(STEP_TRACE_FILE, FileCategory::STEP, PREFIX_CONTEXT.c_str());
+    }
+    if (dataInventory.GetPtr<std::vector<MsprofTxHostData>>()) {
+        WriteFile(MSPROF_TX_FILE, FileCategory::MSPROF_TX, PREFIX_CONTEXT.c_str());
+    }
+    return true;
+}
+
+void TimelineManager::PostDumpJson()
+{
+    for (const auto &it : fileType_) {
+        // 此处需要覆盖文件末尾的","，实测必须使用in、out、ate三种模式打开文件，才可以实现覆盖写入
+        FileWriter writer(it.second, std::ios::in | std::ios::out | std::ios::ate);
+        writer.WriteTextBack(SUFFIX_CONTEXT, JSON_FILE_OFFSET);
+    }
+}
+
+bool TimelineManager::Run(DataInventory &dataInventory)
+{
+    INFO("Start exporting timeline!");
+    PRINT_INFO("Start exporting the timeline!");
+    if (!PreDumpJson(dataInventory)) {
+        ERROR("Can't Get data from dataInventory after data process");
+        PRINT_ERROR("Can't export timeline, msprof_analysis_log in outputPath for more info");
+        return false;
+    }
+    bool runFlag = ProcessTimeLine(dataInventory);
+    PostDumpJson();
+    if (!runFlag) {
+        ERROR("The unified timeline process failed to be executed.");
+        PRINT_ERROR("The unified timeline process failed to be executed. "
+                    "Please check msprof_analysis_log in outputPath for more info.");
+        return false;
+    }
+    PRINT_INFO("End exporting timeline output_file. The file is stored in the PROF file.");
+    return true;
+}
+}
+}
+
---
+++ analysis/csrc/domain/data_process/data_processor_factory.cpp
@@ -0,0 +1,108 @@
+/* ******************************************************************************
+            版权所有 (c) 华为技术有限公司 2024-2024
+            Copyright, 2024, Huawei Tech. Co., Ltd.
+****************************************************************************** */
+/* ******************************************************************************
+ * File Name          : data_processor_factory.cpp
+ * Description        : data_processor_factory，processor工厂类
+ * Author             : msprof team
+ * Creation Date      : 2024/9/3
+ * *****************************************************************************
+ */
+
+#include "analysis/csrc/domain/data_process/include/data_processor_factory.h"
+#include "analysis/csrc/domain/data_process/ai_task/api_processor.h"
+#include "analysis/csrc/domain/data_process/ai_task/communication_info_processor.h"
+#include "analysis/csrc/domain/data_process/ai_task/compute_task_info_processor.h"
+#include "analysis/csrc/domain/data_process/ai_task/kfc_comm_processor.h"
+#include "analysis/csrc/domain/data_process/ai_task/kfc_task_processor.h"
+#include "analysis/csrc/domain/data_process/ai_task/msproftx_device_processor.h"
+#include "analysis/csrc/domain/data_process/ai_task/msproftx_host_processor.h"
+#include "analysis/csrc/domain/data_process/ai_task/step_trace_processor.h"
+#include "analysis/csrc/domain/data_process/ai_task/task_processor.h"
+#include "analysis/csrc/domain/data_process/system/acc_pmu_processor.h"
+#include "analysis/csrc/domain/data_process/system/aicore_freq_processor.h"
+#include "analysis/csrc/domain/data_process/system/chip_trans_processor.h"
+#include "analysis/csrc/domain/data_process/system/ddr_processor.h"
+#include "analysis/csrc/domain/data_process/system/hbm_processor.h"
+#include "analysis/csrc/domain/data_process/system/hccs_processor.h"
+#include "analysis/csrc/domain/data_process/system/host_usage_processor.h"
+#include "analysis/csrc/domain/data_process/system/llc_processor.h"
+#include "analysis/csrc/domain/data_process/system/npu_mem_processor.h"
+#include "analysis/csrc/domain/data_process/system/pcie_processor.h"
+#include "analysis/csrc/domain/data_process/system/sio_processor.h"
+#include "analysis/csrc/domain/data_process/system/soc_bandwidth_processor.h"
+#include "analysis/csrc/domain/data_process/system/sys_io_processor.h"
+#include "analysis/csrc/domain/data_process/system/qos_processor.h"
+
+namespace Analysis {
+namespace Domain {
+std::unordered_map<std::string, ProcessorCreator> DataProcessorFactory::processorTable_{
+    {PROCESSOR_NAME_API, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, ApiProcessor, profPath);}},
+    {PROCESSOR_NAME_COMMUNICATION, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, CommunicationInfoProcessor, profPath);}},
+    {PROCESSOR_NAME_COMPUTE_TASK_INFO, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, ComputeTaskInfoProcessor, profPath);}},
+    {PROCESSOR_NAME_KFC_TASK, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, KfcTaskProcessor, profPath);}},
+    {PROCESSOR_NAME_KFC_COMM, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, KfcCommProcessor, profPath);}},
+    {PROCESSOR_NAME_DEVICE_TX, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, MsprofTxDeviceProcessor, profPath);}},
+    {PROCESSOR_NAME_MSTX, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, MsprofTxHostProcessor, profPath);}},
+    {PROCESSOR_NAME_STEP_TRACE, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, StepTraceProcessor, profPath);}},
+    {PROCESSOR_NAME_TASK, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, TaskProcessor, profPath);}},
+    {PROCESSOR_NAME_ACC_PMU, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, AccPmuProcessor, profPath);}},
+    {PROCESSOR_NAME_AICORE_FREQ, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, AicoreFreqProcessor, profPath);}},
+    {PROCESSOR_NAME_CHIP_TRAINS, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, ChipTransProcessor, profPath);}},
+    {PROCESSOR_NAME_DDR, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, DDRProcessor, profPath);}},
+    {PROCESSOR_NAME_HBM, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, HBMProcessor, profPath);}},
+    {PROCESSOR_NAME_HCCS, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, HCCSProcessor, profPath);}},
+    {PROCESSOR_NAME_CPU_USAGE, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, HostCpuUsageProcessor, profPath);}},
+    {PROCESSOR_NAME_MEM_USAGE, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, HostMemUsageProcessor, profPath);}},
+    {PROCESSOR_NAME_DISK_USAGE, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, HostDiskUsageProcessor, profPath);}},
+    {PROCESSOR_NAME_NETWORK_USAGE, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, HostNetworkUsageProcessor, profPath);}},
+    {PROCESSOR_NAME_LLC, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, LLcProcessor, profPath);}},
+    {PROCESSOR_NAME_NPU_MEM, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, NpuMemProcessor, profPath);}},
+    {PROCESSOR_NAME_PCIE, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, PCIeProcessor, profPath);}},
+    {PROCESSOR_NAME_SIO, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, SioProcessor, profPath);}},
+    {PROCESSOR_NAME_SOC, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, SocBandwidthProcessor, profPath);}},
+    {PROCESSOR_NAME_NIC, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, NicProcessor, profPath);}},
+    {PROCESSOR_NAME_ROCE, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, RoCEProcessor, profPath);}},
+    {PROCESSOR_NAME_QOS, [](const std::string &profPath, std::shared_ptr<DataProcessor> &processor) {
+        MAKE_SHARED_RETURN_VOID(processor, QosProcessor, profPath);}}
+};
+
+std::shared_ptr<DataProcessor> DataProcessorFactory::GetDataProcessByName(const std::string &profPath,
+                                                                          const std::string &processName)
+{
+    std::shared_ptr<DataProcessor> processor = nullptr;
+    auto it = processorTable_.find(processName);
+    if (it != processorTable_.end()) {
+        it->second(profPath, processor);
+    }
+    return processor;
+}
+}
+}
