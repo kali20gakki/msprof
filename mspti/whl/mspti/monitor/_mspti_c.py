af3de34427f2c981fb5d0c633d7878372b149807
yu-liang-bin <y1416490440@163.com>
2024-11-29 17:30:43 +0800

    mspti_c去除lib前缀
    # message auto-generated for no-merge-commit merge:
    merge mspti_release into master
    
    mspti_c去除lib前缀
    
    Created-by: msprofDeveloper
    
    Match-id-fde4eadea3a7236c239cefde51ab2eb1af71022f

--- mspti/whl/mspti/monitor/_mspti_c.py
+++ mspti/whl/mspti/monitor/_mspti_c.py
@@ -9,20 +9,20 @@ import importlib
 so_path = os.path.join(os.path.dirname(__file__), "..", "lib64")
 sys.path.append(os.path.realpath(so_path))
 
-mspti_C_module = importlib.import_module("libmspti_C")
+mspti_C_module = importlib.import_module("mspti_C")
 _start = mspti_C_module.start
 _stop = mspti_C_module.stop
 _flush_all = mspti_C_module.flush_all
 _flush_period = mspti_C_module.flush_period
 
-mspti_C_mstx_module = importlib.import_module("libmspti_C.mstx")
+mspti_C_mstx_module = importlib.import_module("mspti_C.mstx")
 _mstx_register_cb = mspti_C_mstx_module.registerCB
 _mstx_unregister_cb = mspti_C_mstx_module.unregisterCB
 
-mspti_C_kernel_module = importlib.import_module("libmspti_C.kernel")
+mspti_C_kernel_module = importlib.import_module("mspti_C.kernel")
 _kernel_register_cb = mspti_C_kernel_module.registerCB
 _kernel_unregister_cb = mspti_C_kernel_module.unregisterCB
 
-mspti_C_hccl_module = importlib.import_module("libmspti_C.hccl")
+mspti_C_hccl_module = importlib.import_module("mspti_C.hccl")
 _hccl_register_cb = mspti_C_hccl_module.registerCB
 _hccl_unregister_cb = mspti_C_hccl_module.unregisterCB
