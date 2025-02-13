3000a60fab73760c2f9cbc0e533553ddbf9569ba
yu-liang-bin <y1416490440@163.com>
2025-02-13 16:32:11 +0800

    UT clean code
    # message auto-generated for no-merge-commit merge:
    merge mspti_range_merge into master
    
    mstx_monitor 瞬时mark打点信息问题修复
    
    Created-by: msprofDeveloper
    
    Match-id-4189c4aee479fa22c0f199eead28ae0b5c80c0bf

--- mspti/whl/mspti/monitor/mstx_monitor.py
+++ mspti/whl/mspti/monitor/mstx_monitor.py
@@ -70,7 +70,8 @@ class MstxMonitor(BaseMonitor):
         return range_mark_data
 
     def append_data(self, mark_data: MarkerData):
-        if mark_data.flag == MsptiActivityFlag.MSPTI_ACTIVITY_FLAG_MARKER_INSTANTANEOUS:
+        if (mark_data.flag == MsptiActivityFlag.MSPTI_ACTIVITY_FLAG_MARKER_INSTANTANEOUS or
+                mark_data.flag == MsptiActivityFlag.MSPTI_ACTIVITY_FLAG_MARKER_INSTANTANEOUS_WITH_DEVICE):
             self.mark_user_cb(mark_data)
 
         unique_id = (mark_data.ID, mark_data.source_kind)
