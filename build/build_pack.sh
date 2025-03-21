e86c1df88cef49b3ec41d0576e74151a5d9f37a0
yu-liang-bin <y1416490440@163.com>
2025-03-21 18:16:56 +0800

    升级protobuf版本  
    # message auto-generated for no-merge-commit merge:
    merge protobuf into master
    
    升级protobuf版本  
    
    Created-by: msprofDeveloper
    
    Match-id-9b1c9ac19c2c0d8147c906551218f7886ff8898e

--- build/build_pack.sh
+++ build/build_pack.sh
@@ -5,6 +5,7 @@
 set -e
 CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=${CUR_DIR}/..
+OPENSOURCE_DIR=${TOP_DIR}/opensource
 VERSION=""
 PACKAGE_TYPE=""
 BUILD_TYPE=""
@@ -52,6 +53,30 @@ function bep_env_init() {
 }
 
 bep_env_init
+
+function compile_protobuf() {
+    # protobuf_3.14_change_version.patch来源：https://szv-cr-y.codehub.huawei.com/Turing_Solution/hisi/cmake.git
+    cp ${TOP_DIR}/build/opensource_patch/protobuf_3.14_change_version.patch ${OPENSOURCE_DIR}/protobuf
+    cd ${OPENSOURCE_DIR}/protobuf
+    tar -zxvf protobuf-all-3.14.0.tar.gz
+    cd protobuf-3.14.0
+    ulimit -n 8192
+    patch -p1 < ../0001-add-secure-compile-option-in-Makefile.patch
+    patch -p1 < ../0002-add-secure-compile-fs-check-in-Makefile.patch
+    patch -p1 < ../0003-fix-CVE-2021-22570.patch
+    patch -p1 < ../0004-Improve-performance-of-parsing-unknown-fields-in-Jav.patch
+    patch -p1 < ../0005-fix-CVE-2022-1941.patch
+    patch -p1 < ../0006-fix-CVE-2022-3171.patch
+    patch -p1 < ../0007-add-coverage-compile-option.patch
+    patch -p1 < ../protobuf_3.14_change_version.patch
+    cd ..
+    cp -r protobuf-3.14.0 ${OPENSOURCE_DIR}
+    cd ${OPENSOURCE_DIR}
+    rm -rf protobuf
+    mv protobuf-3.14.0 protobuf
+}
+
+compile_protobuf
 
 mkdir -p ${TOP_DIR}/build/prefix
 cmake -S ${TOP_DIR}/cmake/superbuild/ -B ${TOP_DIR}/build/build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DHITEST=${HI_TEST} -DCMAKE_INSTALL_PREFIX=${TOP_DIR}/build/prefix -DOBJECT=all
