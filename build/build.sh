c3d346da3fb3c4de3ffdf40daa845ba5df00873b
yu-liang-bin <y1416490440@163.com>
2025-03-24 20:05:32 +0800

    本地构建以及跑UT脚本更改
    # message auto-generated for no-merge-commit merge:
    merge protobuf into master
    
    本地构建以及跑UT脚本更改
    
    Created-by: msprofDeveloper
    
    Match-id-c43b382b75adb1282caaa5df79f332cfd2395de6

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,6 @@
 set -e
 CUR_DIR=$(dirname $(readlink -f $0))
 TOP_DIR=${CUR_DIR}/..
-OPENSOURCE_DIR=${TOP_DIR}/opensource
 VERSION=""
 PACKAGE_TYPE=""
 BUILD_TYPE=""
@@ -54,29 +53,7 @@ function bep_env_init() {
 
 bep_env_init
 
-function compile_protobuf() {
-    # protobuf_3.14_change_version.patch来源：https://szv-cr-y.codehub.huawei.com/Turing_Solution/hisi/cmake.git
-    cp ${TOP_DIR}/build/opensource_patch/protobuf_3.14_change_version.patch ${OPENSOURCE_DIR}/protobuf
-    cd ${OPENSOURCE_DIR}/protobuf
-    tar -zxvf protobuf-all-3.14.0.tar.gz
-    cd protobuf-3.14.0
-    ulimit -n 8192
-    patch -p1 < ../0001-add-secure-compile-option-in-Makefile.patch
-    patch -p1 < ../0002-add-secure-compile-fs-check-in-Makefile.patch
-    patch -p1 < ../0003-fix-CVE-2021-22570.patch
-    patch -p1 < ../0004-Improve-performance-of-parsing-unknown-fields-in-Jav.patch
-    patch -p1 < ../0005-fix-CVE-2022-1941.patch
-    patch -p1 < ../0006-fix-CVE-2022-3171.patch
-    patch -p1 < ../0007-add-coverage-compile-option.patch
-    patch -p1 < ../protobuf_3.14_change_version.patch
-    cd ..
-    cp -r protobuf-3.14.0 ${OPENSOURCE_DIR}
-    cd ${OPENSOURCE_DIR}
-    rm -rf protobuf
-    mv protobuf-3.14.0 protobuf
-}
-
-compile_protobuf
+bash ${TOP_DIR}/scripts/thirdparty_patch.sh
 
 # Hi Test
 HI_TEST="off"
