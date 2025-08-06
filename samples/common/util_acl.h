0e12ad6247b2a2a6afabc149873c69dda220e691
yu-liang-bin <y1416490440@163.com>
2025-08-06 14:32:22 +0800

    add mstx samples
    # message auto-generated for no-merge-commit merge:
    merge mstx_samples into master
    
    新增mstx用例
    
    Created-by: msprofDeveloper
    
    Match-id-455b86ac4fc31805ecc86aa1860d918c57924cb7

---
+++ samples/common/util_acl.h
@@ -0,0 +1,37 @@
+/**
+* @file util_acl.h
+*
+* Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#ifndef UTIL_ACL_H_
+#define UTIL_ACL_H_
+
+#include "acl/acl.h"
+#include "common/util_mstx.h"
+
+int Init(int32_t deviceId, aclrtContext* context, aclrtStream* stream)
+{
+    // 初始化系统资源
+    ACL_CALL(aclInit(nullptr));
+    ACL_CALL(aclrtSetDevice(deviceId));
+    ACL_CALL(aclrtCreateContext(context, deviceId));
+    ACL_CALL(aclrtSetCurrentContext(*context));
+    ACL_CALL(aclrtCreateStream(stream));
+    return 0;
+}
+
+int DeInit(int32_t deviceId, aclrtContext* context, aclrtStream* stream)
+{
+    ACL_CALL(aclrtDestroyStream(*stream));
+    ACL_CALL(aclrtDestroyContext(*context));
+    ACL_CALL(aclrtResetDevice(deviceId));
+    ACL_CALL(aclFinalize());
+    return 0;
+}
+
+# endif // UTIL_ACL_H_
