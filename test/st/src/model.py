1c1f089fed25222b17192ae6481ecba76ed97b85
yu-liang-bin <y1416490440@163.com>
2025-12-31 10:01:19 +0800

    add msprof st

---
+++ test/st/src/model.py
@@ -0,0 +1,77 @@
+# -------------------------------------------------------------------------
+# Copyright (c) 2025 Huawei Technologies Co., Ltd.
+# This file is part of the MindStudio project.
+#
+# MindStudio is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#
+#    http://license.coscl.org.cn/MulanPSL2
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+# -------------------------------------------------------------------------
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.models as models
+from torchvision.models import ResNet50_Weights
+
+
+class ResNet50:
+    def __init__(self, num_classes=1000, device=None):
+        # Automatically choose the device: NPU > CUDA > CPU
+        if device is None:
+            if hasattr(torch, 'npu') and torch.npu.is_available():
+                self.device = torch.device("npu:0")
+            else:
+                self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        else:
+            self.device = torch.device(device)
+        print(f"[INFO] Using device: {self.device}")
+
+        # Load ResNet50 (with pretrained weights)
+        self.model = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
+        if num_classes != 1000:
+            self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
+        self.model = self.model.to(self.device)
+
+    def train(self, data_loader, epochs=1, lr=1e-4, freeze_backbone=False):
+        """
+        Simple training function.
+        :param data_loader: torch.utils.data.DataLoader returning (images, labels)
+        :param epochs: Number of epochs to train for
+        :param lr: Learning rate
+        :param freeze_backbone: Whether to freeze the ResNet backbone, only training the classification head
+        """
+        # Optionally freeze the backbone (useful for fine-tuning)
+        if freeze_backbone:
+            for param in self.model.parameters():
+                param.requires_grad = False
+            for param in self.model.fc.parameters():
+                param.requires_grad = True
+
+        # Optimize only parameters that require gradients
+        params_to_optimize = [p for p in self.model.parameters() if p.requires_grad]
+        optimizer = optim.Adam(params_to_optimize, lr=lr)
+        criterion = nn.CrossEntropyLoss().to(self.device)
+
+        self.model.train()
+        for epoch in range(epochs):
+            total_loss = 0.0
+            for inputs, labels in data_loader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+                optimizer.zero_grad()
+                outputs = self.model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                total_loss += loss.item()
+
+            avg_loss = total_loss / len(data_loader)
+            print(f"[Epoch {epoch + 1}/{epochs}] Average Loss: {avg_loss:.4f}")
