ec2b12e32a3dab180efb8db7ed47e06bf7f83027
yu-liang-bin <y1416490440@163.com>
2025-12-31 10:01:19 +0800

    add msprof st

---
+++ test/st/test_ascend_msprof_App_aic_mode_sample_based_read_memory.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+file_name=$(basename $0 .sh)
+
+cur_dir="$(cd "$(dirname "$0")"; pwd)"
+
+MSPROF_OPTS="--ascendcl=on --model-execution=on --runtime-api=on --task-time=on --ai-core=on --l2=on --aic-freq=100 --aicpu=on --sys-hardware-mem=on --aic-mode=sample-based --llc-profiling=read --aic-metrics=Memory"
+MODEL_NAME="resnet50"
+
+"$cur_dir/test_base_msprof.sh" "$file_name" "${MSPROF_OPTS}" "${MODEL_NAME}"
