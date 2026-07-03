7a41c125a5f591947380247e618b9d8f3ad5af8e
MrYudd <2392205247@qq.com>
2026-07-03 18:20:15 +0800

    pcie异常数据提示修改
    Co-authored-by: WangangYu<2392205247@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !336 merge cherry-pick-mr-333-1783072510354-auto into 26.1.0
    
    pcie异常数据提示修改
    
    Created-by: MrYudd
    Commit-by: WangangYu
    Merged-by: ascend-robot
    Description: # PR 合入模板
    
    **注：经过自检不涉及的可标注“不涉及”或直接打勾，特殊情况请文字备注。不符合规范的 PR 不允许合入，请（后备）commit 注意。**
    
    ----
    
    ## 1. 修改描述
    
    - **修改原因：**  pcie异常数据提示修改
    - **修改内容：**  pcie异常数据提示修改，暂时调整error 为 warning
    
    ----
    
    ## 2. 功能验证
    
    - [ ] **功能自验**
    - [ ] **本地自验用例截图**
    - [ ] **冒烟是否通过** （填入群链接的自验证报告中，如未通过，请说明原因：____________________ ，功能代码请主动申报添加冒烟）
    
    ----
    
    ## 3. 分支合并要求
    
    - [ ] **代码合并**（请确保将 master 分支的最新代码同步合并至 poc 分支及 pre-research 分支，同时保证 poc 分支的代码也已正确合并到 pre-research 分支。）
    
    ----
    
    ## 3. 代码检视
    
    - **要求：**
      - 合入代码超过 200 行，需三人以上会议检视。
      - 检视密度≥1个/100行。
      - 检视缺陷密度未达要求需提供说明。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [ ] **是否经过代码检视**
    - [ ] **是否具备 UT 测试用例看护** （如不符合，请说明原因：____________________）
    
    - **检视意见数：____ 条** （请填写本次检视的意见总数，用于commit合入前审视）
    
    ----
    
    ## 4. 安全自检
    
    ### Python、C++
    
    - [ ] **对外接口新增/删除/变更后，资料要同步新增/删除/变更，新增接口入参校验参考外部输入表格**
    - [ ] **不允许私有的文件操作，需要使用公共模块的安全函数**
    - [ ] **任务结束后需要删除临时文件，同时需要考虑任务失败后，临时文件没有残留**
    - [ ] **数组访问需要校验越界场景，对除法需要做除零校验**
    - [ ] **需要对递归方法做递归深度校验，正则表达式必须做 ReDoS 校验**
    - [ ] **需要充分进行接口输入和返回值异常情况的校验**
    - [ ] **日志打印不要出现拼写或语法错误，不要暴露代码细节和敏感信息**
    
    ### C++
    
    - [ ] **指针使用前需要判空**
    - [ ] **数值计算校验溢出和反转**
    - [ ] **不可存在内存泄漏（异常场景需要释放内存）**
    - [ ] **类型转换不能出现数据截断**
    - [ ] **拷贝字符串时，目的缓冲区至少比源缓冲区大 1**
    - [ ] **拷贝内存时，目的缓冲区不小于源缓冲区**
    - [ ] **内存释放后指针赋值为 nullptr**
    
    ----
    
    ## 5. 变更知会
    
    - [ ] **资料修改**
    - [ ] **变更通知（消息知会 + 邮件知会）**
    
    ----
    
    
    See merge request: Ascend/msprof!336

--- analysis/viewer/stars/stars_chip_trans_view.py
+++ analysis/viewer/stars/stars_chip_trans_view.py
@@ -14,7 +14,6 @@
 # See the Mulan PSL v2 for more details.
 # -------------------------------------------------------------------------
 
-import json
 import logging
 from collections import OrderedDict
 from collections import defaultdict
@@ -26,14 +25,15 @@ from common_func.ms_constant.stars_constant import StarsConstant
 from common_func.ms_constant.str_constant import StrConstant
 from common_func.trace_view_header_constant import TraceViewHeaderConstant
 from common_func.trace_view_manager import TraceViewManager
-from msmodel.interface.view_model import ViewModel
 from common_func.platform.chip_manager import ChipManager
+from msmodel.interface.view_model import ViewModel
 
 
 class StarsChipTransView:
     """
     Model of viewer for chip trans
     """
+
     PA_LINK_RX = "PA Link Rx"
     PA_LINK_TX = "PA Link Tx"
     PCIE_WRITE = "PCIE Write Bandwidth"
@@ -43,7 +43,7 @@ class StarsChipTransView:
 
     TIMELINE_MAP = {
         StarsConstant.TYPE_STARS_PA: [PA_LINK_RX, PA_LINK_TX, PA_ID],
-        StarsConstant.TYPE_STARS_PCIE: [PCIE_WRITE, PCIE_READ, PCIE_ID]
+        StarsConstant.TYPE_STARS_PCIE: [PCIE_WRITE, PCIE_READ, PCIE_ID],
     }
 
     def __init__(self: any, configs: dict, params: dict) -> None:
@@ -66,10 +66,11 @@ class StarsChipTransView:
             self.get_pa_data()
             self.get_pcie_data()
         if not self._timeline_data:
-            logging.error("Failed to get stars chip trans data, please check the data parsing log.")
+            logging.warning("Failed to get stars chip trans data, please check the data parsing log.")
             return []
-        self._timeline_data.extend(TraceViewManager.metadata_event(
-            [["process_name", self._pid, self._tid, "Stars Chip Trans"]]))
+        self._timeline_data.extend(
+            TraceViewManager.metadata_event([["process_name", self._pid, self._tid, "Stars Chip Trans"]])
+        )
         return self._timeline_data
 
     def get_pa_data(self: any) -> None:
@@ -77,20 +78,23 @@ class StarsChipTransView:
         get stars pa link data
         :return:
         """
-        pa_model = ViewModel(self._project_path,
-                             DBNameConstant.DB_STARS_CHIP_TRANS, [DBNameConstant.TABLE_STARS_PA_LINK])
+        pa_model = ViewModel(
+            self._project_path, DBNameConstant.DB_STARS_CHIP_TRANS, [DBNameConstant.TABLE_STARS_PA_LINK]
+        )
         if not pa_model.init():
             logging.error("Can not get stars chip trans data, please check the data parsing log.")
             return
         if not pa_model.check_table():
             return
-        sql = "select pa_link_id,pa_link_traffic_monit_rx,pa_link_traffic_monit_tx,sys_time/{NS_TO_US} from {0}" \
-            .format(DBNameConstant.TABLE_STARS_PA_LINK, NS_TO_US=NumberConstant.NS_TO_US)
+        sql = "select pa_link_id,pa_link_traffic_monit_rx,pa_link_traffic_monit_tx,sys_time/{NS_TO_US} from {0}".format(
+            DBNameConstant.TABLE_STARS_PA_LINK, NS_TO_US=NumberConstant.NS_TO_US
+        )
         pa_data = pa_model.get_sql_data(sql)
         if not pa_data:
             return
-        self._timeline_data.extend(TraceViewManager.metadata_event(
-            [["thread_name", self._pid, StarsConstant.TYPE_STARS_PA, "PA Link"]]))
+        self._timeline_data.extend(
+            TraceViewManager.metadata_event([["thread_name", self._pid, StarsConstant.TYPE_STARS_PA, "PA Link"]])
+        )
         self._format_data(pa_data, StarsConstant.TYPE_STARS_PA)
 
     def get_pcie_data(self: any) -> None:
@@ -98,17 +102,20 @@ class StarsChipTransView:
         get stars pcie data
         :return:
         """
-        pcie_model = ViewModel(self._project_path,
-                               DBNameConstant.DB_STARS_CHIP_TRANS, [DBNameConstant.TABLE_STARS_PCIE])
+        pcie_model = ViewModel(
+            self._project_path, DBNameConstant.DB_STARS_CHIP_TRANS, [DBNameConstant.TABLE_STARS_PCIE]
+        )
         pcie_data = []
         with pcie_model:
-            sql = "select pcie_id,pcie_write_bandwidth,pcie_read_bandwidth,sys_time/{NS_TO_US} from {0}" \
-                .format(DBNameConstant.TABLE_STARS_PCIE, NS_TO_US=NumberConstant.NS_TO_US)
+            sql = "select pcie_id,pcie_write_bandwidth,pcie_read_bandwidth,sys_time/{NS_TO_US} from {0}".format(
+                DBNameConstant.TABLE_STARS_PCIE, NS_TO_US=NumberConstant.NS_TO_US
+            )
             pcie_data = pcie_model.get_sql_data(sql)
         if not pcie_data:
             return
-        self._timeline_data.extend(TraceViewManager.metadata_event(
-            [["thread_name", self._pid, StarsConstant.TYPE_STARS_PCIE, "PCIE"]]))
+        self._timeline_data.extend(
+            TraceViewManager.metadata_event([["thread_name", self._pid, StarsConstant.TYPE_STARS_PCIE, "PCIE"]])
+        )
         self._format_data(pcie_data, StarsConstant.TYPE_STARS_PCIE)
 
     def get_pcie_data_v6(self: any) -> None:
@@ -116,33 +123,47 @@ class StarsChipTransView:
         get stars pcie data v6
         :return:
         """
-        pcie_model = ViewModel(self._project_path,
-                               DBNameConstant.DB_STARS_CHIP_TRANS, [DBNameConstant.TABLE_STARS_PCIE_V6])
+        pcie_model = ViewModel(
+            self._project_path, DBNameConstant.DB_STARS_CHIP_TRANS, [DBNameConstant.TABLE_STARS_PCIE_V6]
+        )
         pcie_data = []
         with pcie_model:
-            sql = "select die_id,pcie_write_bandwidth,pcie_read_bandwidth,sys_time/{NS_TO_US} from {0}" \
-                .format(DBNameConstant.TABLE_STARS_PCIE_V6, NS_TO_US=NumberConstant.NS_TO_US)
+            sql = "select die_id,pcie_write_bandwidth,pcie_read_bandwidth,sys_time/{NS_TO_US} from {0}".format(
+                DBNameConstant.TABLE_STARS_PCIE_V6, NS_TO_US=NumberConstant.NS_TO_US
+            )
             pcie_data = pcie_model.get_sql_data(sql)
         if not pcie_data:
             return
         pcie_data_dict = defaultdict(dict)
         for data in pcie_data:
             pcie_data_dict.setdefault(data[3], {}).update({data[0]: data[1:]})
-        self._timeline_data.extend(TraceViewManager.metadata_event(
-            [["thread_name", self._pid, StarsConstant.TYPE_STARS_PCIE, "PCIE"]]))
+        self._timeline_data.extend(
+            TraceViewManager.metadata_event([["thread_name", self._pid, StarsConstant.TYPE_STARS_PCIE, "PCIE"]])
+        )
         stars_cons = self.TIMELINE_MAP.get(StarsConstant.TYPE_STARS_PCIE)
         _res = []
         for key, data in pcie_data_dict.items():
             _res.extend(
-                [[stars_cons[0], InfoConfReader().trans_into_local_time(key, use_us=True), self._pid,
-                  StarsConstant.TYPE_STARS_PCIE,
-                  OrderedDict([("U-Die" + str(_key), _val[0]) for _key, _val in data.items()])],
-                 [stars_cons[1], InfoConfReader().trans_into_local_time(key, use_us=True), self._pid,
-                  StarsConstant.TYPE_STARS_PCIE,
-                  OrderedDict([("U-Die" + str(_key), _val[1]) for _key, _val in data.items()])]
-                 ])
-        self._timeline_data.extend(TraceViewManager.column_graph_trace(TraceViewHeaderConstant.COLUMN_GRAPH_HEAD_LEAST,
-                                                                       _res))
+                [
+                    [
+                        stars_cons[0],
+                        InfoConfReader().trans_into_local_time(key, use_us=True),
+                        self._pid,
+                        StarsConstant.TYPE_STARS_PCIE,
+                        OrderedDict([("U-Die" + str(_key), _val[0]) for _key, _val in data.items()]),
+                    ],
+                    [
+                        stars_cons[1],
+                        InfoConfReader().trans_into_local_time(key, use_us=True),
+                        self._pid,
+                        StarsConstant.TYPE_STARS_PCIE,
+                        OrderedDict([("U-Die" + str(_key), _val[1]) for _key, _val in data.items()]),
+                    ],
+                ]
+            )
+        self._timeline_data.extend(
+            TraceViewManager.column_graph_trace(TraceViewHeaderConstant.COLUMN_GRAPH_HEAD_LEAST, _res)
+        )
 
     def _format_data(self: any, stars_data: list, stars_type: int) -> None:
         _res = []
@@ -152,10 +173,23 @@ class StarsChipTransView:
             #               pa_link_id,pa_link_traffic_monit_rx,pa_link_traffic_monit_tx,sys_time
             # stars_cons: pa rx/ pcie read name, pa tx/ pcie write name, id
             _res.extend(
-                [[stars_cons[0], InfoConfReader().trans_into_local_time(_data[3], use_us=True), self._pid,
-                  stars_type, OrderedDict([(stars_cons[0], _data[1]), (stars_cons[2], _data[0])])],
-                 [stars_cons[1], InfoConfReader().trans_into_local_time(_data[3], use_us=True), self._pid,
-                  stars_type, OrderedDict([(stars_cons[1], _data[2]), (stars_cons[2], _data[0])])]
-                 ])
-        self._timeline_data.extend(TraceViewManager.column_graph_trace(TraceViewHeaderConstant.COLUMN_GRAPH_HEAD_LEAST,
-                                                                       _res))
+                [
+                    [
+                        stars_cons[0],
+                        InfoConfReader().trans_into_local_time(_data[3], use_us=True),
+                        self._pid,
+                        stars_type,
+                        OrderedDict([(stars_cons[0], _data[1]), (stars_cons[2], _data[0])]),
+                    ],
+                    [
+                        stars_cons[1],
+                        InfoConfReader().trans_into_local_time(_data[3], use_us=True),
+                        self._pid,
+                        stars_type,
+                        OrderedDict([(stars_cons[1], _data[2]), (stars_cons[2], _data[0])]),
+                    ],
+                ]
+            )
+        self._timeline_data.extend(
+            TraceViewManager.column_graph_trace(TraceViewHeaderConstant.COLUMN_GRAPH_HEAD_LEAST, _res)
+        )
